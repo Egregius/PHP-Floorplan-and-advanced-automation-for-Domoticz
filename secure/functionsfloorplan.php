5a0e7436656b8f7cf5d6038630321d7a74241c51
root <root@domoticz.egregius.be>
2019-05-19 13:20:01 +0200

    Update

--- secure/functionsfloorplan.php
+++ secure/functionsfloorplan.php
@@ -50,8 +50,6 @@ function dimmer($name,$class='i70')
     global $page,$d;
     $page=str_replace('ajax', '', $page);
     echo '
-        <div class="fix z '.$name.'" onclick="location.href=\''.$page.'?setdimmer='.$name.'\';" id="'.$name.'"></div>';
-    /*echo '
         <form method="POST" action="">
             <div class="fix z '.$name.'" onclick="location.href=\''.$page.'?setdimmer='.$name.'\';">
                 <input type="hidden" name="setdimmer" value="'.$name.'">';
@@ -69,7 +67,7 @@ function dimmer($name,$class='i70')
     }
     echo '
 	        </div>
-        </form>';*/
+        </form>';
 }
 function schakelaar($name)
 {
