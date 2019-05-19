c5c862e5a6dd1edbb465420597ed75e4582d80a6
root <root@domoticz.egregius.be>
2019-05-19 09:10:01 +0200

    Update

--- floorplan.heating.php
+++ floorplan.heating.php
@@ -315,7 +315,7 @@ if ($home) {
 					<input type="hidden" name="Roller" value="true">
 				</div>
 				<div class="fix z" style="top:100px;left:70px;">
-					<input type="image" name="Rollerleveloff" value ="0" src="images/arrowgreendown.png" class="i90">
+					<input type="image" name="Rollerleveloff" value ="100" src="images/arrowgreendown.png" class="i90">
 				</div>
 				<div class="fix z" style="top:130px;left:200px;">
 					<input type="submit" name="mode" value ="';
@@ -323,7 +323,7 @@ if ($home) {
         echo '" class="btn i90">
 				</div>
 				<div class="fix z" style="top:100px;left:330px;">
-					<input type="image" name="Rollerlevelon" value ="100" src="images/arrowgreenup.png" class="i90">
+					<input type="image" name="Rollerlevelon" value ="0" src="images/arrowgreenup.png" class="i90">
 				</div>
 				<div class="fix z" style="top:210px;left:10px;">';
         $levels=array(0,25,30,32,34,36,38,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,62,64,66,68,70,75,95,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100);
