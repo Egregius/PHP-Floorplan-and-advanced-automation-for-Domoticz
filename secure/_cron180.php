01726f77eb7252d227dc37004df4134070d88ff2
root <root@domoticz.egregius.be>
2019-05-22 14:40:02 +0200

    Update

--- secure/_cron180.php
+++ secure/_cron180.php
@@ -10,3 +10,22 @@
  * @link     https://egregius.be
  **/
 //lg('               __CRON180__');
+if ($d['bose103']['s']=='On'&&(TIME>=strtotime('22:00')||TIME<=strtotime('5:00')) {
+    $nowplaying=json_decode(json_encode(simplexml_load_string(file_get_contents('http://192.168.2.103:8090/now_playing'))), true);
+    if (!empty($nowplaying)) {
+        if (isset($nowplaying['@attributes']['source'])) {
+            if ($nowplaying['@attributes']['source']!='STANDBY') {
+                $volume=json_decode(json_encode(simplexml_load_string(file_get_contents("http://192.168.2.103:8090/volume"))), true);
+                $cv=$volume['actualvolume']-1;
+                if ($cv==0) {
+                    bosekey("POWER", 0, 103);
+                    sw('bose103', 'Off', true);
+                } else {
+                    bosevolume($cv, 103);
+                }
+            } else {
+                sw('bose103', 'Off', true);
+            }
+        }
+    }
+}
