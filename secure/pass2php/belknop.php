dc32db4e6827ede48d0ec5140b015b8ffd0a60d9
Egregius <guy@egregius.be>
2019-05-24 15:20:01 +0200

    Update

--- secure/pass2php/belknop.php
+++ secure/pass2php/belknop.php
@@ -15,20 +15,21 @@ if ($status=='On'&&$d['auto']['s']&&past('belknop')>10) {
     if ($d['lgtv']['s']=='On') {
         shell_exec('python3 ../lgtv.py -c send-message -a "Deurbel" 192.168.2.27');
     }
-    shell_exec('curl -s "http://192.168.2.11/fifo_command.php?cmd=record%20on%205%2055" > /dev/null 2>/dev/null &');
-    shell_exec('curl -s "http://192.168.2.11/telegram.php?deurbel=true" > /dev/null 2>/dev/null &');
-    shell_exec('curl -s "http://192.168.2.13/telegram.php?snapshot=true" > /dev/null 2>/dev/null &');
+    //shell_exec('curl -s "http://192.168.2.11/fifo_command.php?cmd=record%20on%205%2055" > /dev/null 2>/dev/null &');
+    //shell_exec('curl -s "http://192.168.2.11/telegram.php?deurbel=true" > /dev/null 2>/dev/null &');
+    //shell_exec('curl -s "http://192.168.2.13/telegram.php?snapshot=true" > /dev/null 2>/dev/null &');
     if ($d['zon']['s']==0) {
         sw('voordeur', 'On');
     }
     if ($d['Weg']['s']==0) {
         if ($d['Xvol']['s']!=40) {
             sl('Xvol', 40);
+            usleep(10000);
         }
-        usleep(10000);
         sl('Xbel', 10);
         sw('deurbel', 'On');
         rgb('Xlight', 360, 100);
     }
     telegram('Deurbel', true, 2);
+    sl('Xvol', 5);
 }
