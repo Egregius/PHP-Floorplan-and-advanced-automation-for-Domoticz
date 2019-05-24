21c06577adef58459eb592c19b17ce178ec20f37
Egregius <guy@egregius.be>
2019-05-24 15:30:02 +0200

    Update

--- secure/pass2php/belknop.php
+++ secure/pass2php/belknop.php
@@ -9,15 +9,14 @@
  * @license  GNU GPLv3
  * @link     https://egregius.be
  **/
-/*if ($status=='On'&&$d['auto']['s']=='On'&&past('belknop')>10) {
-    //shell_exec('../ios.sh "Deurbel" > /dev/null 2>/dev/null &');
-    shell_exec('../boseplayinfo.sh "Deurbel" > /dev/null 2>/dev/null &');
+if ($status=='On'&&$d['auto']['s']=='On'&&past('belknop')>10) {
+    shell_exec('/var/www/html/secure/boseplayinfo.sh "Deurbel" > /dev/null 2>/dev/null &');
     if ($d['lgtv']['s']=='On') {
         shell_exec('python3 ../lgtv.py -c send-message -a "Deurbel" 192.168.2.27');
     }
-    //shell_exec('curl -s "http://192.168.2.11/fifo_command.php?cmd=record%20on%205%2055" > /dev/null 2>/dev/null &');
-    //shell_exec('curl -s "http://192.168.2.11/telegram.php?deurbel=true" > /dev/null 2>/dev/null &');
-    //shell_exec('curl -s "http://192.168.2.13/telegram.php?snapshot=true" > /dev/null 2>/dev/null &');
+    shell_exec('curl -s "http://192.168.2.11/fifo_command.php?cmd=record%20on%205%2055" > /dev/null 2>/dev/null &');
+    shell_exec('curl -s "http://192.168.2.11/telegram.php?deurbel=true" > /dev/null 2>/dev/null &');
+    shell_exec('curl -s "http://192.168.2.13/telegram.php?snapshot=true" > /dev/null 2>/dev/null &');
     if ($d['zon']['s']==0) {
         sw('voordeur', 'On');
     }
@@ -33,5 +32,4 @@
     telegram('Deurbel', true, 2);
     sleep(2);
     sl('Xvol', 5);
-}*/
-echo shell_exec('../boseplayinfo.sh "Deurbel" > /dev/null 2>/dev/null &');
+}
