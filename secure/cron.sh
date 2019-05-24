69630211983110fdc19247604863525b1a4af4da
Egregius <guy@egregius.be>
2019-05-24 13:15:02 +0200

    Update

--- secure/cron.sh
+++ secure/cron.sh
@@ -19,16 +19,23 @@ DOMOTICZ=`curl -s --connect-timeout 2 --max-time 5 "http://127.0.0.1:8080/json.h
 STATUS=`echo $DOMOTICZ | jq -r '.status'`
 if [ "$STATUS" == "OK" ] ; then
     MINUTE=$(date +"%M")
+    MINUTE=2
+    CRON=""
     echo $MINUTE
     if [ $(($MINUTE%2)) -eq 0 ] ; then
-     echo "gedeeld door 2"
-    elif [ $(($MINUTE%3)) -eq 0 ] ; then
-     echo "gedeeld door 3"
-    elif [ $(($MINUTE%4)) -eq 0 ] ; then
-     echo "gedeeld door 4"
-    elif [ $(($MINUTE%5)) -eq 0 ] ; then
-     echo "gedeeld door 5"
+        CRON="$CRON&CRON120"
+        echo "gedeeld door 2 $CRON"
     fi
+    if [ $(($MINUTE%3)) -eq 0 ] ; then
+        echo "gedeeld door 3 $CRON"
+    fi
+    if [ $(($MINUTE%4)) -eq 0 ] ; then
+        echo "gedeeld door 4 $CRON"
+    fi
+    if [ $(($MINUTE%5)) -eq 0 ] ; then
+        echo "gedeeld door 5 $CRON"
+    fi
+    exit
 	echo OK
 	#0
 	curl -s --connect-timeout 2 --max-time 30 "http://127.0.0.1/secure/cron.php?rolluiken&cron60&cron10&verwarming" >/dev/null 2>&1 &
