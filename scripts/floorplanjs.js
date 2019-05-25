de6fbe4a25efcb3ea65e2d012f0ca02f21866bc0
Egregius <guy@egregius.be>
2019-05-25 07:55:02 +0200

    Update

--- scripts/floorplanjs.js
+++ scripts/floorplanjs.js
@@ -769,32 +769,3 @@ function toggle_visibility(id){
     if(e.style.display=='inherit') e.style.display='none';
     else e.style.display='inherit';
 }
-function fullScreen() {
-  // Kind of painful, but this is how it works for now
-  if (document.documentElement.requestFullscreen) {
-    document.documentElement.requestFullscreen();
-  } else if (document.documentElement.mozRequestFullScreen) {
-    document.documentElement.mozRequestFullScreen();
-  } else if (document.documentElement.webkitRequestFullscreen) {
-    document.documentElement.webkitRequestFullscreen();
-  } else if (document.documentElement.msRequestFullscreen) {
-    document.documentElement.msRequestFullscreen();
-  }
-}
-
-function smolScreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.webkitExitFullscreen) {
-    document.webkitExitFullscreen();
-  } else if (document.mozCancelFullScreen) {
-    document.mozCancelFullScreen();
-  } else if (document.msExitFullscreen) {
-    document.msExitFullscreen();
-  }
-}
-
-function lock(orientation) {
-  fullScreen();
-  screen.orientation.lock(orientation);
-}
