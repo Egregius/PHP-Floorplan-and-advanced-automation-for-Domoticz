677450ad66677d5c9a780208aef284a0e49d086a
Egregius <guy@egregius.be>
2019-05-29 07:20:01 +0200

    Update

--- scripts/floorplanjs.js
+++ scripts/floorplanjs.js
@@ -55,17 +55,13 @@ function ajax(){
                                     if(tijd>$LastUpdateTime-60)document.getElementById("t"+items[i]).style.color="#FF8800";
                                     else if(tijd>$LastUpdateTime-90)document.getElementById("t"+items[i]).style.color="#FFAA00";
                                     else if(tijd>$LastUpdateTime-300)document.getElementById("t"+items[i]).style.color="#FFCC00";
-                                    else if(tijd>$LastUpdateTime-900)document.getElementById("t"+items[i]).style.color="#FFFF00";
+                                    else if(tijd>$LastUpdateTime-600)document.getElementById("t"+items[i]).style.color="#FFFF00";
                                     else if(tijd>$LastUpdateTime-7200)document.getElementById("t"+items[i]).style.color="#CCC";
-                                    else if(tijd>$LastUpdateTime-10800)document.getElementById("t"+items[i]).style.color="#BBB";
-                                    else if(tijd>$LastUpdateTime-14400)document.getElementById("t"+items[i]).style.color="#AAA";
-                                    else if(tijd>$LastUpdateTime-18000)document.getElementById("t"+items[i]).style.color="#999";
-                                    else if(tijd>$LastUpdateTime-21600)document.getElementById("t"+items[i]).style.color="#888";
-                                    else if(tijd>$LastUpdateTime-25200)document.getElementById("t"+items[i]).style.color="#777";
-                                    else if(tijd>$LastUpdateTime-28800)document.getElementById("t"+items[i]).style.color="#666";
-                                    else if(tijd>$LastUpdateTime-32400)document.getElementById("t"+items[i]).style.color="#555";
-                                    else if(tijd>$LastUpdateTime-36000)document.getElementById("t"+items[i]).style.color="#444";
-                                    else if(tijd>$LastUpdateTime-54000)document.getElementById("t"+items[i]).style.color="#333";
+                                    else if(tijd>$LastUpdateTime-14400)document.getElementById("t"+items[i]).style.color="#BBB";
+                                    else if(tijd>$LastUpdateTime-21600)document.getElementById("t"+items[i]).style.color="#AAA";
+                                    else if(tijd>$LastUpdateTime-28800)document.getElementById("t"+items[i]).style.color="#999";
+                                    else if(tijd>$LastUpdateTime-36000)document.getElementById("t"+items[i]).style.color="#888";
+                                    else if(tijd>$LastUpdateTime-43200)document.getElementById("t"+items[i]).style.color="#777";
                                     else if(tijd>$LastUpdateTime-82800)document.getElementById("t"+items[i]).style.color="#000";
                                 }else{
                                     document.getElementById("t"+items[i]).style.color=null;
@@ -82,17 +78,13 @@ function ajax(){
                                     if(tijd>$LastUpdateTime-60)document.getElementById("t"+items[i]).style.color="#FF8800";
                                     else if(tijd>$LastUpdateTime-90)document.getElementById("t"+items[i]).style.color="#FFAA00";
                                     else if(tijd>$LastUpdateTime-300)document.getElementById("t"+items[i]).style.color="#FFCC00";
-                                    else if(tijd>$LastUpdateTime-900)document.getElementById("t"+items[i]).style.color="#FFFF00";
+                                    else if(tijd>$LastUpdateTime-600)document.getElementById("t"+items[i]).style.color="#FFFF00";
                                     else if(tijd>$LastUpdateTime-7200)document.getElementById("t"+items[i]).style.color="#CCC";
-                                    else if(tijd>$LastUpdateTime-10800)document.getElementById("t"+items[i]).style.color="#BBB";
-                                    else if(tijd>$LastUpdateTime-14400)document.getElementById("t"+items[i]).style.color="#AAA";
-                                    else if(tijd>$LastUpdateTime-18000)document.getElementById("t"+items[i]).style.color="#999";
-                                    else if(tijd>$LastUpdateTime-21600)document.getElementById("t"+items[i]).style.color="#888";
-                                    else if(tijd>$LastUpdateTime-25200)document.getElementById("t"+items[i]).style.color="#777";
-                                    else if(tijd>$LastUpdateTime-28800)document.getElementById("t"+items[i]).style.color="#666";
-                                    else if(tijd>$LastUpdateTime-32400)document.getElementById("t"+items[i]).style.color="#555";
-                                    else if(tijd>$LastUpdateTime-36000)document.getElementById("t"+items[i]).style.color="#444";
-                                    else if(tijd>$LastUpdateTime-54000)document.getElementById("t"+items[i]).style.color="#333";
+                                    else if(tijd>$LastUpdateTime-14400)document.getElementById("t"+items[i]).style.color="#BBB";
+                                    else if(tijd>$LastUpdateTime-21600)document.getElementById("t"+items[i]).style.color="#AAA";
+                                    else if(tijd>$LastUpdateTime-28800)document.getElementById("t"+items[i]).style.color="#999";
+                                    else if(tijd>$LastUpdateTime-36000)document.getElementById("t"+items[i]).style.color="#888";
+                                    else if(tijd>$LastUpdateTime-43200)document.getElementById("t"+items[i]).style.color="#777";
                                     else if(tijd>$LastUpdateTime-82800)document.getElementById("t"+items[i]).style.color="#000";
                                 }else{
                                     document.getElementById("t"+items[i]).style.color=null;
