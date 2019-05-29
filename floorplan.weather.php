62fa0cadd0a59fcdcf41be68c8223fc30cc24b2c
Egregius <guy@egregius.be>
2019-05-29 12:20:00 +0200

    Update

--- floorplan.weather.php
+++ floorplan.weather.php
@@ -17,19 +17,38 @@ require 'secure/functionsfloorplan.php';
 require 'secure/authentication.php';
 if ($home) {
     createheader();
-    echo '
+    $ds=json_decode(file_get_contents('/temp/ds.json'));
+	$ow=json_decode(file_get_contents('/temp/ow.json'));
+	echo '
 	<body>
 	    <div class="fix" id="clock"><a href=\'javascript:navigator_Go("floorplan.weather.php");\' id="time">Refresh</a></div>
 	    <div>
 	        <br>
-	        Nu:
+	        <table>
+	            <thead>
+    	            <tr>
+    	                <th></th>
+    	                <th>Temp</th>
+    	                <th></th>
+    	            </tr>
+    	        </thead>
+    	        <tbody>
+    	            <tr>
+    	                <td>Nu:</td>
+    	                <td>'.number_format($ds->currently->temperature, 1, ',', '').'&#8451;</td>
+    	            </tr>
+    	        </tbody>
+	        </table>
+	        Komende 48u:
+	        <table>
+	            <tr><td></td><td></td></tr>
+	        </table>
+	        Komende week:
+	        <table>
+	            <tr><td></td><td></td></tr>
+	        </table>
 	    </div>';
-	$ds=json_decode(file_get_contents('/temp/ds.json'));
-	$ow=json_decode(file_get_contents('/temp/ow.json'));
 	echo '<pre>';print_r($ds);echo '</pre>';
 	echo '<pre>';print_r($ow);echo '</pre>';
-	echo '
-	    <script>
-	        setTimeout("window.location.href=window.location.href;", 1000);
-	    </script>';
+	//echo '<script>setTimeout("window.location.href=window.location.href;", 1000);</script>';
 }
