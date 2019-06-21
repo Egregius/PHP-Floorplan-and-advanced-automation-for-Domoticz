bd77e07270afd779dc36fcc5d6e67d9163bd5495
Egregius <guy@egregius.be>
2019-06-21 12:30:50 +0200

    Update

--- secure/_cron240.php
+++ secure/_cron240.php
@@ -26,5 +26,99 @@ if ($d['auto']['s']=='On') {
 				alert('raamalex', 'Raam Alex dicht doen, '.$alex_temp.' °C.', 1800,	false);
 			}
 		}
+		if ($d['heating']['s']>=2) {
+            if ($d['buiten_temp']['s']>$d['kamer_temp']['s']
+                &&$d['buiten_temp']['s']>$d['tobi_temp']['s']
+                &&$d['buiten_temp']['s']>$d['alex_temp']['s']
+                &&($d['raamkamer']['s']=='Open'
+                ||$d['raamtobi']['s']=='Open'
+                ||$d['raamalex']['s']=='Open')
+                &&($d['kamer_temp']['s']>17
+                ||$d['tobi_temp']['s']>17
+                ||$d['alex_temp']['s']>17)
+            ) {
+                alert(
+                    'ramenboven',
+                    'Ramen boven dicht doen, te warm buiten.
+                    Buiten = '.round($d['buiten_temp']['s'], 1).',
+                    kamer = '.$d['kamer_temp']['s'].',
+                    Tobi = '.$d['tobi_temp']['s'].',
+                    Alex = '.$d['alex_temp']['s'],
+                    7200,
+                    false,
+                    2,
+                    false
+                );
+            } elseif (($d['buiten_temp']['s']<=$d['kamer_temp']['s']
+                ||$d['buiten_temp']['s']<=$d['tobi_temp']['s']
+                ||$d['buiten_temp']['s']<=$d['alex_temp']['s'])
+                &&($d['raamkamer']['s']=='Closed'
+                ||$d['raamtobi']['s']=='Closed'
+                ||$d['raamalex']['s']=='Closed')
+                &&($d['kamer_temp']['s']>17
+                ||$d['tobi_temp']['s']>17
+                ||$d['alex_temp']['s']>17)
+            ) {
+                alert(
+                    'ramenboven',
+                    'Ramen boven open doen, te warm binnen.
+                    Buiten = '.round($d['buiten_temp']['s'], 1).',
+                    kamer = '.$d['kamer_temp']['s'].',
+                    Tobi = '.$d['tobi_temp']['s'].',
+                    Alex = '.$d['alex_temp']['s'],
+                    7200,
+                    false,
+                    2,
+                    false
+                );
+            }
+        } else {
+            if (($d['buiten_temp']['s']>$d['kamer_temp']['s']
+                &&$d['buiten_temp']['s']>$d['tobi_temp']['s']
+                &&$d['buiten_temp']['s']>$d['alex_temp']['s'])
+                &&$d['buiten_temp']['s']>22
+                &&($d['kamer_temp']['s']>19
+                ||$d['tobi_temp']['s']>19
+                ||$d['alex_temp']['s']>19)
+                &&($d['raamkamer']['s']=='Open'
+                ||$d['raamtobi']['s']=='Open'
+                ||$d['raamalex']['s']=='Open')
+            ) {
+                alert(
+                    'ramenboven',
+                    'Ramen boven dicht doen, te warm buiten.
+                    Buiten = '.round($d['buiten_temp']['s'], 1).',
+                    kamer = '.$d['kamer_temp']['s'].',
+                    Tobi = '.$d['tobi_temp']['s'].',
+                    Alex = '.$d['alex_temp']['s'],
+                    7200,
+                    false,
+                    2,
+                    false
+                );
+            } elseif (($d['buiten_temp']['s']<=$d['kamer_temp']['s']
+                ||$d['buiten_temp']['s']<=$d['tobi_temp']['s']
+                ||$d['buiten_temp']['s']<=$d['alex_temp']['s'])
+                &&($d['kamer_temp']['s']>19
+                ||$d['tobi_temp']['s']>19
+                ||$d['alex_temp']['s']>19)
+                &&($d['raamkamer']['s']=='Closed'
+                ||$d['raamtobi']['s']=='Closed'
+                ||$d['raamalex']['s']=='Closed')
+            ) {
+                alert(
+                    'ramenboven',
+                    'Ramen boven open doen, te warm binnen.
+                    Buiten = '.round($d['buiten_temp']['s'], 1).',
+                    kamer = '.$d['kamer_temp']['s'].',
+                    Tobi = '.$d['tobi_temp']['s'].',
+                    Alex = '.$d['alex_temp']['s'],
+                    7200,
+                    false,
+                    2,
+                    false
+                );
+            }
+        }
 	}
 }
