98b36a83414b4612929a9bcfc3417b907d42da95
Egregius <guy@egregius.be>
2019-06-23 10:05:49 +0200

    Update

--- secure/_cron120.php
+++ secure/_cron120.php
@@ -57,21 +57,21 @@ if ($d['auto']['s']=='On') {
 		$x=$y+$x;
 		$windhist=round($x/4, 2);
 	}
-	if (	$d['heating']['s']>=2	&&$d['living_temp']['s']>22&&$d['zon']['s']>3000&&TIME>strtotime("10:00")) { //Heating
+	if (	$d['heating']['s']>=2	&&$d['living_temp']['s']>22&&$d['zon']['s']>3000&&TIME>=strtotime("10:00")) { //Heating
 		if ($d['wind']['s']>=30) 	 $luifel=0;
 		elseif ($d['wind']['s']>=25) $luifel=25;
 		elseif ($d['wind']['s']>=20) $luifel=30;
 		elseif ($d['wind']['s']>=15) $luifel=35;
 		elseif ($d['wind']['s']>=10) $luifel=40;
 		else $luifel=40;
-	} elseif ($d['heating']['s']==0	&&$d['living_temp']['s']>20&&$d['zon']['s']>2000&&TIME>strtotime("10:00")) { //Neutral
+	} elseif ($d['heating']['s']==0	&&$d['living_temp']['s']>20&&$d['zon']['s']>2000&&TIME>=strtotime("10:00")) { //Neutral
 		if ($d['wind']['s']>=30) 	 $luifel=0;
 		elseif ($d['wind']['s']>=25) $luifel=25;
 		elseif ($d['wind']['s']>=20) $luifel=30;
 		elseif ($d['wind']['s']>=15) $luifel=35;
 		elseif ($d['wind']['s']>=10) $luifel=40;
 		else $luifel=40;
-	} elseif ($d['heating']['s']==1	&&$d['living_temp']['s']>19&&$d['zon']['s']>500&&TIME>strtotime("10:00")) { //Cooling
+	} elseif ($d['heating']['s']==1	&&$d['living_temp']['s']>19&&$d['zon']['s']>200&&TIME>=strtotime("10:00")) { //Cooling
 		if ($d['wind']['s']>=30) 	 $luifel=0;
 		elseif ($d['wind']['s']>=25) $luifel=30;
 		elseif ($d['wind']['s']>=20) $luifel=40;
@@ -81,14 +81,21 @@ if ($d['auto']['s']=='On') {
 	} else {
 		$luifel=0;
 	}
-	if ($luifel>$d['luifel']['s']) $luifel=$d['luifel']['s']+10;
-	elseif ($luifel<$d['luifel']['s']&&$d['achterdeur']['s']=='Closed') $luifel=$d['luifel']['s']-10;
+	//if ($luifel>$d['luifel']['s']) $luifel=$d['luifel']['s']+10;
+	//elseif ($luifel<$d['luifel']['s']&&$d['achterdeur']['s']=='Closed') $luifel=$d['luifel']['s']-10;
 	if ($d['wind']['s']>=40) 	 $luifel=0;
 	if ($d['buien']['s']>=10) 	 $luifel=0;
 	if ($luifel<10) $luifel=0;
 	elseif ($luifel>100) $luifel=100;
 
 	echo $luifel;
+	
+	echo past('luifel');
+	if (		past('luifel')>900&&$d['luifel']['s']<$luifel&&$d['luifel']['m']==0&&$d['wind']['s']<$windhist) {
+		sl('luifel', $luifel);
+	} elseif (	past('luifel')>300&&$d['luifel']['s']>$luifel&&$d['luifel']['m']==0) {
+		sl('luifel', $luifel);
+	}
 	if ($d['luifel']['m']==1) {
 		if (past('luifel')>3600&&$luifel<30) {
 			storemode('luifel', 0);
@@ -97,10 +104,5 @@ if ($d['auto']['s']=='On') {
 			storemode('luifel', 0);
 			$d['luifel']['m']=1;
 		}
-	}
-	if (		past('luifel')>900&&$d['luifel']['s']<$luifel&&$d['luifel']['m']==0&&$d['wind']['s']<$windhist) {
-		sl('luifel', $luifel);
-	} elseif (	past('luifel')>300&&$d['luifel']['s']>$luifel&&$d['luifel']['m']==0) {
-		sl('luifel', $luifel);
 	}
 }
