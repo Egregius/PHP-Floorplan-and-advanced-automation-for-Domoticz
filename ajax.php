df9627f484d53eca4625ba2c97f86ccee73c3e97
Egregius <guy@egregius.be>
2019-06-27 09:05:49 +0200

    Update

--- ajax.php
+++ ajax.php
@@ -30,7 +30,7 @@ if ($home==true) {
         $t=time();
         $d['t']=$t;
         $diff=$t-$_REQUEST['t'];
-        $t=$_REQUEST['t']-1;
+        $t=$_REQUEST['t']-2;
         $db=new PDO("mysql:host=localhost;dbname=domotica;", 'domotica', 'domotica');
         $stmt=$db->query("SELECT n,i,s,t,m,dt,icon FROM devices WHERE t >= $t;");
         while ($row=$stmt->fetch(PDO::FETCH_ASSOC)) {
