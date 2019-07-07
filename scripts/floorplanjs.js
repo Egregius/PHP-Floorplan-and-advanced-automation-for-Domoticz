880dee741b72966aad10829f70b181e991d29b56
Egregius <guy@egregius.be>
2019-07-07 07:40:49 +0200

    Update

--- scripts/floorplanjs.js
+++ scripts/floorplanjs.js
@@ -974,7 +974,7 @@ function floorplan(){
         items.forEach(function(item){html+='<div class="fix" id="'+item+'"></div>';});
         html+='<div class="fix verbruik" onclick="location.href=\'https://verbruik.egregius.be/dag.php?Guy=on\';" id="verbruik"><table><tr id="trelec"></tr><tr id="trzon"><td>Zon:</td><td id="zon"></td><td id="zonvandaag"></td></tr><tr id="trgas"></tr><tr id="trwater"></tr><tr id="trdgas"></tr><tr id="trdwater"></tr>';
         watertuin=localStorage.getItem('watertuin');
-			if(watertuin>0)html+='<tr><td>Tuin</td><td id="watertuin">'+watertuin+' L</td></tr>';
+			if(watertuin!='undefined')html+='<tr><td>Tuin</td><td id="watertuin">'+watertuin+' L</td></tr>';
 		html+='</table></div>';
         $('#placeholder').html(html).fadeIn(3000);
    }catch{}
@@ -1111,7 +1111,7 @@ function floorplanothers(){
         	html+='<button onclick="ajaxcontrol(\'water\', \'water\', 7200);" class="btn b3" id="water7200">Water 2 uur</button>';
 		}
 		watertuin=localStorage.getItem('watertuin');
-		if(watertuin>0)html+='<br><span id="watertuin">'+watertuin+' L</span>';
+		if(watertuin!='undefined')html+='<br><span id="watertuin">'+watertuin+' L</span>';
 		html+='<div class="fix z1 bottom" style="right:0px"><form method="POST"><input type="hidden" name="username" value="logout"/><input type="submit" name="logout" value="Logout" class="btn" style="padding:0px;margin:0px;width:90px;height:35px;"/></form><br/><br/></div>';
         $('#placeholder').html(html);
     }catch{}
