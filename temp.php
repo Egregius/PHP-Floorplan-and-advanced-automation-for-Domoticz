3d9433857bbf8a26b45a1d17b46774f95e08839d
Egregius <guy@egregius.be>
2019-07-27 07:30:55 +0200

    Update

--- temp.php
+++ temp.php
@@ -136,7 +136,7 @@ if ($home===true) {
             echo 'No data for last week.<hr>';
             goto enda;
         } else {
-            echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Graph for last week';
+            echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Grafiek laatste 5 dagen.';
         }
         while ($row=$result->fetch_assoc()) {
             $graphhour[]=$row;
