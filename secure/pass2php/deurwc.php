f6e6217b635d7ee9f42abcbf690d0d2906ac02b3
Egregius <guy@egregius.be>
2019-07-28 09:25:55 +0200

    Update

---
+++ secure/pass2php/deurwc.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Pass2PHP
+ * php version 7.3.4-2
+ *
+ * @category Home_Automation
+ * @package  Pass2PHP
+ * @author   Guy Verschuere <guy@egregius.be>
+ * @license  GNU GPLv3
+ * @link     https://egregius.be
+ **/
+if ($status=='Open'&&$d['auto']['s']=='On') {
+	if (past('wc')>5&&$d['wc']['s']=='Off') {
+		sw('wc', 'On');
+	}
+    finkom();
+    fliving();
+    sirene('Deur wc open');
+}
