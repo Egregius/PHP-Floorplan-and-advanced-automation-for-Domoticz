a5344f86357548f8fe9b10e6ae9d6211086eb5eb
Egregius <guy@egregius.be>
2019-08-16 06:55:55 +0200

    Update

--- secure/pass2php/8Hall-1.php
+++ secure/pass2php/8Hall-1.php
@@ -33,3 +33,13 @@ if ($status=='On') {
     }
     huisslapen();
 }
+
+
+<?php
+if ($status=='On') {
+	if (TIME<=strtotime('8:00')||TIME>=strtotime('21:00')) {
+		sl('Badkamer', 40);
+	} else {
+		sl('Badkamer', 100);
+	}
+}
