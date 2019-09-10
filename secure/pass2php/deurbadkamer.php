ceb18e1090bafb1e0af1c079c6c42eba681b50b8
Egregius <guy@egregius.be>
2019-09-10 13:40:56 +0200

    Update

--- secure/pass2php/deurbadkamer.php
+++ secure/pass2php/deurbadkamer.php
@@ -13,7 +13,7 @@ if ($status=='Open'&&$d['auto']['s']) {
     fbadkamer();
 } else {
     if (past('8badkamer-8')>10) {
-        $d['lichtbadkamer']['s']=25;
+        if($d['zon']['s']==0||(TIME>strtotime('5:00')&& TIME<strtotime('10:00'))) $d['lichtbadkamer']['s']=25;
         $d['deurbadkamer']['s']='Closed';
         $d['8badkamer-7']['t']=0;
         $d['badkamervuur1']['t']=0;
