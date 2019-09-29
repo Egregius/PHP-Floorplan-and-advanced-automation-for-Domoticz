b6d3ff1e4dc41370ddf12b5df5e265991e5daba8
Egregius <guy@egregius.be>
2019-09-29 08:05:55 +0200

    Update

--- secure/pass2php/Ralex.php
+++ secure/pass2php/Ralex.php
@@ -1,6 +1,13 @@
 <?php
-if ($status==0||(isset($_REQUEST['status']&&$_REQUEST['status']==0)) {
-	lg('switch picam2 off');
+if (isset($status)&&$status==0) {
+	file_get_contents('http://192.168.2.12/fifo_command.php?cmd=halt');
+    sleep(1);
+    file_get_contents('http://192.168.2.12/fifo_command.php?cmd=halt');
+    sleep(10);
+    sw('picam2plug', 'Off', basename(__FILE__).':'.__LINE__);
+}
+if (isset($_REQUEST['status'])&&$_REQUEST['status']==0) {
+	require '../functions.php';
 	file_get_contents('http://192.168.2.12/fifo_command.php?cmd=halt');
     sleep(1);
     file_get_contents('http://192.168.2.12/fifo_command.php?cmd=halt');
