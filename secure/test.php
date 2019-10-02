790b3cc31a8dc277b44906a332b1a491a1964654
Egregius <guy@egregius.be>
2019-10-02 06:10:56 +0200

    Update

--- secure/test.php
+++ secure/test.php
@@ -13,7 +13,9 @@ $start=microtime(true);
 require 'functions.php';
 echo '<pre>';
 /*-------------------------------------------------*/
-echo strtotime( 'first day of ' . date( 'F Y'));
+$time=$_SERVER['REQUEST_TIME'];
+$period=strftime('%F',$time-86400*30*3);
+echo date('F jS Y H:i:s', strtotime('first day of '.$period));
 
 
 /*---------------------------*/
