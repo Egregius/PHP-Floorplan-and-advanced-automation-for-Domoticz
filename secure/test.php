2b099947a6547a473193ae6a1e1acfbb9fb6e0d3
Egregius <guy@egregius.be>
2019-11-29 13:20:56 +0100

    Update

--- secure/test.php
+++ secure/test.php
@@ -13,6 +13,37 @@ $start=microtime(true);
 require 'functions.php';
 require_once 'gcal/google-api-php-client/vendor/autoload.php';
 
+
+
+
+    $articleText = 'Het is 1 uur. Het wordt tussen 5 en 9 graden met verspreide wolken.';
+    $client = new GuzzleHttp\Client();
+    $requestData = [
+        'input' =>[
+            'text' => $articleText
+        ],
+        'voice' => [
+            'languageCode' => 'en-US',
+            'name' => 'en-US-Wavenet-F'
+        ],
+        'audioConfig' => [
+            'audioEncoding' => 'MP3',
+            'pitch' => 0.00,
+            'speakingRate' => 1.00
+        ]
+    ];
+    try {
+        $response = $client->request('POST', 'https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=' . $googleTTSAPIKey, [
+            'json' => $requestData
+        ]);
+    } catch (Exception $e) {
+        die('Something went wrong: ' . $e->getMessage());
+    }
+    $fileData = json_decode($response->getBody()->getContents(), true);
+    file_put_contents('/var/www/html/sounds/tts.mp3', base64_decode($fileData['audioContent']));
+    
+    
+    
 echo '<pre>';
 $d=fetchdata();
 /*-------------------------------------------------*/
