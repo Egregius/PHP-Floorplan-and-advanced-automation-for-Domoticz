a7372efe9794a9e2e2a2eafa2c605c50050c1f6b
Egregius <guy@egregius.be>
2019-12-10 15:25:55 +0100

    Update

--- secure/functions.php
+++ secure/functions.php
@@ -10,7 +10,7 @@
  * @link     https://egregius.be
  **/
 session_start();
-require '../config.php';
+require '/var/www/config.php';
 $db=new PDO("mysql:host=localhost;dbname=$dbname;",$dbuser,$dbpass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //$d=fetchdata();
