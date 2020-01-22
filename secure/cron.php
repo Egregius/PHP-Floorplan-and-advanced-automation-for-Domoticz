2263b4f88e3dbc609fdc2fead3cebd542e68a571
Egregius <guy@egregius.be>
2020-01-22 12:20:51 +0100

    Update

--- secure/cron.php
+++ secure/cron.php
@@ -41,15 +41,9 @@ if (isset($_REQUEST['tempcontrol'])) {
 	elseif ($x==1) include ('_TC_aircoheating.php');
 	elseif ($x==2) include ('_TC_gasheating.php');
 }
-if (isset($_REQUEST['cron10'])) {
-    include '_cron10.php';
-}
-if (isset($_REQUEST['cron60'])) {
-    include '_cron60.php';
-}
-if (isset($_REQUEST['cron120'])) {
-    include '_cron120.php';
-}
+if (isset($_REQUEST['cron10'])) include '_cron10.php';
+if (isset($_REQUEST['cron60'])) include '_cron60.php';
+if (isset($_REQUEST['cron120'])) include '_cron120.php';
 if (isset($_REQUEST['cron180'])) {
     include '_cron180.php';
     include 'gcal/gcal.php';
@@ -57,18 +51,6 @@ if (isset($_REQUEST['cron180'])) {
     include 'gcal/tobibeitem.php';
     include 'gcal/mirom.php';
 }
-if (isset($_REQUEST['cron240'])) {
-    include '_cron240.php';
-}
-if (isset($_REQUEST['cron300'])) {
-    include '_cron300.php';
-}
-if (isset($_REQUEST['cron3600'])) {
-    include '_cron3600.php';
-}
-if (isset($_REQUEST['weather'])) {
-    include '_weather.php';
-}
-if (isset($_REQUEST['test'])) {
-    include 'gcal/gcal.php';
-}
+if (isset($_REQUEST['cron240'])) include '_cron240.php';
+if (isset($_REQUEST['cron300'])) include '_cron300.php';
+if (isset($_REQUEST['cron3600'])) include '_cron3600.php';
