8ad48dcf895a9835968d28837d495a82eca144f0
Egregius <guy@egregius.be>
2020-02-01 08:00:21 +0100

    Update

--- secure/phpMyAdmin/js/export_output.js
+++
@@ -1,8 +0,0 @@
-AJAX.registerOnload('export_output.js', function () {
-    $(document).on('keydown', function (e) {
-        if ((e.which || e.keyCode) === 116) {
-            e.preventDefault();
-            $('#export_refresh_form').trigger('submit');
-        }
-    });
-});
