2823fc449cdfcc8ebb2aa692f3eaf908645843d5
Egregius <guy@egregius.be>
2020-02-06 06:40:50 +0100

    Update

--- secure/functions.php
+++ secure/functions.php
@@ -644,6 +644,12 @@ function rookmelder($msg){
 	boseplayinfo($msg, 45);
     resetsecurity();
 }
+function ifttt($event, $value1, $value2='', $value3='')
+{
+	global $iftttkey;
+	$reply=file_get_contents('https://maker.ifttt.com/trigger/'.$event.'/with/key/'.$iftttkey.'?value1='.$value1.'&value2='.$value2.'&value3='.$value3);
+	telegram($reply);
+}
 function koekje($user,$expirytime)
 {
     global $cookie,$domainname;
