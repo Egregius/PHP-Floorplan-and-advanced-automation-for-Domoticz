04ab16baece14f9cb8aaa7793e1015bb350fa4a8
Egregius <guy@egregius.be>
2020-03-16 15:45:54 +0100

    Update

--- garmin.php
+++ garmin.php
@@ -12,7 +12,13 @@
  * @link     https://egregius.be
  **/
 require 'secure/functions.php';
-if (isset($_REQUEST['token'])&&$_REQUEST['token']==$garmintoken) {
+if (isset($_REQUEST['token'])&&isset($_REQUEST['action'])&&$_REQUEST['token']==$garmintoken) {
     echo 'OK';
-    
+    if ($_REQUEST['action']=='weg') store('Weg', 2, basename(__FILE__).':'.__LINE__);
+    elseif ($_REQUEST['action']=='thuis') store('Weg', 0, basename(__FILE__).':'.__LINE__);
+    elseif ($_REQUEST['action']=='slapen') store('Weg', 1, basename(__FILE__).':'.__LINE__);
+    elseif ($_REQUEST['action']=='poortrf') {
+    	store('Weg', 0, basename(__FILE__).':'.__LINE__);
+    	sw('poortrf', 'On', basename(__FILE__).':'.__LINE__);
+    }
 }
