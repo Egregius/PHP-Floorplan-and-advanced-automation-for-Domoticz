2c6263d899cc292e20773e4779c67f02943557e7
Egregius <guy@egregius.be>
2020-04-06 07:00:53 +0200

    Update

--- secure/_cron120.php
+++ secure/_cron120.php
@@ -18,27 +18,20 @@ if ($d['auto']['s']=='On') {
 		$x=$y+$x;
 		$windhist=round($x/4, 2);
 	}
-	if (	$d['heating']['s']>=2	&&$d['living_temp']['s']>22&&$d['zon']['s']>3000&&TIME>=strtotime("10:00")) { //Heating
+	if (	$d['heating']['s']<0	&&$d['living_temp']['s']>20&&$d['zon']['s']>2000&&TIME>=strtotime("10:00")) { //Cooling
+		if ($d['wind']['s']>=30) 	 $luifel=0;
+		elseif ($d['wind']['s']>=25) $luifel=25;
+		elseif ($d['wind']['s']>=20) $luifel=30;
+		elseif ($d['wind']['s']>=15) $luifel=35;
+		elseif ($d['wind']['s']>=10) $luifel=40;
+		else $luifel=45;
+	} elseif ($d['heating']['s']==0	&&$d['living_temp']['s']>22&&$d['zon']['s']>3000&&TIME>=strtotime("10:00")) { //Neutral
 		if ($d['wind']['s']>=30) 	 $luifel=0;
 		elseif ($d['wind']['s']>=25) $luifel=25;
 		elseif ($d['wind']['s']>=20) $luifel=30;
 		elseif ($d['wind']['s']>=15) $luifel=30;
 		elseif ($d['wind']['s']>=10) $luifel=30;
 		else $luifel=40;
-	} elseif ($d['heating']['s']==0	&&$d['living_temp']['s']>20&&$d['zon']['s']>2000&&TIME>=strtotime("10:00")) { //Neutral
-		if ($d['wind']['s']>=30) 	 $luifel=0;
-		elseif ($d['wind']['s']>=25) $luifel=25;
-		elseif ($d['wind']['s']>=20) $luifel=30;
-		elseif ($d['wind']['s']>=15) $luifel=30;
-		elseif ($d['wind']['s']>=10) $luifel=30;
-		else $luifel=40;
-	} elseif ($d['heating']['s']==1	&&$d['living_temp']['s']>19&&$d['zon']['s']>200&&TIME>=strtotime("10:00")) { //Cooling
-		if ($d['wind']['s']>=30) 	 $luifel=0;
-		elseif ($d['wind']['s']>=25) $luifel=30;
-		elseif ($d['wind']['s']>=20) $luifel=30;
-		elseif ($d['wind']['s']>=15) $luifel=30;
-		elseif ($d['wind']['s']>=10) $luifel=30;
-		else $luifel=70;
 	} else {
 		$luifel=0;
 	}
@@ -51,7 +44,6 @@ if ($d['auto']['s']=='On') {
 	elseif ($luifel>100) $luifel=100;
 
 
-	echo 'past='.past('luifel').' $d[\'luifel\'][\'s\']='.$d['luifel']['s'].' $luifel='.$luifel.' $d[\'luifel\'][\'m\']='.$d['luifel']['m'].' wind='.$d['wind']['s'].' windhist='.$windhist;
 	if (		past('luifel')>900&&$d['luifel']['s']<$luifel&&$d['luifel']['m']==0&&$d['wind']['s']<$windhist) {
 		sl('luifel', $luifel, basename(__FILE__).':'.__LINE__);
 	} elseif (	past('luifel')>300&&$d['luifel']['s']>$luifel&&$d['luifel']['m']==0) {
