cea4ca61b963b90e62371c2bcc5ce821d35ac7bc
Egregius <guy@egregius.be>
2020-04-28 07:50:57 +0200

    Update

--- secure/cron.php
+++ secure/cron.php
@@ -43,13 +43,13 @@ if (isset($_REQUEST['cron10'])) {
 }
 if (isset($_REQUEST['cron60'])) include '_cron60.php';
 if (isset($_REQUEST['cron120'])) include '_cron120.php';
-if (isset($_REQUEST['cron180'])) {
-    include '_cron180.php';
+if (isset($_REQUEST['cron180'])) include '_cron180.php';
+if (isset($_REQUEST['cron240'])) include '_cron240.php';
+if (isset($_REQUEST['cron300'])) {
+	include '_cron300.php';
     include 'gcal/gcal.php';
     include 'gcal/verlof.php';
     include 'gcal/tobibeitem.php';
     include 'gcal/mirom.php';
 }
-if (isset($_REQUEST['cron240'])) include '_cron240.php';
-if (isset($_REQUEST['cron300'])) include '_cron300.php';
 if (isset($_REQUEST['cron3600'])) include '_cron3600.php';
--- secure/pass2php/$ 8Kamer-5.php
+++ secure/pass2php/$ 8Kamer-5.php
@@ -16,7 +16,9 @@ if ($d['kamer']['m']==2) {
 } elseif ($status=='On') {
     $item='RkamerL';
     if ($d['heating']['s']<0) {
-		if ($d[$item]['s']<82) sl($item, 82, basename(__FILE__).':'.__LINE__);
+		if ($d[$item]['s']<50) sl($item, 50, basename(__FILE__).':'.__LINE__);
+		elseif ($d[$item]['s']<70) sl($item, 70, basename(__FILE__).':'.__LINE__);
+		elseif ($d[$item]['s']<82) sl($item, 82, basename(__FILE__).':'.__LINE__);
 	} else {
 		if ($d[$item]['s']<100) sl($item, 100, basename(__FILE__).':'.__LINE__);
 	}
