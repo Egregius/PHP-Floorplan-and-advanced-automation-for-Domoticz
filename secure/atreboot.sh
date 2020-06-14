32876b59a000b5199707f0d00e2dc4d27c2cf89f
Egregius <guy@egregius.be>
2020-06-15 01:15:53 +0200

    Update

--- secure/atreboot.sh
+++ secure/atreboot.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+service mariadb stop
+rsync -aP /mysqldb/ /temp/mysql/
+service mariadb start
+service domoticz stop
+rsync -aP /domoticzdb/ /temp/domoticzdb/
 touch /temp/domoticz.log
 touch /temp/phperror.log
 mkdir -p /temp/mysql/
 chown mysql:mysql /temp/mysql/
-service mariadb stop
-rsync -aP /mysqldb/ /temp/mysql/
 chmod 666 /temp/*.log
+service domoticz start
