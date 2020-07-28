7a4ac4150d7cf7ea3dbfdfacda598ba46981d0f5
Egregius <guy@egregius.be>
2020-07-28 06:40:52 +0200

    Update

--- floorplan.bose.php
+++ floorplan.bose.php
@@ -32,8 +32,10 @@ if ($home) {
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black">';
     if ($udevice=='iPhone') {
+//        echo '
+//        <meta name="viewport" content="width=device-width,height=device-height,initial-scale=0.655,user-scalable=yes,minimal-ui">';
         echo '
-        <meta name="viewport" content="width=device-width,height=device-height,initial-scale=0.655,user-scalable=yes,minimal-ui">';
+        <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1,user-scalable=yes,minimal-ui">';
     } elseif ($udevice=='iPad') {
         echo '
         <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.2,user-scalable=yes,minimal-ui">';
