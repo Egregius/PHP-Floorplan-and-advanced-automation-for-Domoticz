2ba64ac6795e1252b686801b450061299547d076
Egregius <guy@egregius.be>
2020-08-17 08:40:52 +0200

    Update

--- floorplan.doorsensors.php
+++ floorplan.doorsensors.php
@@ -23,7 +23,10 @@ if ($home) {
 		<meta name="apple-mobile-web-app-status-bar-style" content="black">';
 	if ($udevice=='iPhone') {
 	    echo '
-		<meta name="viewport" content="width=device-width,height=device-height,initial-scale=0.655,user-scalable=yes,minimal-ui"/>';
+		<meta name="viewport" content="width=device-width,height=device-height,initial-scale=0.77,user-scalable=yes,minimal-ui"/>';
+	} elseif ($udevice=='iPhoneSE') {
+	    echo '
+		<meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.4,user-scalable=yes,minimal-ui"/>';
 	} elseif ($udevice=='iPad') {
 	    echo '
 		<meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.2,user-scalable=yes,minimal-ui"/>';
@@ -381,7 +384,7 @@ if ($home) {
         </a>';
     }
 */
-   echo '
+   echo $udevice.'
         <script type="text/javascript">
 			function navigator_Go(url) {window.location.assign(url);}
 		</script>';
