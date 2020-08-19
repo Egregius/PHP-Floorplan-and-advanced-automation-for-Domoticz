417031bce2dbe987fe3461b90e693be930befcd6
Egregius <guy@egregius.be>
2020-08-19 11:25:53 +0200

    Update

--- floorplan.proxmox.php
+++ floorplan.proxmox.php
@@ -12,6 +12,9 @@
 $start=microtime(true);
 require 'secure/functions.php';
 require 'secure/authentication.php';
+require '/var/www/proxmox/vendor/autoload.php';
+use ProxmoxVE\Proxmox;
+
 if ($home) {
 	$db=dbconnect();
     echo '
@@ -50,151 +53,85 @@ if ($home) {
 		    .border_right {border-right:1pt dotted #777;}
         </style>
 	</head>
-	<body>';
-
-	echo '
-		<div class="fix" style="top:0px;right:0px;">
-			<a href=\'javascript:navigator_Go("floorplan.php");\'>
-				<img src="/images/close.png" width="50px" height="50px"/>
+	<body>
+	<div class="fix" style="top:0px;left:0px;height:50px;width:50px;background-color:#CCC">
+			<a href=\'javascript:navigator_Go("floorplan.proxmox.php");\'>
+				<img src="/images/restart.png" width="50px" height="50px"/>
 			</a>
-		</div>
-		<br>
-		<br>
-		<br>
-	    <div class="fix" style="top:82px;left:0px">
-		<table  id="table" cellpadding="2" cellspacing="0">
-		    <thead>
-		        <tr>
-		        	<th rowspan="2" class="border_right">Date</th>
-		        	<th colspan="3" class="border_right">Heat</th>
-		        	<th colspan="3" class="border_right">Cool</th>
-		        	<th rowspan="2" class="border_right">Sum</th>
-		        </tr>
-		        <tr class="border_bottom">
-		            <th>Living</th>
-		            <th>Kamer</th>
-		            <th class="border_right">Alex</th>
-		            <th>Living</th>
-		            <th>Kamer</th>
-		            <th class="border_right">Alex</th>
-		        </tr>
-		    </thead>
-		    <tbody>';
-	$d=fetchdata();
-    $sql="SELECT * FROM `daikin` ORDER BY `date` DESC LIMIT 0,30";
-    if (!$result=$db->query($sql)){die('There was an error running the query ['.$sql.'-'.$db->error.']');}
-    $livingheat=0;
-    $kamerheat=0;
-    $alexheat=0;
-    $livingcool=0;
-    $kamercool=0;
-    $alexcool=0;
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        echo '
-        <tr class="border_bottom">
-        	<td nowrap class="border_right">'.$row['date'].'</td>
-        	<td>'.($row['livingheat']>1?number_format($row['livingheat']*0.1, 1, ',', ''):'').'</td>
-        	<td>'.($row['kamerheat']>1?number_format($row['kamerheat']*0.1, 1, ',', ''):'').'</td>
-        	<td class="border_right">'.($row['alexheat']>1?number_format($row['alexheat']*0.1, 1, ',', ''):'').'</td>
-        	<td>'.($row['livingcool']>1?number_format($row['livingcool']*0.1, 1, ',', ''):'').'</td>
-        	<td>'.($row['kamercool']>1?number_format($row['kamercool']*0.1, 1, ',', ''):'').'</td>
-        	<td class="border_right">'.($row['alexcool']>1?number_format($row['alexcool']*0.1, 1, ',', ''):'').'</td>
-        	<td class="border_right">'.(($row['livingheat']+$row['kamerheat']+$row['alexheat']+$row['livingcool']+$row['kamercool']+$row['alexcool'])>0?number_format(($row['livingheat']+$row['kamerheat']+$row['alexheat']+$row['livingcool']+$row['kamercool']+$row['alexcool'])*0.1, 1, ',', ''):'').'</td>
-        </tr>';
-        $livingheat=$livingheat+$row['livingheat'];
-        $kamerheat=$kamerheat+$row['kamerheat'];
-        $alexheat=$alexheat+$row['alexheat'];
-        $livingcool=$livingcool+$row['livingcool'];
-        $kamercool=$kamercool+$row['kamercool'];
-        $alexcool=$alexcool+$row['alexcool'];
-    }
-    echo '
-        </tbody>
-        <tfoot>
-        	<tr>
-        		<th class="border_right border_bottom">Sum</th>
-        		<th>'.($livingheat>1?number_format($livingheat*0.1, 1, ',', ''):'').'</th>
-        		<th>'.($kamerheat>1?number_format($kamerheat*0.1, 1, ',', ''):'').'</th>
-        		<th class="border_right">'.($alexheat>1?number_format($alexheat*0.1, 1, ',', ''):'').'</th>
-        		<th>'.($livingcool>1?number_format($livingcool*0.1, 1, ',', ''):'').'</th>
-        		<th>'.($kamercool>1?number_format($kamercool*0.1, 1, ',', ''):'').'</th>
-        		<th class="border_right">'.($alexcool>1?number_format($alexcool*0.1, 1, ',', ''):'').'</th>
-        	</tr>
-        	<tr>
-        		<th class="border_right border_bottom">Total</th>
-        		<th colspan="6" class="border_right border_bottom">'.number_format(($livingheat+$kamerheat+$alexheat+$livingcool+$kamercool+$alexcool)*0.1, 1, ',', '') .'</th>
-        	<tr>
-        </tfoot>
-    </table>
-    <br>
-    <br>
-    <table  id="table" cellpadding="2" cellspacing="0">
-		    <thead>
-		        <tr>
-		        	<th rowspan="2" class="border_right">Month</th>
-		        	<th colspan="3" class="border_right">Heat</th>
-		        	<th colspan="3" class="border_right">Cool</th>
-		        	<th rowspan="2" class="border_right">Sum</th>
-		        </tr>
-		        <tr class="border_bottom">
-		            <th>Living</th>
-		            <th>Kamer</th>
-		            <th class="border_right">Alex</th>
-		            <th>Living</th>
-		            <th>Kamer</th>
-		            <th class="border_right">Alex</th>
-		        </tr>
-		    </thead>
-		    <tbody>';
-    $sql="SELECT LEFT(date,7) as date, SUM(livingheat) AS livingheat, SUM(kamerheat) AS kamerheat, SUM(alexheat) AS alexheat, SUM(livingcool) AS livingcool, SUM(kamercool) AS kamercool, SUM(alexcool) AS alexcool FROM `daikin` GROUP BY LEFT(date, 7) ORDER BY `date` DESC LIMIT 0,30";
-    if (!$result=$db->query($sql)){die('There was an error running the query ['.$sql.'-'.$db->error.']');}
-    $livingheat=0;
-    $kamerheat=0;
-    $alexheat=0;
-    $livingcool=0;
-    $kamercool=0;
-    $alexcool=0;
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        echo '
-        <tr class="border_bottom">
-        	<td nowrap class="border_right">'.$row['date'].'</td>
-        	<td>'.($row['livingheat']>1?number_format($row['livingheat']*0.1, 1, ',', ''):'').'</td>
-        	<td>'.($row['kamerheat']>1?number_format($row['kamerheat']*0.1, 1, ',', ''):'').'</td>
-        	<td class="border_right">'.($row['alexheat']>1?number_format($row['alexheat']*0.1, 1, ',', ''):'').'</td>
-        	<td>'.($row['livingcool']>1?number_format($row['livingcool']*0.1, 1, ',', ''):'').'</td>
-        	<td>'.($row['kamercool']>1?number_format($row['kamercool']*0.1, 1, ',', ''):'').'</td>
-        	<td class="border_right">'.($row['alexcool']>1?number_format($row['alexcool']*0.1, 1, ',', ''):'').'</td>
-        	<td class="border_right">'.(($row['livingheat']+$row['kamerheat']+$row['alexheat']+$row['livingcool']+$row['kamercool']+$row['alexcool'])>0?number_format(($row['livingheat']+$row['kamerheat']+$row['alexheat']+$row['livingcool']+$row['kamercool']+$row['alexcool'])*0.1, 1, ',', ''):'').'</td>   		
-        </tr>';
-        $livingheat=$livingheat+$row['livingheat'];
-        $kamerheat=$kamerheat+$row['kamerheat'];
-        $alexheat=$alexheat+$row['alexheat'];
-        $livingcool=$livingcool+$row['livingcool'];
-        $kamercool=$kamercool+$row['kamercool'];
-        $alexcool=$alexcool+$row['alexcool'];
-    }
-    echo '
-        </tbody>
-        <tfoot>
-        	<tr>
-        		<th class="border_right">Sum</th>
-        		<th>'.($livingheat>0.1?number_format($livingheat*0.1, 1, ',', ''):'').'</th>
-        		<th>'.($kamerheat>0.1?number_format($kamerheat*0.1, 1, ',', ''):'').'</th>
-        		<th class="border_right">'.($alexheat>0.1?number_format($alexheat*0.1, 1, ',', ''):'').'</th>
-        		<th>'.($livingcool>0.1?number_format($livingcool*0.1, 1, ',', ''):'').'</th>
-        		<th>'.($kamercool>0.1?number_format($kamercool*0.1, 1, ',', ''):'').'</th>
-        		<th class="border_right">'.($alexcool>0.1?number_format($alexcool*0.1, 1, ',', ''):'').'</th>
-        	</tr>
-        	<tr>
-        		<th rowspan="2" class="border_right border_bottom">Total</th>
-        		<th colspan="3" class="border_right border_bottom">'.number_format(($livingheat+$kamerheat+$alexheat)*0.1, 1, ',', '') .'</th>
-        		<th colspan="3" class="border_right border_bottom">'.number_format(($livingcool+$kamercool+$alexcool)*0.1, 1, ',', '') .'</th>
-        	</tr>
-        	<tr>
-        		<th colspan="6" class="border_right border_bottom">'.number_format(($livingheat+$kamerheat+$alexheat+$livingcool+$kamercool+$alexcool)*0.1, 1, ',', '') .'</th>
-        	<tr>
-        </tfoot>
-    </table>';
+		</div>';
+
+$proxmox = new Proxmox($proxmoxcredentials);
+
+if (isset($_POST['vmid'])&&isset($_POST['action'])) {
+	$proxmox->create('/nodes/proxmox/qemu/'.$_POST['vmid'].'/status/'.$_POST['action']);
+}
+
+$data = $proxmox->get('/nodes/proxmox/qemu');
+
+	  echo '
+	  <style>
+	  	table{border-collapse: collapse; }
+	  	tr{border:1px solid grey;}
+	  	th, td{border:1px solid grey;padding:5px;}
+	  	.right{text-align:right;}
+	  </style>
+	  <table>
+	  	<thead>
+	  		<tr>
+	  			<th>id</th>
+	  			<th>Name</th>
+	  			<th>Status</th>
+	  			<th>Days up</th>
+	  			<th>Hours</th>
+	  			<th>CPU</th>
+	  			<th>Memory</th>
+	  			<th>netin</th>
+	  			<th>netout</th>
+	  			<th>Action</th>
+	  		</tr>
+	  	</thead>
+	  	<tbody>';
+		foreach ($data as $node) {
+			uasort($node, "cmp");
+			foreach ($node as $vm) {
+				echo '
+				<tr>
+					<td>'.$vm['vmid'] .'</td>
+					<td>'.substr($vm['name'], 2).'</td>
+					<td>'.$vm['status'].'</td>
+					<td class="right">'.floor($vm['uptime']/86400).'d</td>
+					<td class="right">'.gmdate("G:i", ($vm['uptime']%86400)).'</td>
+					<td class="right">'.number_format($vm['cpu'], 2).'</td>
+					<td class="right">'.human_filesize($vm['mem']).'/'.human_filesize($vm['maxmem']).'</td>
+					<td class="right">'.human_filesize($vm['netin']).'</td>
+					<td class="right">'.human_filesize($vm['netout']).'</td>
+					<td>
+						<form method="POST">
+							<input type="hidden" name="vmid" value="'.$vm['vmid'].'"/>';
+				if($vm['status']=='stopped') {
+					echo '
+							<input type="submit" name="action" value="start">';
+				} else {
+					echo '
+							<input type="submit" name="action" value="stop">
+							<input type="submit" name="action" value="shutdown">';
+				}
+				echo '
+						</form>
+					</td>
+				</tr>';
+			}
+		}
+	  echo '
+	  	</tbody>
+	  </table>';
+
+
+
+	
+}
+function cmp($a, $b) {
+    return strcmp($a['name'], $b['name']);
 }
 ?>
     </body>
