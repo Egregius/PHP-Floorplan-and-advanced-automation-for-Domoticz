7b275a3dfc3f2484fd048e9b108f012864b1b43e
Egregius <guy@egregius.be>
2020-09-07 17:00:54 +0200

    Update

--- secure/test.php
+++ secure/test.php
@@ -15,8 +15,8 @@ require 'functions.php';
 
 
 //echo shell_exec('echo ok | /var/www/html/secure/lgtv.py -c current-app 192.168.2.27');
-echo exec('sudo /var/www/html/secure/lgtv.py -c volume-up 192.168.2.27');
-
+echo exec('sudo /var/www/html/secure/lgtv.py -c volume-down 192.168.2.27');
+echo exec('sudo /var/www/html/secure/lgtv.py -c set-input -a HDMI_1 192.168.2.27');
 
 /*-------------------------------------------------*/
 //require_once 'gcal/google-api-php-client/vendor/autoload.php';
