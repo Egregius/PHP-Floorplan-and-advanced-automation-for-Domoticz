b238d6a454bac8c908fb697ac5137ea5ef56c0a1
Egregius <guy@egregius.be>
2020-09-27 06:50:52 +0200

    Update

--- secure/pass2php/badkamer_temp.php
+++ secure/pass2php/badkamer_temp.php
@@ -13,54 +13,13 @@ $n='badkamer';
 if ($status>$d[$n.'_temp']['s']+0.2) $status=$d[$n.'_temp']['s']+0.2;
 elseif ($status<$d[$n.'_temp']['s']-0.2) $status=$d[$n.'_temp']['s']-0.2;
 $db=new PDO("mysql:host=localhost;dbname=$dbname;",$dbuser,$dbpass);
-$result=$db->query("SELECT AVG(temp) as AVG FROM (SELECT $n as temp FROM `temp` ORDER BY `temp`.`stamp` DESC LIMIT 0,30) as A");
+$result=$db->query("SELECT AVG(temp) as AVG FROM (SELECT $n as temp FROM `temp` ORDER BY `temp`.`stamp` DESC LIMIT 0,15) as A");
 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
 	$avg=$row['AVG'];
 }
-if ($status>=$avg+0.5) {
-	if ($d[$n.'_temp']['icon']!='red5') {
-		storeicon($n.'_temp', 'red5', basename(__FILE__).':'.__LINE__);
-	}
-} elseif ($status>=$avg+0.4) {
-    if ($d[$n.'_temp']['icon']!='red4') {
-    	storeicon($n.'_temp', 'red4', basename(__FILE__).':'.__LINE__);
-    }
-} elseif ($status>=$avg+0.3) {
-    if ($d[$n.'_temp']['icon']!='red3') {
-    	storeicon($n.'_temp', 'red3', basename(__FILE__).':'.__LINE__);
-    }
-} elseif ($status>=$avg+0.2) {
-    if ($d[$n.'_temp']['icon']!='red') {
-    	storeicon($n.'_temp', 'red', basename(__FILE__).':'.__LINE__);
-    }
-} elseif ($status>=$avg+0.1) {
-    if ($d[$n.'_temp']['icon']!='up') {
-    	storeicon($n.'_temp', 'up', basename(__FILE__).':'.__LINE__);
-    }
-} elseif ($status<=$avg-0.5) {
-    if ($d[$n.'_temp']['icon']!='blue5') {
-    	storeicon($n.'_temp', 'blue5', basename(__FILE__).':'.__LINE__);
-    }
-} elseif ($status<=$avg-0.4) {
-    if ($d[$n.'_temp']['icon']!='blue4') {
-    	storeicon($n.'_temp', 'blue4', basename(__FILE__).':'.__LINE__);
-    }
-} elseif ($status<=$avg-0.3) {
-    if ($d[$n.'_temp']['icon']!='blue3') {
-    	storeicon($n.'_temp', 'blue3', basename(__FILE__).':'.__LINE__);
-    }
-} elseif ($status<=$avg-0.2) {
-    if ($d[$n.'_temp']['icon']!='blue') {
-    	storeicon($n.'_temp', 'blue', basename(__FILE__).':'.__LINE__);
-    }
-} elseif ($status<=$avg-0.1) {
-    if ($d[$n.'_temp']['icon']!='down') {
-    	storeicon($n.'_temp', 'down', basename(__FILE__).':'.__LINE__);
-    }
-} else {
-	if ($d[$n.'_temp']['icon']!='') {
-		storeicon($n.'_temp', '', basename(__FILE__).':'.__LINE__);
-	}
+$diff=$status-$avg;
+if ($d[$n.'_temp']['icon']!=$diff) {
+	storeicon($n.'_temp', $diff, basename(__FILE__).':'.__LINE__);
 }
 
 $prev=$d[$n.'_temp']['s'];
--- secure/pass2php/zolder_temp.php
+++ secure/pass2php/zolder_temp.php
@@ -13,32 +13,13 @@ $n='zolder';
 if ($status>$d[$n.'_temp']['s']+0.2) $status=$d[$n.'_temp']['s']+0.2;
 elseif ($status<$d[$n.'_temp']['s']-0.2) $status=$d[$n.'_temp']['s']-0.2;
 $db=new PDO("mysql:host=localhost;dbname=$dbname;",$dbuser,$dbpass);
-$result=$db->query("SELECT AVG(temp) as AVG FROM (SELECT $n as temp FROM `temp` ORDER BY `temp`.`stamp` DESC LIMIT 0,30) as A");
+$result=$db->query("SELECT AVG(temp) as AVG FROM (SELECT $n as temp FROM `temp` ORDER BY `temp`.`stamp` DESC LIMIT 0,15) as A");
 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
 	$avg=$row['AVG'];
 }
-if ($status>=$avg+0.5) {
-	storeicon($n.'_temp', 'red5', basename(__FILE__).':'.__LINE__);
-} elseif ($status>=$avg+0.4) {
-    storeicon($n.'_temp', 'red4', basename(__FILE__).':'.__LINE__);
-} elseif ($status>=$avg+0.3) {
-    storeicon($n.'_temp', 'red3', basename(__FILE__).':'.__LINE__);
-} elseif ($status>=$avg+0.2) {
-    storeicon($n.'_temp', 'red', basename(__FILE__).':'.__LINE__);
-} elseif ($status>=$avg+0.1) {
-    storeicon($n.'_temp', 'up', basename(__FILE__).':'.__LINE__);
-} elseif ($status<=$avg-0.5) {
-    storeicon($n.'_temp', 'blue5', basename(__FILE__).':'.__LINE__);
-} elseif ($status<=$avg-0.4) {
-    storeicon($n.'_temp', 'blue4', basename(__FILE__).':'.__LINE__);
-} elseif ($status<=$avg-0.3) {
-    storeicon($n.'_temp', 'blue3', basename(__FILE__).':'.__LINE__);
-} elseif ($status<=$avg-0.2) {
-    storeicon($n.'_temp', 'blue', basename(__FILE__).':'.__LINE__);
-} elseif ($status<=$avg-0.1) {
-    storeicon($n.'_temp', 'down', basename(__FILE__).':'.__LINE__);
-} else {
-	storeicon($n.'_temp', '', basename(__FILE__).':'.__LINE__);
+$diff=$status-$avg;
+if ($d[$n.'_temp']['icon']!=$diff) {
+	storeicon($n.'_temp', $diff, basename(__FILE__).':'.__LINE__);
 }
 $d[$n.'_temp']['s']=$status;
 require '_verwarming.php';
