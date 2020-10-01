6a9a2c61822156c16c2a67740bbf0d61d61e8ad2
Egregius <guy@egregius.be>
2020-10-01 13:10:56 +0200

    Update

--- ajax.php
+++ ajax.php
@@ -186,7 +186,7 @@ if ($home==true) {
 					if ($d[$i]['s']!=$_REQUEST['action']) sl($i, $_REQUEST['action'], basename(__FILE__).':'.__LINE__);
 				}
 			} elseif ($_REQUEST['device']=='tv') {
-				if ($d['Rliving']['s']<34) sl('Rliving', 34, basename(__FILE__).':'.__LINE__);
+				if ($d['Rliving']['s']<30) sl('Rliving', 30, basename(__FILE__).':'.__LINE__);
 				if ($d['Rbureel']['s']<70) sl('Rbureel', 70, basename(__FILE__).':'.__LINE__);
 				if ($d['RkeukenL']['s']<55) sl('RkeukenL', 55, basename(__FILE__).':'.__LINE__);
 				if ($d['RkeukenR']['s']<55) sl('RkeukenR', 55, basename(__FILE__).':'.__LINE__);
