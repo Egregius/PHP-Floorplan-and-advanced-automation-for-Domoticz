17df904edeee6b16d9323d868334ce804162a743
Egregius <guy@egregius.be>
2020-10-03 06:10:56 +0200

    Update

--- iftttring.php
+++ iftttring.php
@@ -12,18 +12,18 @@
 require 'secure/functions.php';
 if (isset($_REQUEST['token'])&&$_REQUEST['token']==$ifttttoken) {
 	$d=fetchdata();
-	if (isset($_REQUEST['action'])&&$_REQUEST['action']=='motion'&&$d['Weg']['s']==0) {
+	if (isset($_REQUEST['action'])&&$_REQUEST['action']=='motion') {
 		echo 'Motion';
 		if ($d['zon']['s']==0) {
 			sw('voordeur', 'On', basename(__FILE__).':'.__LINE__);
 		}
-		shell_exec('curl -s "http://192.168.2.11/telegram.php?action=beweging" > /dev/null 2>/dev/null &');
-		shell_exec('curl -s "http://192.168.2.13/telegram.php?action=beweging" > /dev/null 2>/dev/null &');
+		shell_exec('curl -s "http://192.168.2.11/telegram.php?action=Beweging" > /dev/null 2>/dev/null &');
+		shell_exec('curl -s "http://192.168.2.13/telegram.php?action=Beweging" > /dev/null 2>/dev/null &');
 		shell_exec('curl -s "http://192.168.2.11/fifo_command.php?cmd=record%20on%2015%2055" > /dev/null 2>/dev/null &');
 		shell_exec('curl -s "http://192.168.2.13/fifo_command.php?cmd=record%20on%2015%2055" > /dev/null 2>/dev/null &');
 		if ($d['Weg']['s']==0) {
 			if ($d['lgtv']['s']=='On') {
-			    shell_exec('python3 secure/lgtv.py -c send-message -a "Beweging voordeur" 192.168.2.27');
+			    shell_exec('python3 secure/lgtv.py -c send-message -a "Beweging Ring" 192.168.2.27');
 			}
 			if (past('Xbel')>60) {
 				if ($d['Xvol']['s']!=5) {
@@ -32,13 +32,13 @@ if (isset($_REQUEST['token'])&&$_REQUEST['token']==$ifttttoken) {
 				sl('Xbel', 30, basename(__FILE__).':'.__LINE__);
 			}
 		}
-	} else {
+	} elseif (isset($_REQUEST['action'])&&$_REQUEST['action']=='doorbell') {
 		echo 'Doorbell';
 		if ($d['zon']['s']==0) {
 			sw('voordeur', 'On', basename(__FILE__).':'.__LINE__);
 		}
-		shell_exec('curl -s "http://192.168.2.11/telegram.php?action=deurbel" > /dev/null 2>/dev/null &');
-		shell_exec('curl -s "http://192.168.2.13/telegram.php?action=deurbel" > /dev/null 2>/dev/null &');
+		shell_exec('curl -s "http://192.168.2.11/telegram.php?action=DEURBEL" > /dev/null 2>/dev/null &');
+		shell_exec('curl -s "http://192.168.2.13/telegram.php?action=DEURBEL" > /dev/null 2>/dev/null &');
 		shell_exec('curl -s "http://192.168.2.11/fifo_command.php?cmd=record%20on%2015%2055" > /dev/null 2>/dev/null &');
 		shell_exec('curl -s "http://192.168.2.13/fifo_command.php?cmd=record%20on%2015%2055" > /dev/null 2>/dev/null &');
 		telegram('Deurbel', true, 2);
