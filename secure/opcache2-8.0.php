808a0021e3938244fe903d1d60ba3a1c5fc501e8
root <root@domoticz.home.egregius.be>
2020-11-15 09:41:07 +0100

    Update

--- secure/opcache2-8.0.php
+++ secure/opcache2-8.0.php
@@ -389,7 +389,7 @@ $opcache = (new Service($options))->handle();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <title>OPcache statistics on <?= $opcache->getData('version', 'host'); ?></title>
+    <title>8.0 OPcache statistics on <?= $opcache->getData('version', 'host'); ?></title>
     <script src="//unpkg.com/react@16/umd/react.development.js" crossorigin></script>
     <script src="//unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
     <script src="//unpkg.com/axios/dist/axios.min.js" crossorigin></script>
