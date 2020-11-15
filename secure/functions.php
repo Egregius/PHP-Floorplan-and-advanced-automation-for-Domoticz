8088f8af9f7889d347b52e6645b654b2463c09e2
root <root@domoticz.home.egregius.be>
2020-11-15 10:26:07 +0100

    Update

--- secure/functions.php
+++ secure/functions.php
@@ -1024,7 +1024,7 @@ function fbadkamer()
 function fkeuken()
 {
 	global $d;
-	if (TIME<strtotime('20:00')&&$d['Weg']['s']==0&&$d['keuken']['s']=='Off'&&$d['wasbak']['s']=='Off'&&$d['werkblad1']['s']=='Off'&&$d['kookplaat']['s']=='Off'&&($d['zon']['s']==0||($d['RkeukenL']['s']>70&&$d['RkeukenR']['s']>70))) {
+	if (TIME<strtotime('20:00')&&$d['Weg']['s']==0&&$d['keuken']['s']=='Off'&&$d['wasbak']['s']=='Off'&&$d['werkblad1']['s']=='Off'&&$d['kookplaat']['s']=='Off'&&($d['zon']['s']>=0||($d['RkeukenL']['s']>70&&$d['RkeukenR']['s']>70))) {
 		sw('keuken', 'On', basename(__FILE__).':'.__LINE__);
 	} elseif (TIME>=strtotime('20:00')&&$d['Weg']['s']==0&&$d['keuken']['s']=='Off'&&$d['wasbak']['s']=='Off'&&$d['werkblad1']['s']=='Off'&&$d['kookplaat']['s']=='Off'&&($d['zon']['s']==0||($d['RkeukenL']['s']>70&&$d['RkeukenR']['s']>70))) {
 		if ($d['jbl']['s']=='Off') {
