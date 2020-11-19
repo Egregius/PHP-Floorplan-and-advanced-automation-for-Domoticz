eeb73ee33171b91ae660b35c63226e5359626fc5
root <root@domoticz.home.egregius.be>
2020-11-19 10:10:52 +0100

    Update

--- secure/preload.php
+++ secure/preload.php
@@ -6,7 +6,5 @@ $fullTree = new RecursiveIteratorIterator($directory);
 $phpFiles = new RegexIterator($fullTree, '/.+((?<!Test)+\.php$)/i', RecursiveRegexIterator::GET_MATCH);
 
 foreach ($phpFiles as $key => $file) {
-	echo $key.' ';
-	print_r($file, true);
-	echo '<br>';
+	if (opcache_compile_file($key)) echo $key.' OK<br>'; else echo $key.' FAILED<br>';
 }
