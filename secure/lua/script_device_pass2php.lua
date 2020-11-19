a7b49917997567df01103864f7a319a8f4950870
root <root@domoticz.home.egregius.be>
2020-11-19 15:20:51 +0100

    Update

--- secure/lua/script_device_pass2php.lua
+++ secure/lua/script_device_pass2php.lua
@@ -1,5 +1,5 @@
 for d,s in pairs(devicechanged)
 do
-os.execute('curl -X POST -d "d='..d.."&s="..s..'" http://127.0.0.1/secure/pass2php.php &')
+os.execute('wget -O /dev/null -o /dev/null "http://127.0.0.1/secure/pass2php.php?d='..d..'&s='..s..'" &')
 end
 commandArray={}
