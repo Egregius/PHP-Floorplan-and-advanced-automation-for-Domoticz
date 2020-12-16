e7bf7372c192174ed9f47192833beeefd28823d7
guy@egregius.be <guy@egregius.be>
2020-12-16 14:00:51 +0100

    Update

--- secure/ringdoorbell.php
+++ secure/ringdoorbell.php
@@ -15,10 +15,10 @@ if (isset($_REQUEST['token'])&&$_REQUEST['token']==$ifttttoken) {
 	if (isset($_REQUEST['RING'])) { // PYTHON
 		echo __LINE__.'<br>';
 		$last=apcu_fetch($_REQUEST['RING']);
-		$new=ceil($_REQUEST['time']);
+		$new=$_REQUEST['id'];
 		echo 'last='.$last.'<br>';
 		echo 'new='.$new.'<br>';
-		if ($last!=$new&&$new>($last+60)) {
+		if ($last!=$new) {
 			echo __LINE__.'<br>';
 			apcu_store($_REQUEST['RING'], $new);
 			if ($_REQUEST['RING']=='motion') {
@@ -39,7 +39,7 @@ if (isset($_REQUEST['token'])&&$_REQUEST['token']==$ifttttoken) {
 				belknop();
 				lg('PYTHON Deurbel');
 			}
-			//telegram($_REQUEST['RING'].PHP_EOL.'python');
+			telegram('Python Ring Doorbell'.PHP_EOL.$_REQUEST['RING'].PHP_EOL.$_REQUEST['source']);
 		}
 		if ($_REQUEST['battery']<60) {
 			echo __LINE__.'<br>';
@@ -52,37 +52,6 @@ if (isset($_REQUEST['token'])&&$_REQUEST['token']==$ifttttoken) {
 	                        );
                         }
                 }
-	} elseif (isset($_REQUEST['ring'])&&$_REQUEST['ring']=='Beweging') { //IFTTT
-		$last=apcu_fetch('motion');
-		$split = preg_split('/[\ \n\,]+/', trim($_REQUEST['time']));
-		$new=strtotime($split[1].' '.$split[0].' '.$split[2].' '.$split[4]);
-		if ($last!=$new&&$new>($last+60)) {
-			apcu_store('motion', $new);
-			if ($d['zon']['s']==0&&(TIME<$d['Sun']['s']||TIME>$d['Sun']['m'])) {
-				sw('voordeur', 'On', basename(__FILE__).':'.__LINE__);
-			}
-			if ($d['poortrf']['s']=='Off'&&$d['deurvoordeur']['s']=='Closed'&&past('deurvoordeur')>90&&past('poortrf')>90) {
-				shell_exec('wget -O /dev/null -o /dev/null "http://192.168.2.11/telegram.php?ringbeweging" > /dev/null 2>/dev/null &');
-				shell_exec('wget -O /dev/null -o /dev/null "http://192.168.2.13/telegram.php?ringbeweging" > /dev/null 2>/dev/null &');
-				shell_exec('wget -O /dev/null -o /dev/null "http://192.168.2.11/fifo_command.php?cmd=record%20on%205%2055" > /dev/null 2>/dev/null &');
-				shell_exec('wget -O /dev/null -o /dev/null "http://192.168.2.13/fifo_command.php?cmd=record%20on%205%2055" > /dev/null 2>/dev/null &');
-			}
-			//telegram($_REQUEST['ring'].PHP_EOL.'ifttt');
-		}
-		
-	} elseif (isset($_REQUEST['ring'])&&$_REQUEST['ring']=='DEURBEL') { //IFTTT
-		$last=apcu_fetch('ding');
-		$split = preg_split('/[\ \n\,]+/', trim($_REQUEST['time']));
-		$new=strtotime($split[1].' '.$split[0].' '.$split[2].' '.$split[4]);
-		if ($last!=$new) {
-			if ($new>($last+60)) {
-				apcu_store('ding', $new);
-				belknop();
-				lg('IFTTT Deurbel');
-				telegram($_REQUEST['ring'].PHP_EOL.'ifttt');
-			}
-		}
-		
 	}
 }
 
