55bb90d523fdf1918a255ae98a14bb0d3df0798f
guy@egregius.be <guy@egregius.be>
2020-12-31 10:30:50 +0100

    Update

--- secure/adminer/index.php
+++ secure/adminer/index.php
@@ -1,32 +1,10 @@
 <?php
 function adminer_object() {
-	// required to run any plugin
 	include_once "./plugins/plugin.php";
-
-	// autoloader
-	foreach (glob("plugins/*.php") as $filename) {
-		include_once "./$filename";
-	}
-
-	$plugins = array(
-		// specify enabled plugins here
-		new AdminerDumpXml,
-		new AdminerTinymce,
-		new AdminerFileUpload("data/"),
-		new AdminerSlugify,
-		new AdminerTranslation,
-		new AdminerForeignSystem,
-	);
-
-	/* It is possible to combine customization and plugins:
-	class AdminerCustomization extends AdminerPlugin {
-	}
-	return new AdminerCustomization($plugins);
-	*/
-
-	return new AdminerPlugin($plugins);
+	include_once "./plugins/login-password-less.php";
+	return new AdminerPlugin(array(
+		// TODO: inline the result of password_hash() so that the password is not visible in source codes
+		new AdminerLoginPasswordLess(password_hash("4A7zKLXL3Mtf2CcKA2uHNrF9ow72WspMKKbAK3pBvXto4v2FZTpET3974WeGRpfqaEmNpeDfbD78M4BiUYziwEsND4dCBvSThgpT", PASSWORD_DEFAULT)),
+	));
 }
-
-// include original Adminer or Adminer Editor
 include "./adminer.php";
-?>
