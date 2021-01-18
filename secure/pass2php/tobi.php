bd4235040f8e92f5d70e4527789fcd7463471ccf
guy@egregius.be <guy@egregius.be>
2021-01-18 10:50:52 +0100

    Update

--- secure/pass2php/tobi.php
+++ secure/pass2php/tobi.php
@@ -11,9 +11,9 @@
  **/
 if ($status>0) {
 	if ($d['tobi']['m']!=0&&TIME>strtotime('6:00')&&TIME<strtotime('12:00')) {
-	    storemode('tobi', 0, basename(__FILE__).':'.__LINE__);
+		storemode('tobi', 0, basename(__FILE__).':'.__LINE__);
 	}
-	if (TIME>strtotime('6:00')&&TIME<strtotime('10:00')) {
+	if (TIME>strtotime('6:00')&&TIME<strtotime('10:00')&&$d['deuralex']['s']=='Open') {
 		if ($d['Rtobi']['s']>0) {
 			sl('Rtobi', 0, basename(__FILE__).':'.__LINE__);
 		}
