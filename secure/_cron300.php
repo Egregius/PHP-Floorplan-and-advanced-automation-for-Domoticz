d9c9d9fcdee5a67f05c20a422075a12539a97cef
guy@egregius.be <guy@egregius.be>
2021-01-30 09:40:52 +0100

    Update

--- secure/_cron300.php
+++ secure/_cron300.php
@@ -27,16 +27,16 @@ $stmt=$db->query("SELECT SUM(`buien`) AS buien FROM regen;");
 while ($row=$stmt->fetch(PDO::FETCH_ASSOC)) $rainpast=$row['buien'];
 if ($rainpast==0) $rainpast=1;
 if ($d['minmaxtemp']['m'] > -3) {
-	if ($rainpast>25000) $pomppauze=3600;
-	elseif ($rainpast>22000) $pomppauze=5400;
-	elseif ($rainpast>19000) $pomppauze=7200;
-	elseif ($rainpast>16000) $pomppauze=10800;
-	elseif ($rainpast>13000) $pomppauze=21600;
-	elseif ($rainpast>10000) $pomppauze=43200;
-	elseif ($rainpast>7000) $pomppauze=86400;
-	elseif ($rainpast>3000) $pomppauze=129600;
-	elseif ($rainpast>1000) $pomppauze=259200;
-	else $pomppauze=2592000;
+	if ($rainpast>25000) $pomppauze=3590;
+	elseif ($rainpast>22000) $pomppauze=5390;
+	elseif ($rainpast>19000) $pomppauze=7190;
+	elseif ($rainpast>16000) $pomppauze=10790;
+	elseif ($rainpast>13000) $pomppauze=21590;
+	elseif ($rainpast>10000) $pomppauze=43190;
+	elseif ($rainpast>7000) $pomppauze=86390;
+	elseif ($rainpast>3000) $pomppauze=129590;
+	elseif ($rainpast>1000) $pomppauze=259190;
+	else $pomppauze=2591990;
 	if ($d['regenpomp']['s']=='Off'&&past('regenpomp')>=$pomppauze) sw('regenpomp', 'On', basename(__FILE__).':'.__LINE__.' '.'Pomp pauze = '.$pomppauze.', maxtemp = '.$d['minmaxtemp']['m'].'°C, rainpast = '.$rainpast);
 }
 
