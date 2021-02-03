838b2ad66d3d8254fe92eed6996212420af0c33d
guy@egregius.be <guy@egregius.be>
2021-02-03 11:30:52 +0100

    Update

--- Smappee.php
+++ Smappee.php
@@ -98,20 +98,28 @@ if ($home===true) {
 	elseif ($udevice=='iPhone') {$args['width']=360;$args['height']=240;}
 	elseif ($udevice=='Mac') {$args['width']=460;$args['height']=300;}
 	else {$args['width']=460;$args['height']=200;}
-	$query="SELECT DATE_FORMAT(timestamp, '%W %H:%i') as timestamp,consumption,solar,alwaysOn,gridImport,gridExport,selfConsumption,selfSufficiency from `smappee_kwartaal` ORDER BY timestamp ASC";
+	for($j=2017;$j<=strftime("%Y",$time);$j++){
+		$months=array('01'=>'Januari','02'=>'Februari','03'=>'Maart','04'=>'April','05'=>'Mei','06'=>'Juni','07'=>'Juli','08'=>'Augustus','09'=>'September','10'=>'Oktober','11'=>'November','12'=>'December');
+		foreach($months as $m=>$ms){
+		}
+	}
+	$query="SELECT timestamp,consumption,solar,alwaysOn,gridImport,gridExport,selfConsumption,selfSufficiency from `smappee_kwartaal` ORDER BY timestamp ASC";
 	if (!$result=$db->query($query)) die('There was an error running the query ['.$query.'-'.$db->error.']');
 	if ($result->num_rows==0) {
 		echo 'No data for dates '.$f_startdate.' to '.$f_enddate.'<hr>';goto end;
 	}
 	while ($row=$result->fetch_assoc()) {
-		$graph[]=$row;
+		$year=strftime("%Y", $row['timestamp']);
+		$i['time']=strftime("%F %T", $row['timestamp']);
+		$i[$year.'value']=$row['consumption']/1000;
+		$consumption[]=$i;
 	}
 	$result->free();
-	$chart=array_to_chart($graph, $args);
+	$chart=array_to_chart($consumption, $args);
 	echo $chart['script'];
 	echo $chart['div'];
 	unset($chart);
-
+	echo '<pre>';print_r($consumption);echo '</pre>';
 	exit;
 	echo '<h3>Pluviometer per dag</h3>';
 	$args['chart']='ColumnChart';
