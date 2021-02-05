0ba9d2c0918625c982dfa2f922420f602de5ec58
guy@egregius.be <guy@egregius.be>
2021-02-05 16:50:50 +0100

    Update

--- secure/smappeepower.php
+++ secure/smappeepower.php
@@ -7,43 +7,29 @@
  * @package  Pass2PHP
  * @author   Guy Verschuere <guy@egregius.be>
  * @license  GNU GPLv3
- * @link     https://egregius.be
+ * @link	 https://egregius.be
  **/
 require '/var/www/html/secure/functions.php';
 $ctx=stream_context_create(array('http'=>array('timeout' =>5)));
 $smappee=json_decode(file_get_contents('http://192.168.2.15/gateway/apipublic/reportInstantaneousValues', false, $ctx), true);
 if (!empty($smappee['report'])) {
-    preg_match_all(
-        "/ activePower=(\\d*.\\d*)/",
-        $smappee['report'],
-        $matches
-    );
-    if (!empty($matches[1][1])) {
+	preg_match_all("/ activePower=(\\d*.\\d*)/",$smappee['report'],$matches);
+	if (!empty($matches[1][1])) {
 	$db=new PDO("mysql:host=localhost;dbname=$dbname;",$dbuser,$dbpass);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $newzon=round($matches[1][1], 0);
-        if ($newzon<0) $newzon=0;
-        if ($newzon==0 ) {
-            $result = $db->query(
-                "UPDATE devices SET s='$newzon' WHERE n='zon';"
-            ) or trigger_error($db->error);
-        } else {
-            $result = $db->query(
-                "UPDATE devices SET s='$newzon',t='".TIME."' WHERE n='zon';"
-            ) or trigger_error($db->error);
-        }
-        if (!empty($matches[1][2])) {
-            $consumption=round($matches[1][2], 0);
-            $result = $db->query(
-                "UPDATE devices SET s='$consumption',t='".TIME."' WHERE n='el';"
-            ) or trigger_error($db->error);
-            if ($consumption>8500) {
-                alert('Power', 'Power usage: '.$consumption.' W!', 600, false);
-            }
-        }
-    }
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$newzon=round($matches[1][1], 0);
+		if ($newzon<0) $newzon=0;
+		if ($newzon==0 ) $result = $db->query("UPDATE devices SET s='$newzon' WHERE n='zon';") or trigger_error($db->error);
+		else $result = $db->query("UPDATE devices SET s='$newzon',t='".TIME."' WHERE n='zon';") or trigger_error($db->error);
+
+		if (!empty($matches[1][2])) {
+			$consumption=round($matches[1][2], 0);
+			$result = $db->query("UPDATE devices SET s='$consumption',t='".TIME."' WHERE n='el';") or trigger_error($db->error);
+			if ($consumption>8500) alert('Power', 'Power usage: '.$consumption.' W!', 600, false);
+		}
+	}
 } else {
-    if (shell_exec('curl -H "Content-Type: application/json" -X POST -d "" http://192.168.2.15/gateway/apipublic/logon')!='{"success":"Logon successful!","header":"Logon to the monitor portal successful..."}') {
-        exit;
-    }
+	if (shell_exec('curl -H "Content-Type: application/json" -X POST -d "" http://192.168.2.15/gateway/apipublic/logon')!='{"success":"Logon successful!","header":"Logon to the monitor portal successful..."}') {
+		exit;
+	}
 }
