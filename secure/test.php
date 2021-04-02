c247370bf27e669ac85756fe96200761e4c71d32
guy@egregius.be <guy@egregius.be>
2021-04-02 11:40:50 +0200

    Update

--- secure/test.php
+++ secure/test.php
@@ -14,34 +14,6 @@ require 'functions.php';
 echo '<pre>';
 
 
-print_r(getEachIpInRange ( '172.16.0.0/25'));
-
-function getIpRange(  $cidr) {
-
-    list($ip, $mask) = explode('/', $cidr);
-
-    $maskBinStr =str_repeat("1", $mask ) . str_repeat("0", 32-$mask );      //net mask binary string
-    $inverseMaskBinStr = str_repeat("0", $mask ) . str_repeat("1",  32-$mask ); //inverse mask
-
-    $ipLong = ip2long( $ip );
-    $ipMaskLong = bindec( $maskBinStr );
-    $inverseIpMaskLong = bindec( $inverseMaskBinStr );
-    $netWork = $ipLong & $ipMaskLong;
-
-    $start = $netWork+1;//ignore network ID(eg: 192.168.1.0)
-
-    $end = ($netWork | $inverseIpMaskLong) -1 ; //ignore brocast IP(eg: 192.168.1.255)
-    return array('firstIP' => $start, 'lastIP' => $end );
-}
-
-function getEachIpInRange ( $cidr) {
-    $ips = array();
-    $range = getIpRange($cidr);
-    for ($ip = $range['firstIP']; $ip <= $range['lastIP']; $ip++) {
-        $ips[] = long2ip($ip);
-    }
-    return $ips;
-}
 
 
 
