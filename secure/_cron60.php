7a0b47d16b3125260031aa68ddeaf83b1229e013
guy@egregius.be <guy@egregius.be>
2021-04-08 07:50:51 +0200

    Update

--- secure/_cron60.php
+++ secure/_cron60.php
@@ -332,7 +332,10 @@ if ($d['auto']['s']=='On') {
 	if ($d['luifel']['s']==0&&$d['ledluifel']['s']>0) {
 		sl('ledluifel', 0, basename(__FILE__).':'.__LINE__);
 	}
-	if ($d['kookplaatpower']['s']=='On'&&$d['wasbak']['s']=='Off'&&$d['kookplaat']['s']=='Off'&&$d['kookplaatpower_W']['s']<10&&past('kookplaatpower_W')>600) sw('kookplaatpower', 'Off', basename(__FILE__).':'.__LINE__);
+	if ($d['kookplaatpower']['s']=='On'&&$d['wasbak']['s']=='Off'&&$d['kookplaat']['s']=='Off') {
+		$level=explode(';', $d['kookplaatpower_kWh']['s']);
+		if ($level[0]<10&&past('kookplaatpower_kWh')>600) sw('kookplaatpower', 'Off', basename(__FILE__).':'.__LINE__);
+	}
 }
 
 	/* -------------------------------------------- ALTIJD ----------------------------*/
