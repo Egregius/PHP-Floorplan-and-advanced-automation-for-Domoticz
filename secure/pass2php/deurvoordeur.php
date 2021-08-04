5e9e79aef1b0d145d49185aae8758fcc1369414c
guy@egregius.be <guy@egregius.be>
2021-08-04 10:04:03 +0200

    Update

--- secure/pass2php/deurvoordeur.php
+++ secure/pass2php/deurvoordeur.php
@@ -14,6 +14,35 @@ if ($status=="Open"&&$d['auto']['s']=='On') {
 	$zononder=($d['civil_twilight']['m']+$d['Sun']['m'])/2;
 	if ($d['voordeur']['s']=='Off'&&$d['zon']['s']==0&&(TIME<$zonop||TIME>$zononder)) sw('voordeur', 'On', basename(__FILE__).':'.__LINE__);
 	finkom();
+	for ($k=1;$k<=60;$k++) {
+		file_get_contents('http://192.168.2.13/fifo_command.php?cmd=motion_enable%20off');
+		if ($http_response_header[0]=='HTTP/1.1 200 OK') {
+			break;
+		}
+		sleep($k);
+	}
+	for ($k=1;$k<=60;$k++) {
+		file_get_contents('http://192.168.2.11/fifo_command.php?cmd=motion_enable%20off');
+		if ($http_response_header[0]=='HTTP/1.1 200 OK') {
+			break;
+		}
+		sleep($k);
+	}
+} else {
+	for ($k=1;$k<=60;$k++) {
+		file_get_contents('http://192.168.2.13/fifo_command.php?cmd=motion_enable%20on');
+		if ($http_response_header[0]=='HTTP/1.1 200 OK') {
+			break;
+		}
+		sleep($k);
+	}
+	for ($k=1;$k<=60;$k++) {
+		file_get_contents('http://192.168.2.11/fifo_command.php?cmd=motion_enable%20on');
+		if ($http_response_header[0]=='HTTP/1.1 200 OK') {
+			break;
+		}
+		sleep($k);
+	}
 }
 if ($status=='Open') sirene('Voordeur open');
 else sirene('Voordeur dicht');
--- secure/pass2php/poortrf.php
+++ secure/pass2php/poortrf.php
@@ -7,37 +7,37 @@
  * @package  Pass2PHP
  * @author   Guy Verschuere <guy@egregius.be>
  * @license  GNU GPLv3
- * @link     https://egregius.be
+ * @link	 https://egregius.be
  **/
 if ($status=='On') {
-    store('Weg', 0, basename(__FILE__).':'.__LINE__);
-    /*for ($k=1;$k<=60;$k++) {
-        file_get_contents('http://192.168.2.13/fifo_command.php?cmd=motion_enable%20off');
-        if ($http_response_header[0]=='HTTP/1.1 200 OK') {
-            break;
-        }
-        sleep(5);
-    }
-    for ($k=1;$k<=60;$k++) {
-        file_get_contents('http://192.168.2.11/fifo_command.php?cmd=motion_enable%20off');
-        if ($http_response_header[0]=='HTTP/1.1 200 OK') {
-            break;
-        }
-        sleep(5);
-    }*/
+	store('Weg', 0, basename(__FILE__).':'.__LINE__);
+for ($k=1;$k<=60;$k++) {
+		file_get_contents('http://192.168.2.13/fifo_command.php?cmd=motion_enable%20off');
+		if ($http_response_header[0]=='HTTP/1.1 200 OK') {
+			break;
+		}
+		sleep($k);
+	}
+	for ($k=1;$k<=60;$k++) {
+		file_get_contents('http://192.168.2.11/fifo_command.php?cmd=motion_enable%20off');
+		if ($http_response_header[0]=='HTTP/1.1 200 OK') {
+			break;
+		}
+		sleep($k);
+	}
 } else {
-   /* for ($k=1;$k<=60;$k++) {
-        file_get_contents('http://192.168.2.13/fifo_command.php?cmd=motion_enable%20on');
-        if ($http_response_header[0]=='HTTP/1.1 200 OK') {
-            break;
-        }
-        sleep(5);
-    }
-    for ($k=1;$k<=60;$k++) {
-        file_get_contents('http://192.168.2.11/fifo_command.php?cmd=motion_enable%20on');
-        if ($http_response_header[0]=='HTTP/1.1 200 OK') {
-            break;
-        }
-        sleep(5);
-    }*/
+	for ($k=1;$k<=60;$k++) {
+		file_get_contents('http://192.168.2.13/fifo_command.php?cmd=motion_enable%20on');
+		if ($http_response_header[0]=='HTTP/1.1 200 OK') {
+			break;
+		}
+		sleep($k);
+	}
+	for ($k=1;$k<=60;$k++) {
+		file_get_contents('http://192.168.2.11/fifo_command.php?cmd=motion_enable%20on');
+		if ($http_response_header[0]=='HTTP/1.1 200 OK') {
+			break;
+		}
+		sleep($k);
+	}
 }
