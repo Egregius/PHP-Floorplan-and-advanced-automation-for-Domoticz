20e6743da582e83bde91f0940e9238f480a81d6a
guy@egregius.be <guy@egregius.be>
2021-09-13 16:30:51 +0200

    Update

--- secure/cron.php
+++ secure/cron.php
@@ -19,17 +19,16 @@ elseif ($d['heating']['s']==0) include ('_TC_neutral.php');
 elseif ($d['heating']['s']>0) include '_TC_heating.php';
 $user='cron';
 include '_cron10.php';
-if($s<10) {
+if($s<9) {
 	include '_cron60.php';
 	$m=date('i');
 	if ($m%2==0) include '_cron120.php';
 	if ($m%3==0) include '_cron180.php';
 	if ($m%4==0) include '_cron240.php';
-	if ($m%5==0) {
-		include '_cron300.php';
-		include 'gcal/gcal.php';
+	if ($m%5==0) include '_cron300.php';
+	if ($m==0) {
+		include '_cron3600.php';
 		include 'gcal/mirom.php';
 	}
-	if ($m==0) include '_cron3600.php';
 }
 if (isset($_REQUEST['cron'])) include '_'.$_REQUEST['cron'].'.php';
