6dcde1a2f477ebbcfa18d028be075348645a9efb
guy@egregius.be <guy@egregius.be>
2021-09-27 11:30:51 +0200

    Update

--- secure/pass2php/$ 8keuken1.php
+++ secure/pass2php/$ 8keuken1.php
@@ -1,9 +1,10 @@
 <?php
 if ($d['zon']['s']>0) {
-	if ($d['wasbak']['s']>=14) $level=$d['wasbak']['s']+5;
-	else $level=14;
+	if ($d['wasbak']['s']>=14) $level=$d['wasbak']['s']+8;
+	else $level=8;
 } else {
-	if ($d['wasbak']['s']>=3) $level=$d['wasbak']['s']+5;
+	if ($d['wasbak']['s']>=3) $level=$d['wasbak']['s']+4;
 	else $level=3;
 }
+if ($level>100) $level=100;
 sl('wasbak', $level, basename(__FILE__).':'.__LINE__);
--- secure/pass2php/$ 8keuken5.php
+++ secure/pass2php/$ 8keuken5.php
@@ -1,5 +1,10 @@
 <?php
-if ($d['wasbak']['s']>0) $level=$d['wasbak']['s']-10;
-else $level=0;
+if ($d['zon']['s']>0) {
+	if ($d['wasbak']['s']>=14) $level=$d['wasbak']['s']-8;
+	else $level=8;
+} else {
+	if ($d['wasbak']['s']>=4) $level=$d['wasbak']['s']-4;
+	else $level=0;
+}
 if ($level<0) $level=0;
 sl('wasbak', $level, basename(__FILE__).':'.__LINE__);
