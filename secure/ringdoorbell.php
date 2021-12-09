0fc157906fd55f285b3a3638d43622bd7216fd53
guy@egregius.be <guy@egregius.be>
2021-12-09 09:30:50 +0100

    Update

--- secure/ringdoorbell.php
+++ secure/ringdoorbell.php
@@ -9,20 +9,16 @@
  * @license	GNU GPLv3
  * @link		https://egregius.be
  **/
-
 require_once '/var/www/html/secure/functions.php';
-
 if (isset($_REQUEST['source'])) {
-
 	echo ' Start | ';
 	if (apcu_fetch('ring-'.$_REQUEST['kind'])!=$_REQUEST['id']) {
 		echo ' new id | ';
 		apcu_store('ring-'.$_REQUEST['kind'], $_REQUEST['id']);
-		echo 'ok';exit;
 		$d=fetchdata();
 		$zonop=($d['civil_twilight']['s']+$d['Sun']['s'])/2;
 		$zononder=($d['civil_twilight']['m']+$d['Sun']['m'])/2;
-		if ($d['voordeur']['s']=='Off'&&$d['zon']['s']==0&&(TIME<$zonop||TIME>$zononder)) sw('voordeur', 'On', basename(__FILE__).':'.__LINE__);
+		if ($d['voordeur']['s']=='Off'/*&&$d['zon']['s']==0&&(TIME<$zonop||TIME>$zononder)*/) sw('voordeur', 'On', basename(__FILE__).':'.__LINE__);
 		if ($_REQUEST['kind']=='motion'&&$_REQUEST['dt']=='human') {
 			echo ' Motion human | ';
 			if ($d['poortrf']['s']=='Off'&&$d['deurvoordeur']['s']=='Closed'&&past('deurvoordeur')>90&&past('poortrf')>90) {
@@ -32,14 +28,14 @@ if (isset($_REQUEST['source'])) {
 				shell_exec('/usr/bin/wget -O /dev/null -o /dev/null "http://192.168.2.11/fifo_command.php?cmd=record%20on%205%2055" > /dev/null 2>/dev/null &');
 				shell_exec('/usr/bin/wget -O /dev/null -o /dev/null "http://192.168.2.13/fifo_command.php?cmd=record%20on%205%2055" > /dev/null 2>/dev/null &');
 			}
-			//telegram('Ring beweging');
+			telegram('Ring beweging '.$_REQUEST['source'], true, 1);
 		} elseif ($_REQUEST['kind']=='ding') {
 			echo ' Ding | ';
 			if ($d['zon']['s']==0&&(TIME<$zonop||TIME>$zononder)) {
 				echo __LINE__.' Voordeur On | ';
 				sw('deurbel', 'On', basename(__FILE__).':'.__LINE__);
 			}
-			telegram('Ring DEURBEL', false, 2);
+			telegram('Ring DEURBEL '.$_REQUEST['source'], false, 2);
 			shell_exec('/usr/bin/wget -O /dev/null -o /dev/null "http://192.168.2.11/telegram.php?deurbel" > /dev/null 2>/dev/null &');
 			shell_exec('/usr/bin/wget -O /dev/null -o /dev/null "http://192.168.2.13/telegram.php?deurbel" > /dev/null 2>/dev/null &');
 			shell_exec('/usr/bin/wget -O /dev/null -o /dev/null "http://192.168.2.11/fifo_command.php?cmd=record%20on%205%2055" > /dev/null 2>/dev/null &');
