97272b33ca67a3dbb8c3359dce64826dd846e3b9
guy@egregius.be <guy@egregius.be>
2022-02-02 08:40:51 +0100

    Update

--- secure/functions.php
+++ secure/functions.php
@@ -835,22 +835,22 @@ function daikinstatus($device) {
  *
  * @return array();
  */
-function daikinset($device, $power, $mode, $stemp,$msg='', $fan='A', $spmode=0) {
+function daikinset($device, $power, $mode, $stemp,$msg='', $fan='A', $spmode=-1) {
 	if ($device=='living') $ip=111;
 	elseif ($device=='kamer') $ip=112;
 	elseif ($device=='alex') $ip=113;
 	$url="http://192.168.2.$ip/aircon/set_control_info?pow=$power&mode=$mode&stemp=$stemp&f_rate=$fan&shum=0&f_dir=0";
 	lg($url);
 	file_get_contents($url);
-	lg("Daikin $device pow=$power&mode=$mode&stemp=$stemp&f_rate=$fan&shum=0&f_dir=0 spmode=$spmode($msg)");
+	lg("Daikin $device pow=$power&mode=$mode&stemp=$stemp&f_rate=$fan&shum=0&f_dir=0 spmode=$spmode ($msg)");
 	sleep(1);
 	store('daikin'.$device, daikinstatus($device));
 	if ($power==0) storemode('daikin'.$device, 0, basename(__FILE__).':'.__LINE__.':'.$msg);
 	else storemode('daikin'.$device, $mode, basename(__FILE__).':'.__LINE__.':'.$msg);
-	usleep(100000);
+	sleep(1);
 	if ($spmode==0) file_get_contents('http://192.168.2.'.$ip.'/aircon/set_special_mode?set_spmode=0&spmode_kind=1'); // Normal
 	elseif ($spmode==-1) file_get_contents('http://192.168.2.'.$ip.'/aircon/set_special_mode?set_spmode=1&spmode_kind=2'); // Eco
-	elseif ($spmode==1) file_get_contents('http://192.168.2.'.$ip.'/aircon/set_special_mode?set_spmode=1&spmode_kind=2'); // Power
+	elseif ($spmode==1) file_get_contents('http://192.168.2.'.$ip.'/aircon/set_special_mode?set_spmode=1&spmode_kind=1'); // Power
 }
 
 function RefreshZwave($node){
