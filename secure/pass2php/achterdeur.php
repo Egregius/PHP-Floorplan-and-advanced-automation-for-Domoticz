bd3f0e1b52c72108e631add58d6c24583ef1a40b
guy@egregius.be <guy@egregius.be>
2022-02-14 09:10:51 +0100

    Update

--- secure/pass2php/achterdeur.php
+++ secure/pass2php/achterdeur.php
@@ -13,13 +13,13 @@ if ($status=="Open") {
 	if ($d['dampkap']['s']=='On') {
 		sw('dampkap', 'Off', basename(__FILE__).':'.__LINE__);
 	}
-	/* // BOSE BUITEN
-	if ($d['Weg']['s']==0&&$d['bose105']['m']=='Online') {
+	// BOSE BUITEN
+	if ($d['Weg']['s']==0&&$d['bose106']['icon']=='Online') {
 		$status=json_decode(
 			json_encode(
 				simplexml_load_string(
 					@file_get_contents(
-						"http://192.168.2.105:8090/now_playing"
+						"http://192.168.2.106:8090/now_playing"
 					)
 				)
 			),
@@ -28,26 +28,46 @@ if ($status=="Open") {
 		if (!empty($status)) {
 			if (isset($status['@attributes']['source'])) {
 				if ($status['@attributes']['source']=='STANDBY') {
-					bosezone(105);
-					sw('bose105', 'On', basename(__FILE__).':'.__LINE__);
+					bosezone(106);
+					sw('bose106', 'On', basename(__FILE__).':'.__LINE__);
 				}
 			}
 		}
 	}
-	*/ // END BOSE BUITEN
+	if ($d['Weg']['s']==0&&$d['bose107']['icon']=='Online') {
+		$status=json_decode(
+			json_encode(
+				simplexml_load_string(
+					@file_get_contents(
+						"http://192.168.2.107:8090/now_playing"
+					)
+				)
+			),
+			true
+		);
+		if (!empty($status)) {
+			if (isset($status['@attributes']['source'])) {
+				if ($status['@attributes']['source']=='STANDBY') {
+					bosezone(107);
+					sw('bose107', 'On', basename(__FILE__).':'.__LINE__);
+				}
+			}
+		}
+	}
+	// END BOSE BUITEN
 	sirene('Achterdeur open');
  } else {
 	if ($d['Weg']['s']>0&&$d['auto']['s']==1&&past('Weg')>178) {
 			sw('sirene', 'On', basename(__FILE__).':'.__LINE__);
 			telegram('Achterdeur open om '.strftime("%k:%M:%S", TIME), false, 3);
 	}
-	/* // BOSE BUITEN
-	if ($d['Weg']['s']==0&&$d['bose105']['m']=='Online') {
+	// BOSE BUITEN
+	if ($d['Weg']['s']==0&&$d['bose106']['icon']=='Online') {
 		$status=json_decode(
 			json_encode(
 				simplexml_load_string(
 					@file_get_contents(
-						"http://192.168.2.105:8090/now_playing"
+						"http://192.168.2.106:8090/now_playing"
 					)
 				)
 			),
@@ -56,13 +76,33 @@ if ($status=="Open") {
 		if (!empty($status)) {
 			if (isset($status['@attributes']['source'])) {
 				if ($status['@attributes']['source']!='STANDBY') {
-					bosekey("POWER", 0, 105);
-					sw('bose105', 'Off', basename(__FILE__).':'.__LINE__);
+					bosekey("POWER", 0, 106);
+					sw('bose106', 'Off', basename(__FILE__).':'.__LINE__);
 				}
 			}
 		}
 	}
-	*/ // END BOSE BUITEN
+	if ($d['Weg']['s']==0&&$d['bose107']['icon']=='Online') {
+		$status=json_decode(
+			json_encode(
+				simplexml_load_string(
+					@file_get_contents(
+						"http://192.168.2.107:8090/now_playing"
+					)
+				)
+			),
+			true
+		);
+		if (!empty($status)) {
+			if (isset($status['@attributes']['source'])) {
+				if ($status['@attributes']['source']!='STANDBY') {
+					bosekey("POWER", 0, 107);
+					sw('bose107', 'Off', basename(__FILE__).':'.__LINE__);
+				}
+			}
+		}
+	}
+	// END BOSE BUITEN
 	sirene('Achterdeur dicht');
 }
 
