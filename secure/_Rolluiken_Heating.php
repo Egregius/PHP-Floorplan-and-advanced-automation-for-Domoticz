60e205138ac31abbf4f5eef13ca8d725ab2af579
guy@egregius.be <guy@egregius.be>
2022-02-24 14:00:51 +0100

    Update

--- secure/_Rolluiken_Heating.php
+++ secure/_Rolluiken_Heating.php
@@ -26,7 +26,7 @@ if ($d['auto']['s']=='On') {
 			if ($d['RkamerR']['s']>0&&TIME>=strtotime('6:30')) sl('RkamerR', 0, basename(__FILE__).':'.__LINE__);
 		}
 		if ($dag==true||$d['pirhall']['s']=='On') {
-			if (($d['Weg']['s']!=1||$d['pirhall']['s']=='On')&&$d['Ralex']['s']==0&&$d['Rspeelkamer']['s']>0&&TIME>=strtotime('7:30')&&($d['deurspeelkamer']['s']=='Open'||$d['speelkamer']['s']>0)) sl('Rspeelkamer', 0, basename(__FILE__).':'.__LINE__);
+			if (($d['Weg']['s']!=1||$d['pirhall']['s']=='On')&&$d['Ralex']['s']==0&&$d['Rspeelkamer']['s']>0&&TIME>=strtotime('7:30')&&($d['deuralex']['s']=='Open'||$d['speelkamer']['s']>0)) sl('Rspeelkamer', 0, basename(__FILE__).':'.__LINE__);
 			if (($d['Weg']['s']!=1||$d['pirhall']['s']=='On')&&$d['Ralex']['s']>0&&TIME>=strtotime('7:30')&&($d['deuralex']['s']=='Open'||$d['alex']['s']>0)) sl('Ralex', 0, basename(__FILE__).':'.__LINE__);
 		}
 		if ($dag==true&&$zon==0&&$d['Weg']['s']!=1&&$d['tv']['s']=='Off') {
--- secure/pass2php/$ 8keuken1.php
+++ secure/pass2php/$ 8keuken1.php
@@ -1,9 +1,9 @@
 <?php
 if ($d['zon']['s']>0) {
-	if ($d['wasbak']['s']>=14) $level=ceil($d['wasbak']['s']*1.5);
+	if ($d['wasbak']['s']>=14) $level=ceil($d['wasbak']['s']*1.8);
 	else $level=14;
 } else {
-	if ($d['wasbak']['s']>=3) $level=ceil($d['wasbak']['s']*1.2);
+	if ($d['wasbak']['s']>=3) $level=ceil($d['wasbak']['s']*1.5);
 	else $level=3;
 }
 if ($level>100) $level=100;
