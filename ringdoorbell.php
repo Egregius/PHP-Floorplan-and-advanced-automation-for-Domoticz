376e35e80c2cf66b83131d8e13d21dd26a7f38cc
guy@egregius.be <guy@egregius.be>
2022-03-29 09:50:50 +0200

    Update

--- ringdoorbell.php
+++ ringdoorbell.php
@@ -12,41 +12,35 @@
 require_once '/var/www/html/secure/functions.php';
 if (isset($_REQUEST['source'])&&isset($_REQUEST['token'])&&$_REQUEST['token']=='CKgSwM01pQibqgAzWfUsdE5nUlzT1wnNdtz09EO2') {
 	echo ' Start | ';
-	$id=floor($_REQUEST['id']/60)*60;
-	if (apcu_fetch('ring-'.$_REQUEST['kind'])!=$id) {
-		echo ' new id | ';
-		apcu_store('ring-'.$_REQUEST['kind'], $id);
-		$d=fetchdata();
-		$zonop=($d['civil_twilight']['s']+$d['Sun']['s'])/2;
-		$zononder=($d['civil_twilight']['m']+$d['Sun']['m'])/2;
-		if ($d['zon']['s']==0&&(TIME<$zonop||TIME>$zononder)) {
-			echo ' licht voordeur aan | ';
-			sw('voordeur', 'On', basename(__FILE__).':'.__LINE__);
-		}
-		if ($_REQUEST['kind']=='on_demand') {
-			echo ' On demand | ';
-			telegram('Ring on demand '.$_REQUEST['source'], true, 1);
-		} elseif ($_REQUEST['kind']=='motion'&&$_REQUEST['dt']=='human') {
-			echo ' Motion human | ';
-			if ($_SERVER['REQUEST_TIME']>apcu_fetch('ringbeweging')+30) {
-				apcu_store('ringbeweging', $_SERVER['REQUEST_TIME']);
-				if ($d['poortrf']['s']=='Off'&&$d['deurvoordeur']['s']=='Closed'&&past('deurvoordeur')>90&&past('poortrf')>90) {
-					echo ' Picams | ';
-					shell_exec('/usr/bin/wget -O /dev/null -o /dev/null "http://192.168.2.11/telegram.php?ringbeweging&source='.$_REQUEST['source'].'" > /dev/null 2>/dev/null &');
-	//				shell_exec('/usr/bin/wget -O /dev/null -o /dev/null "http://192.168.2.12/telegram.php?ringbeweging&source='.$_REQUEST['source'].'" > /dev/null 2>/dev/null &');
-					shell_exec('/usr/bin/wget -O /dev/null -o /dev/null "http://192.168.2.11/fifo_command.php?cmd=record%20on%205%2055" > /dev/null 2>/dev/null &');
-	//				shell_exec('/usr/bin/wget -O /dev/null -o /dev/null "http://192.168.2.12/fifo_command.php?cmd=record%20on%205%2055" > /dev/null 2>/dev/null &');
-				}
-	//			telegram('Ring beweging '.$_REQUEST['source'], true, 1);
+	$d=fetchdata();
+	$zonop=($d['civil_twilight']['s']+$d['Sun']['s'])/2;
+	$zononder=($d['civil_twilight']['m']+$d['Sun']['m'])/2;
+	if ($d['zon']['s']==0&&(TIME<$zonop||TIME>$zononder)) {
+		echo ' licht voordeur aan | ';
+		sw('voordeur', 'On', basename(__FILE__).':'.__LINE__);
+	}
+	if ($_REQUEST['kind']=='on_demand') {
+		echo ' On demand | ';
+		telegram('Ring on demand '.$_REQUEST['source'], true, 1);
+	} elseif ($_REQUEST['kind']=='motion'&&$_REQUEST['dt']=='human') {
+		echo ' Motion human | ';
+		if ($_SERVER['REQUEST_TIME']>apcu_fetch('ring_motion')+30) {
+			apcu_store('ring_motion', $_SERVER['REQUEST_TIME']);
+			if ($d['poortrf']['s']=='Off'&&$d['deurvoordeur']['s']=='Closed'&&past('deurvoordeur')>90&&past('poortrf')>90) {
+				echo ' Picams | ';
+				shell_exec('/usr/bin/wget -O /dev/null -o /dev/null "http://192.168.2.11/telegram.php?ringbeweging&source='.$_REQUEST['source'].'" > /dev/null 2>/dev/null &');
+				shell_exec('/usr/bin/wget -O /dev/null -o /dev/null "http://192.168.2.11/fifo_command.php?cmd=record%20on%205%2055" > /dev/null 2>/dev/null &');
 			}
-		} elseif ($_REQUEST['kind']=='ding') {
-			echo ' Ding | ';
-//			telegram('Ring DEURBEL '.$_REQUEST['source'], false, 1);
+//			telegram('Ring beweging '.$_REQUEST['source'], true, 1);
+		}
+	} elseif ($_REQUEST['kind']=='ding') {
+		echo ' Ding | ';
+		if ($_SERVER['REQUEST_TIME']>apcu_fetch('ring_ding')+30) {
+			apcu_store('ring_ding', $_SERVER['REQUEST_TIME']);
+			telegram('Ring DEURBEL '.$_REQUEST['source'], false, 1);
 			if ($d['deurvoordeur']['s']=='Closed') {
 				shell_exec('/usr/bin/wget -O /dev/null -o /dev/null "http://192.168.2.11/telegram.php?ringdeurbel&source='.$_REQUEST['source'].'" > /dev/null 2>/dev/null &');
-//				shell_exec('/usr/bin/wget -O /dev/null -o /dev/null "http://192.168.2.12/telegram.php?ringdeurbel&source='.$_REQUEST['source'].'" > /dev/null 2>/dev/null &');
 				shell_exec('/usr/bin/wget -O /dev/null -o /dev/null "http://192.168.2.11/fifo_command.php?cmd=record%20on%205%2055" > /dev/null 2>/dev/null &');
-//				shell_exec('/usr/bin/wget -O /dev/null -o /dev/null "http://192.168.2.12/fifo_command.php?cmd=record%20on%205%2055" > /dev/null 2>/dev/null &');
 			}
 			if ($d['Weg']['s']==0) {
 				echo ' Deurbel | ';
@@ -62,7 +56,7 @@ if (isset($_REQUEST['source'])&&isset($_REQUEST['token'])&&$_REQUEST['token']=='
 				sl('Xvol', 5, basename(__FILE__).':'.__LINE__);
 			}
 		}
-		echo ' END';
 	}
+	echo ' END';
 }
 //telegram(print_r($_REQUEST, true), true, 1);
