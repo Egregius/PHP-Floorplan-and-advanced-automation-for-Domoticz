491c725fda971e4bfc853f667b15775ec33922a8
guy@egregius.be <guy@egregius.be>
2022-04-06 13:50:51 +0200

    Update

--- secure/cleandisk.sh
+++ secure/cleandisk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIRECTORY="/var/www/html/picam1/archive"
-CAPACITY=80
+CAPACITY=95
 while [[ $(df $DIRECTORY | awk 'NR==2 && gsub("%","") {print$5}') -ge $CAPACITY ]];do
         rm -rf $(find $DIRECTORY -mindepth 1 -printf '%T+ %p\n' | sort | awk 'NR==1 {print$2}')
 done
