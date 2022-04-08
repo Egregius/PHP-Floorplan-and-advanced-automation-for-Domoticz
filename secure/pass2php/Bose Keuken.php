2feba9fa0f60c00a0e7fb59b4ff1e899ebffa860
guy@egregius.be <guy@egregius.be>
2022-04-08 07:50:50 +0200

    Update

--- secure/pass2php/Bose Keuken.php
+++ secure/pass2php/Bose Keuken.php
@@ -1,9 +1,9 @@
 <?php
 if($status>60)$status=30;
 if ($d['bose105']['s']=='Off'&&$status>0) {
-	sw('bose105', 'On');
-	bosezone(105, true);
+	bosezone(105, true, $status);
+	sw('bose105', 'On',basename(__FILE__).':'.__LINE__);
 } elseif ($d['bose105']['s']=='On'&&$status==0) {
-	sw('bose105', 'Off');
-	bosekey("POWER", 75000, 105);
+	bosekey("POWER", 0, 105);
+	sw('bose105', 'Off',basename(__FILE__).':'.__LINE__);
 } else bosevolume($status, 105, basename(__FILE__).':'.__LINE__);
