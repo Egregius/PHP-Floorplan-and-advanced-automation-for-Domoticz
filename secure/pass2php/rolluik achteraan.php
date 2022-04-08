be85425405b15341056da8b37e6f528f56f32827
guy@egregius.be <guy@egregius.be>
2022-04-08 10:00:50 +0200

    Update

--- secure/pass2php/rolluik achteraan.php
+++ secure/pass2php/rolluik achteraan.php
@@ -1,9 +1,2 @@
 <?php
-if($status>60)$status=30;
-if ($d['bose101']['s']=='Off'&&$status>0) {
-	sw('bose101', 'On');
-	bosekey("PRESET_5", 0, 101);
-} elseif ($d['bose101']['s']=='On'&&$status==0) {
-	sw('bose101', 'Off');
-	bosekey("POWER");
-} else bosevolume($status);
+sl('Rliving', $status, basename(__FILE__).':'.__LINE__);
