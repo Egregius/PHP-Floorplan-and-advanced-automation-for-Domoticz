3b6cee8c8275c62d84d0e3b788fa03a6ecb50943
guy@egregius.be <guy@egregius.be>
2022-04-10 08:30:50 +0200

    Update

--- ajax.php
+++ ajax.php
@@ -325,7 +325,7 @@ elseif (isset($_REQUEST['device'])&&isset($_REQUEST['command'])&&isset($_REQUEST
 		if ($_REQUEST['device']=='nas') {
 			shell_exec('secure/wakenas.sh');
 		} else {
-			call_user_func($_REQUEST['command'],$_REQUEST['device'],$_REQUEST['action'],basename(__FILE__).':'.__LINE__);
+			call_user_func($_REQUEST['command'],str_replace('_', ' ', $_REQUEST['device']),$_REQUEST['action'],basename(__FILE__).':'.__LINE__);
 		}
 	}
 }
