bcd4bce548ca81e9543292ad8bb67c541145be9b
guy@egregius.be <guy@egregius.be>
2022-05-05 17:30:51 +0200

    Update

--- ajax.php
+++ ajax.php
@@ -202,7 +202,7 @@ elseif (isset($_REQUEST['device'])&&isset($_REQUEST['command'])&&isset($_REQUEST
 			}
 		} elseif ($_REQUEST['device']=='tv') {
 			if ($d['Rliving']['s']<30) sl('Rliving', 30, basename(__FILE__).':'.__LINE__);
-			if ($d['Rbureel']['s']<70) sl('Rbureel', 70, basename(__FILE__).':'.__LINE__);
+			if ($d['Rbureel']['s']<70) sl('Rbureel', 69, basename(__FILE__).':'.__LINE__);
 			if ($d['RkeukenL']['s']<55) sl('RkeukenL', 55, basename(__FILE__).':'.__LINE__);
 			if ($d['RkeukenR']['s']<55) sl('RkeukenR', 55, basename(__FILE__).':'.__LINE__);
 		} else {
