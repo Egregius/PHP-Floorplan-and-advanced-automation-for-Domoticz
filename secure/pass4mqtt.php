480210285eebbf353033ae09d1c2bb351971b2ad
guy@egregius.be <guy@egregius.be>
2023-05-22 16:54:50 +0200

    Update

--- secure/pass4mqtt.php
+++ secure/pass4mqtt.php
@@ -1,46 +1,17 @@
 <?php
-// Using https://github.com/php-mqtt/client
-
 declare(strict_types=1);
 
 require '/var/www/client-examples/vendor/autoload.php';
 require '/var/www/html/secure/functions.php';
 
-//use PhpMqtt\Client\Examples\Shared\SimpleLogger;
 use PhpMqtt\Client\Exceptions\MqttClientException;
 use PhpMqtt\Client\MqttClient;
 use Psr\Log\LogLevel;
 
-// Create an instance of a PSR-3 compliant logger. For this example, we will also use the logger to log exceptions.
-//$logger = new SimpleLogger(LogLevel::INFO);
-
-try {
-    // Create a new instance of an MQTT client and configure it to use the shared broker host and port.
-    $client = new MqttClient('127.0.0.1', 1883, 'pass4mqtt', MqttClient::MQTT_3_1, null, null);
-
-    // Connect to the broker without specific connection settings but with a clean session.
-    $client->connect(null, true);
-
-    // Subscribe to the topic 'foo/bar/baz' using QoS 0.
-    $client->subscribe('#', function (string $topic, string $message, bool $retained) use ($client) {
- /*       $logger->info('{topic}: {message}', [
-            'topic' => $topic,
-            'message' => $message,
-            'typeOfMessage' => $retained ? 'retained message' : 'message',
-        ]);*/
-		lg($topic.'	'.$message);
-        // After receiving the first message on the subscribed topic, we want the client to stop listening for messages.
-       // $client->interrupt();
-    }, MqttClient::QOS_AT_MOST_ONCE);
-
-    // Since subscribing requires to wait for messages, we need to start the client loop which takes care of receiving,
-    // parsing and delivering messages to the registered callbacks. The loop will run indefinitely, until a message
-    // is received, which will interrupt the loop.
-    $client->loop(true);
-
-    // Gracefully terminate the connection to the broker.
-    $client->disconnect();
-} catch (MqttClientException $e) {
-    // MqttClientException is the base exception of all exceptions in the library. Catching it will catch all MQTT related exceptions.
-//    $logger->error('Subscribing to a topic using QoS 0 failed. An exception occurred.', ['exception' => $e]);
-}
+$client = new MqttClient('127.0.0.1', 1883, 'pass4mqtt', MqttClient::MQTT_3_1, null, null);
+$client->connect(null, true);
+$client->subscribe('#', function (string $topic, string $message, bool $retained) use ($client) {
+	lg($topic.'	'.$message);
+}, MqttClient::QOS_AT_MOST_ONCE);
+$client->loop(true);
+$client->disconnect();
