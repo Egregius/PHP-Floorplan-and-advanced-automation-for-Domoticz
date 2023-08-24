a65867181d4176a37461cc5b2d315a0a0014a4a1
guy@egregius.be <guy@egregius.be>
2023-08-24 07:00:50 +0200

    Update

--- secure/spotify.php
+++ secure/spotify.php
@@ -1,5 +1,5 @@
 <?php
 require 'functions.php';
 $d=fetchdata();
-if ($d['bose101']['s']=='On'||$d['bose105']['s']=='On') echo 1;
+if ($d['bose101']['s']=='On'||$d['bose105']['s']=='On'||past('pirliving')<3600) echo 1;
 else echo 0;
