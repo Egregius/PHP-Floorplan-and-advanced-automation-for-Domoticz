1ea7206b5ee21535b22acdbf9cad6901fea5e22e
guy@egregius.be <guy@egregius.be>
2023-10-05 08:00:50 +0200

    Update

--- secure/cleandomoticzdb.php
+++ secure/cleandomoticzdb.php
@@ -5,8 +5,8 @@ $clean = strftime("%G-%m-%d %k:%M:%S", $time-3600*24*7);
 $tables = array( 'LightingLog',
 	'MultiMeter',
 	'MultiMeter_Calendar',
-//	'Meter',
-//	'Meter_Calendar',
+	'Meter',
+	'Meter_Calendar',
 	'Percentage',
 	'Percentage_Calendar',
 	'Rain',
@@ -38,7 +38,7 @@ foreach ($tables as $table) {
 	}
 */
 }
-/*
+
 $query=$db->exec("DELETE FROM LightingLog WHERE DeviceRowID in (6,24,170,171,172,173,174,175,176,177,295,296,297,298,299,300,301,302)");
 if ($query) {
 	$rows = $db->changes();
@@ -55,8 +55,8 @@ if ($query) {
 		echo $rows." old rows removed from LightingLog".PHP_EOL;
 		sleep(1);
 	}
-}*/
-//$sql = 'VACUUM;';
-//if(!$result = $db->exec($sql))
-//	die('There was an error running the query [' . $db->error . ']');
+}
+$sql = 'VACUUM;';
+if(!$result = $db->exec($sql))
+die('There was an error running the query [' . $db->error . ']');
 ?>
