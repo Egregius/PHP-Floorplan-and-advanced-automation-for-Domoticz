7e89c6139bcd1ad39ed424c672821d17a04bd7b4
guy@egregius.be <guy@egregius.be>
2023-10-09 10:00:50 +0200

    Update

--- camera.php
+++ camera.php
@@ -1,35 +1,36 @@
 <?php
 require '/var/www/config.php';
-$user='camera';
-$mysqli=new mysqli('localhost', $dbuser, $dbpass, $dbname);
-$result = $mysqli->query("select n,i,s,t,m from devices WHERE n in ('Weg', 'poortrf', 'deurvoordeur','civil_twilight', 'Sun', 'zon', 'voordeur');") or trigger_error($mysqli->error." [$sql]");
-while ($row = $result->fetch_array()) {
-	$d[$row['n']]['i'] = $row['i'];
-	$d[$row['n']]['s'] = $row['s'];
-	$d[$row['n']]['t'] = $row['t'];
-	$d[$row['n']]['m'] = $row['m'];
-}
-$data=array();
-$data['w']=$d['Weg']['s'];
-$data['p']=$d['poortrf']['s'];
-$data['d']=$d['deurvoordeur']['s'];
-$times[]=TIME-$d['deurvoordeur']['t'];
-$times[]=TIME-$d['poortrf']['t'];
-$times[]=TIME-$d['Weg']['t'];
+if (isset($_GET['token'])&&$_GET['token']==$cameratoken) {
+	$user='camera';
+	$mysqli=new mysqli('localhost', $dbuser, $dbpass, $dbname);
+	$result = $mysqli->query("select n,i,s,t,m from devices WHERE n in ('Weg', 'poortrf', 'deurvoordeur','civil_twilight', 'Sun', 'zon', 'voordeur');") or trigger_error($mysqli->error." [$sql]");
+	while ($row = $result->fetch_array()) {
+		$d[$row['n']]['i'] = $row['i'];
+		$d[$row['n']]['s'] = $row['s'];
+		$d[$row['n']]['t'] = $row['t'];
+		$d[$row['n']]['m'] = $row['m'];
+	}
+	$data=array();
+	$data['w']=$d['Weg']['s'];
+	$data['p']=$d['poortrf']['s'];
+	$data['d']=$d['deurvoordeur']['s'];
+	$times[]=TIME-$d['deurvoordeur']['t'];
+	$times[]=TIME-$d['poortrf']['t'];
+	$times[]=TIME-$d['Weg']['t'];
 
-$data['t']=min($times);
-$zonop=($d['civil_twilight']['s']+$d['Sun']['s'])/2;
-$zononder=($d['civil_twilight']['m']+$d['Sun']['m'])/2;
-if ($d['Weg']['s']==0&&$data['t']>90) {
-	file_get_contents($domoticzurl.'/json.htm?type=command&param=switchlight&idx=749&switchcmd=Set%20Level&level=10');
-	file_get_contents($domoticzurl.'/json.htm?type=command&param=switchlight&idx=747&switchcmd=Set%20Level&level=20&passcode=');
-}
-if ($d['zon']['s']==0&&(TIME<$zonop||TIME>$zononder)) {
-	$data['z']=0;
-	sw('voordeur', 'On', basename(__FILE__).':'.__LINE__);
-} else $data['z']=1;
-echo serialize($data);
-
+	$data['t']=min($times);
+	$zonop=($d['civil_twilight']['s']+$d['Sun']['s'])/2;
+	$zononder=($d['civil_twilight']['m']+$d['Sun']['m'])/2;
+	if ($d['Weg']['s']==0&&$data['t']>90) {
+		file_get_contents($domoticzurl.'/json.htm?type=command&param=switchlight&idx=749&switchcmd=Set%20Level&level=10');
+		file_get_contents($domoticzurl.'/json.htm?type=command&param=switchlight&idx=747&switchcmd=Set%20Level&level=20&passcode=');
+	}
+	if ($d['zon']['s']==0&&(TIME<$zonop||TIME>$zononder)) {
+		$data['z']=0;
+		sw('voordeur', 'On', basename(__FILE__).':'.__LINE__);
+	} else $data['z']=1;
+	echo serialize($data);
+} else echo '403 Access denied';
 function sw($name,$action='Toggle',$msg='') {
 	global $user,$d,$domoticzurl;
 	if (!isset($d)) $d=fetchdata();
