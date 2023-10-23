a0985f0f5970cc78f5c3d06c0e3364c355d2494b
guy@egregius.be <guy@egregius.be>
2023-10-23 16:00:50 +0200

    Update

--- secure/wakeimac.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-wakeonlan -i 192.168.2.50 -p 9 3c:07:54:22:34:17 | tee -a '/var/log/domoticz.log'
-wakeonlan -i 192.168.2.51 -p 9 04:54:53:02:0a:34 | tee -a '/var/log/domoticz.log'
-wakeonlan -i 192.168.2.255 -p 9 3c:07:54:22:34:17 | tee -a '/var/log/domoticz.log'
-wakeonlan -i 192.168.2.255 -p 9 04:54:53:02:0a:34 | tee -a '/var/log/domoticz.log'
-
