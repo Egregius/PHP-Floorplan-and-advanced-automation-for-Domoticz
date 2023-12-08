c00c0681071b3f7256c0a35f081dda822046cf6b
guy@egregius.be <guy@egregius.be>
2023-12-08 09:00:51 +0100

    Update

--- secure/functions.php
+++ secure/functions.php
@@ -13,7 +13,7 @@ function fliving() {
 	if ($d['Media']['s']=='Off'&&$d['lamp kast']['s']!='On'&&$d['eettafel']['s']==0) {
 		if (($d['zon']['s']==0&&$d['dag']<3)||($d['RkeukenL']['s']>80&&$d['RkeukenR']['s']>80&&$d['Rbureel']['s']>80&&$d['Rliving']['s']>80)) {
 			if ($d['wasbak']['s']==0&&$time<strtotime('21:30')) sl('wasbak', 10, basename(__FILE__).':'.__LINE__);
-			if ($d['lamp kast']['s']=='Off'&&$d['snijplank']['s']==0&&$time<strtotime('21:30')) sw('lamp kast', 'On', basename(__FILE__).':'.__LINE__.' dag='.$d['dag']);
+			if ($d['lamp kast']['s']=='Off'&&$d['wasbak']['s']==0&&$d['snijplank']['s']==0&&$time<strtotime('21:30')) sw('lamp kast', 'On', basename(__FILE__).':'.__LINE__.' dag='.$d['dag']);
 		}
 		if ($d['bose101']['s']=='Off'&&$time>=strtotime('5:30')&&$time<strtotime('17:30')) {
 			bosezone(101);
