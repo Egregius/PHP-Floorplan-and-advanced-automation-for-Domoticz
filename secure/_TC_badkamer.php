5a86225da3c4634cdce7d48d512e005b9e2f4d4a
guy@egregius.be <guy@egregius.be>
2024-01-16 12:00:53 +0100

    Update

--- secure/_TC_badkamer.php
+++ secure/_TC_badkamer.php
@@ -94,7 +94,7 @@ if ($d['deurbadkamer']['s']=='Open'&&$pastdeurbadkamer>60) {
 
 if ($d['badkamer_set']['m']==1&&$l>2&&$d['lichtbadkamer']['s']==0) {$l=2;$m.=' + '.__LINE__.' slow';}
 if ($d['Weg']['s']>=3) {$l=0;$m.=' + '.__LINE__.' Weg';}
-if ($time>$t+2700) {$l=0;$m.=' + '.__LINE__.' Weg';}
+if ($time>$t+2700&&$l>1) {$l=1;$m.=' + '.__LINE__.' Weg';}
 if ($d['luchtdroger']['m']!='Auto') {$l=$d['luchtdroger']['m'];$m.=' + '.__LINE__.' Fixed';}
 
 if ($l==0) {
