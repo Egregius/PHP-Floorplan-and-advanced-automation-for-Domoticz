7ced995bfe3ec34f0b4cfc8a46615724db2e89f6
guy@egregius.be <guy@egregius.be>
2024-04-15 18:00:52 +0200

    Update

--- secure/cache.php
+++ secure/cache.php
@@ -9,6 +9,7 @@ if (isset($_REQUEST['fetch'])) {
 	if ($_REQUEST['m']=='auto'&&$d['auto']['m']==0) echo 0;
     else echo $d[$_REQUEST['m']]['m'];
 } elseif (isset($_REQUEST['store'])&&isset($_REQUEST['value'])) {
+	echo $_REQUEST['store'], $_REQUEST['value'];
     store($_REQUEST['store'], $_REQUEST['value'], basename(__FILE__).':'.__LINE__);
 } elseif (isset($_REQUEST['count'])) {
     $data=$d[$_REQUEST['count']]['s']+1;
