b5ebc4ca0fa85dfa38a728ab1b980beb08b6083c
guy@egregius.be <guy@egregius.be>
2024-04-16 11:00:52 +0200

    Update

--- secure/functions.php
+++ secure/functions.php
@@ -87,6 +87,7 @@ function fhall() {
 function huisslapen() {
 	global $d,$boseipbuiten;
 	if (!isset($d['zon']['s'])) $d=fetchdata();
+	if ($d['bed']['s']=='Off') sw('bed', 'On', basename(__FILE__).':'.__LINE__);
 	sl(array('hall','inkom','eettafel','zithoek','wasbak','terras','ledluifel'), 0, basename(__FILE__).':'.__LINE__);
 	sw(array('garageled','garage','pirgarage','pirkeuken','pirliving','pirinkom','pirhall','kristal','bureel','lamp kast','tuin','snijplank','zolderg','wc','GroheRed','kookplaat','steenterras','houtterras'), 'Off', basename(__FILE__).':'.__LINE__);
 	foreach (array('living_set','alex_set','kamer_set','badkamer_set','eettafel','zithoek','luifel') as $i) {
@@ -115,6 +116,7 @@ function huisthuis() {
 	global $d;
 	if (!is_array($d)) $d=fetchdata();
 	store('Weg', 0);
+	if ($d['bed']['s']=='On') sw('bed', 'Off', basename(__FILE__).':'.__LINE__);
 	if ($d['bose101']['m']!=1) storemode('bose101', 1, basename(__FILE__).':'.__LINE__);
 	if ($d['bose103']['m']!=0) storemode('bose103', 0, basename(__FILE__).':'.__LINE__);
 	if ($d['auto']['s']!='On') store('auto', 'On', basename(__FILE__).':'.__LINE__);
