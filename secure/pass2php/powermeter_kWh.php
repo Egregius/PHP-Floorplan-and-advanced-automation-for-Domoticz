332a9900e275ed6a3ae5b26944b89f6e924bb525
guy@egregius.be <guy@egregius.be>
2024-04-21 09:00:53 +0200

    Update

--- secure/pass2php/powermeter_kWh.php
+++ secure/pass2php/powermeter_kWh.php
@@ -1,8 +1,8 @@
 <?php
 $past=time()-mget('powermeter');
-if ($status<2200&&$d['powermeter']['s']=='On'&&$past>3600) {
+if ($status<2200&&$d['powermeter']['s']=='On'&&$past>3590) {
 	sw('powermeter', 'Off', basename(__FILE__).':'.__LINE__);
-	alert('auto','Auto opgeladen, '.$status.'W '.past('powermeter'),600);
+	alert('auto','Auto opgeladen, '.$status.'W '.$past,600);
 }
 //if ($status<2220&&$d['powermeter']['s']=='On'&&past('powermeter')>3600) sw('powermeter', 'Off', basename(__FILE__).':'.__LINE__);
 //telegram($status);
