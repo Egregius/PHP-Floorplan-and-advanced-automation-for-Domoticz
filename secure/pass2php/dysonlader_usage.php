c367f3d360e3296e52f3b5d93399d8a4e1b03878
guy@egregius.be <guy@egregius.be>
2024-06-17 15:00:51 +0200

    Update

--- secure/pass2php/dysonlader_usage.php
+++ secure/pass2php/dysonlader_usage.php
@@ -1,4 +1,4 @@
 <?php
 if ($status!=$d['dysonlader_usage']['s']) {
-	if ($status<15&&$d['dysonlader']['s']=='On'&&past('dysonlader')>60) sw('dysonlader', 'Off', basename(__FILE__).':'.__LINE__);
+	if ($status<15&&$d['dysonlader']['s']=='On'&&past('dysonlader')>1800) sw('dysonlader', 'Off', basename(__FILE__).':'.__LINE__);
 }
--- secure/pass2php/lg_webos_tv_cd9e.php
+++ secure/pass2php/lg_webos_tv_cd9e.php
@@ -1,6 +1,8 @@
 <?php
-if ($status!=$d[$device]['s']) store($device,$status, basename(__FILE__).':'.__LINE__);//lg(print_r($topic,true));
-if ($status=='On') {
+if ($status!=$d[$device]['s']) store($device,$status, basename(__FILE__).':'.__LINE__);
+//lg(print_r($topic,true));
+
+if ($status=='On'&&$d['Rliving']['s']<25) {
 	$time=time();
 	if ($time>=strtotime('18:30')) sl('Rliving', 25, basename(__FILE__).':'.__LINE__);
 }
