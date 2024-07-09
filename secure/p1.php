787fae830fa40f9a2b46f10480a3aabd471359e6
guy@egregius.be <guy@egregius.be>
2024-07-09 18:00:53 +0200

    Update

--- secure/p1.php
+++ secure/p1.php
@@ -1,18 +1,38 @@
 <?php
 
-$fp = fopen("/dev/ttyUSB1", "w+");
-$data=fread($fp, 1024);
+	$fp = fopen("/dev/ttyUSB1", "w+");
+	$data=fread($fp, 1024);
+	
+	$data=explode("\n", $data);
+	//print_r($data);
+	foreach ($data as $i) {
+		$c=string_between_two_string($i, ":", "(");
+		if ($c=="1.8.1") {
+			$w=1*substr($i,10,-6);
+			$elec=$w;
+			echo 'Elec dag :	'.$w.PHP_EOL;
+		} elseif ($c=="1.8.2") {
+			$w=1*substr($i,10,-6);
+			$elec+=$w;
+			echo 'Elec nacht :	'.$w.PHP_EOL;
+//		} elseif ($c=="2.8.1") {
+//			echo 'Injectie dag :		'. 1*substr($i,10,-6).PHP_EOL;
+//		} elseif ($c=="2.8.2") {
+//			echo 'Injectie nacht :	'. 1*substr($i,10,-6).PHP_EOL;
+		} elseif ($c=="24.2.1") {
+			echo 'Water :		'. 1*substr($i,26,-5).PHP_EOL;
+		} elseif ($c=="24.2.3") {
+			echo 'Gas :		'. 1*substr($i,26,-5).PHP_EOL;
+		}
+	}
+	
+	echo 'Elec totaal :	'.$elec.PHP_EOL;
 
-$data=explode("\n", $data);
-print_r($data);
-foreach ($data as $i) {
-	echo $i.PHP_EOL;
-	if (getStringBetween($i, ":", "(")=="1.8.1") echo $i;
+function string_between_two_string($str, $starting_word, $ending_word) {
+    $subtring_start = strpos($str, $starting_word);
+    $subtring_start += strlen($starting_word); 
+    $size = strpos($str, $ending_word, $subtring_start) - $subtring_start; 
+    return substr($str, $subtring_start, $size); 
 }
 
 
-function getStringBetween($str,$from,$to)
-{
-    $sub = substr($str, strpos($str,$from)+strlen($from),strlen($str));
-    return substr($sub,0,strpos($sub,$to));
-}
