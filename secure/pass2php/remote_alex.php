d09e6d9da6a53fa3e54d37d426134086fa5bcc4a
guy@egregius.be <guy@egregius.be>
2024-08-05 08:00:52 +0200

    Update

--- secure/pass2php/remote_alex.php
+++ secure/pass2php/remote_alex.php
@@ -4,5 +4,5 @@ if ($status=='On') {
 	else sl('alex', 5, basename(__FILE__).':'.__LINE__);
 } else {
 	if ($d['alex']['s']>1) sl('alex', 1, basename(__FILE__).':'.__LINE__);
-	else sw('alex', 'Off', basename(__FILE__).':'.__LINE__);
+	else sl('alex', 0, basename(__FILE__).':'.__LINE__);
 }
