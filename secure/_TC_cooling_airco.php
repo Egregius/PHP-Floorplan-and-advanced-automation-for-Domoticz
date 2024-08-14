1e2318616d0ff9b74cb584688b4effe0b102eed5
guy@egregius.be <guy@egregius.be>
2024-08-14 15:00:52 +0200

    Update

--- secure/_TC_cooling_airco.php
+++ secure/_TC_cooling_airco.php
@@ -331,11 +331,11 @@ if ($d['auto']['s']=='On') {
 	}
 
 	elseif ($time>=strtotime('15:00')&&$time<strtotime('22:00')) {
-		if($d['zon']['s']>2500) {
+		if($d['zon']['s']>1500) {
 			if ($d['raamwaskamer']['s']=='Closed'&&$d['Rwaskamer']['s']<50) sl('Rwaskamer', 83, basename(__FILE__).':'.__LINE__);
 			if ($d['raamalex']['s']=='Closed'&&$d['Ralex']['s']<50) sl('Ralex', 83, basename(__FILE__).':'.__LINE__);
 			if ($d['Rbureel']['s']<30) sl('Rbureel', 30, basename(__FILE__).':'.__LINE__);
-			if ($d['Weg']['s']>1&&$d['Rliving']['s']<86&&$d['living_temp']['s']>21&&$d['luifel']['s']<30) sl('Rliving', 86, basename(__FILE__).':'.__LINE__);
+			if ($d['Weg']['s']>1&&$d['Rliving']['s']<86&&$d['living_temp']['s']>21&&$d['luifel']['s']<30&&past('Weg')>3600) sl('Rliving', 86, basename(__FILE__).':'.__LINE__);
 		}
 	}
 
--- secure/pass2php/remote_alex.php
+++ secure/pass2php/remote_alex.php
@@ -3,6 +3,6 @@ if ($status=='On') {
 	if ($d['alex']['s']==0) sl('alex', 1, basename(__FILE__).':'.__LINE__);
 	else sl('alex', 5, basename(__FILE__).':'.__LINE__);
 } else {
-	if ($d['alex']['s']>1) sl('alex', 1, basename(__FILE__).':'.__LINE__);
-	else sl('alex', 0, basename(__FILE__).':'.__LINE__);
+	if ($d['alex']['s']>2) sl('alex', 1, basename(__FILE__).':'.__LINE__);
+	else sw('alex', 'Off', basename(__FILE__).':'.__LINE__);
 }
