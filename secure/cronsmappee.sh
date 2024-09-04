a8a8be23d1ce806cd073e709ea8180a4488b426f
guy@egregius.be <guy@egregius.be>
2024-09-04 13:00:52 +0200

    Update

--- secure/cronsmappee.sh
+++ secure/cronsmappee.sh
@@ -4,6 +4,6 @@ do
 	curl -s --connect-timeout 2 --max-time 30 "http://127.0.0.1/secure/smappeepower.php" >/dev/null 2>&1 &
 	sleep 3.95
 done
-pkill -f "p1.php"
-/var/www/p1.php
+#pkill -f "p1.php"
+#/var/www/p1.php
 
