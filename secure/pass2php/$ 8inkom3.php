cbcaa3009d736d4a0168b54b7f1c008524ab0b8c
guy@egregius.be <guy@egregius.be>
2024-12-10 08:00:52 +0100

    Update

--- secure/pass2php/$ 8inkom3.php
+++ secure/pass2php/$ 8inkom3.php
@@ -1,5 +1,5 @@
 <?php
 mset('powermeter',time());
-storemode('powermeter', 2500, basename(__FILE__).':'.__LINE__);
+storemode('powermeter', 2000, basename(__FILE__).':'.__LINE__);
 usleep(1000000);
 sw('powermeter', 'On', basename(__FILE__).':'.__LINE__,true);
--- secure/pass2php/$ 8keuken8.php
+++ secure/pass2php/$ 8keuken8.php
@@ -1,3 +1,3 @@
 <?php
 sw('GroheRed', 'On', basename(__FILE__).':'.__LINE__);
-storemode('GroheRed', 2500, basename(__FILE__).':'.__LINE__);
+storemode('GroheRed', 2000, basename(__FILE__).':'.__LINE__);
