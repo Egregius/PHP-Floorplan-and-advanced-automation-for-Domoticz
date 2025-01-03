631d19851f2af8e050d65cf21c2365ddf71d4f55
guy@egregius.be <guy@egregius.be>
2025-01-03 14:00:52 +0100

    Update

--- secure/energy.php
+++ secure/energy.php
@@ -54,14 +54,14 @@ while (1){
 		$sec=date('s');
 		$min=date('i');
 		$uur=date('G');
-		if ($sec==52&&($min==14||$min==29||$min==44||$min==59)) { // Einde kwartier
+		if ($data->active_power_average_w>2500&&$sec==52&&($min==14||$min==29||$min==44||$min==59)) { // Einde kwartier
 			if (!isset($dbverbruik)) {
 				$dbverbruik=new mysqli('192.168.2.20','home','H0m€','verbruik');
 				if($dbverbruik->connect_errno>0){die('Unable to connect to database ['.$dbverbruik->connect_error.']');}
 			}
 			$query="INSERT INTO `kwartierpiek` (`date`,`wh`) VALUES ('".date('Y-m-d H:i')."','".$data->active_power_average_w."')";
 			if(!$result=$dbverbruik->query($query)){echo('There was an error running the query "'.$query.'" - '.$dbverbruik->error);}
-			if($data->active_power_average_w>2500) telegram('Kwartierpiek: '.$data->active_power_average_w.' Wh');
+			telegram('Kwartierpiek: '.$data->active_power_average_w.' Wh');
 		}
 		if ($total!=$prevtotal) {
 			$elec=$data->total_power_import_kwh;
--- secure/pass2php/Grohered_kWh.php
+++ secure/pass2php/Grohered_kWh.php
@@ -1,10 +1,9 @@
 <?php
-//lg('grohered='.$status);
-//lg('Grohered_kWh: status='.$status.', Xlightmode='.$d['Xlight']['m']);
 if ($status>100&&($d['Xlight']['m']=='groen'||$d['Xlight']['m']=='uit')) {
 	rgb('Xlight', 0, 70);
 	storemode('Xlight', 'rood');
 } elseif ($status<100&&$d['GroheRed']['s']=='On'&&($d['Xlight']['m']=='rood'||$d['Xlight']['m']=='uit')) {
 	rgb('Xlight', 115, 30);
 	storemode('Xlight', 'groen');
+	if (past('GroheRed')>300) sw('GroheRed', 'Off');
 }
