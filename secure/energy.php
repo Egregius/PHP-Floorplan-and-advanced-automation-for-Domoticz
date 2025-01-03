0272ac33583005b334a5c1baebf0600581676802
guy@egregius.be <guy@egregius.be>
2025-01-03 09:00:52 +0100

    Update

--- secure/energy.php
+++ secure/energy.php
@@ -63,9 +63,7 @@ while (1){
 			if(!$result=$dbverbruik->query($query)){echo('There was an error running the query "'.$query.'" - '.$dbverbruik->error);}
 			if($data->active_power_average_w>2500) telegram('Kwartierpiek: '.$data->active_power_average_w.' Wh');
 		}
-//		if ($uur>=5&&(($uur<23&&$min%10==0&&$sec==0)||($uur==23&&$min==59&&$sec==55))) {
 		if ($total!=$prevtotal) {
-			lg('--- Updating energy data ---');
 			$elec=$data->total_power_import_kwh;
 			$injectie=$data->total_power_export_kwh;
 			foreach ($data->external as $i) {
