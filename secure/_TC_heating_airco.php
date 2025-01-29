3e902b0ddf784934bd087c9189256b756281b383
guy@egregius.be <guy@egregius.be>
2025-01-29 17:00:53 +0100

    Update

--- secure/_TC_heating_airco.php
+++ secure/_TC_heating_airco.php
@@ -34,8 +34,10 @@ if ($d['daikin']['m']==1) {
 				elseif ($dif>=1.2) $d[$k.'_set']['s']=$d[$k.'_set']['s']-1.5;
 				elseif ($dif>=0.8) $d[$k.'_set']['s']=$d[$k.'_set']['s']-1;
 				elseif ($dif>=0.4) $d[$k.'_set']['s']=$d[$k.'_set']['s']-0.5;
+				elseif ($dif<=-0.4) $d[$k.'_set']['s']=$d[$k.'_set']['s']+1;
+				elseif ($dif<=0) $d[$k.'_set']['s']=$d[$k.'_set']['s']+0.5;
 				if ($k=='living') {
-					$set=$d[$k.'_set']['s']-0;
+					$set=$d[$k.'_set']['s'];
 					if ($maxpow==40&&$set>$d[$k.'_temp']['s']-2&&$d[$k.'_temp']['s']>19.5) $set=(ceil($d[$k.'_temp']['s']*2)/2)-2;
 					elseif ($maxpow==50&&$set>$d[$k.'_temp']['s']-1.5&&$d[$k.'_temp']['s']>19.5) $set=(ceil($d[$k.'_temp']['s']*2)/2)-1.5;
 					if ((($d['Media']['s']=='On'&&$time>strtotime('19:00'))||($d['eettafel']['s']>0&&$time>strtotime('11:45')&&$time>strtotime('13:00'))||($d['eettafel']['s']>0&&$time>strtotime('17:30')&&$time>strtotime('19:00')))) $rate='B';
