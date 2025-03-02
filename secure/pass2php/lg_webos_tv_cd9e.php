93e190404319dda7b2beb9074695ea7ebd460eb2
guy@egregius.be <guy@egregius.be>
2025-03-02 07:00:53 +0100

    Update

--- secure/pass2php/lg_webos_tv_cd9e.php
+++ secure/pass2php/lg_webos_tv_cd9e.php
@@ -12,7 +12,7 @@ if ($status=='On') {
 		if ($d['Rbureel']['s']<70) sl('Rbureel', 69, basename(__FILE__).':'.__LINE__);
 		if ($d['RkeukenL']['s']<55) sl('RkeukenL', 55, basename(__FILE__).':'.__LINE__);
 		if ($d['RkeukenR']['s']<55) sl('RkeukenR', 55, basename(__FILE__).':'.__LINE__);
-		hass('switch','turn_off','switch.plug7_socket_1');
+//		hass('switch','turn_off','switch.plug7_socket_1');
 	}
 	if ($d['bose101']['m']==1&&$d['bose101']['s']=='On'&&$d['bose102']['s']=='Off'&&$d['bose103']['s']=='Off'&&$d['bose104']['s']=='Off'&&$d['bose105']['s']=='Off'&&$d['bose106']['s']=='Off'&&$d['bose107']['s']=='Off'&&$d['eettafel']['s']==0) {
 		bosekey("POWER", 0, 101, basename(__FILE__).':'.__LINE__);
