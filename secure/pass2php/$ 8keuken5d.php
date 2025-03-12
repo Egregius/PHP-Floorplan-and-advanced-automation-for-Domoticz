a5c3111ec4f825ae99050e79b31abeeadd3d2f42
guy@egregius.be <guy@egregius.be>
2025-03-12 07:00:52 +0100

    Update

--- secure/pass2php/$ 8keuken5d.php
+++ secure/pass2php/$ 8keuken5d.php
@@ -2,7 +2,7 @@
 if ($d['wasbak']['s']>0) sl('wasbak', 0, basename(__FILE__).':'.__LINE__, true);
 if ($d['snijplank']['s']>0) sl('snijplank', 0, basename(__FILE__).':'.__LINE__, true);
 if ($d['kookplaat']['s']=='On') sw('kookplaat', 'Off', basename(__FILE__).':'.__LINE__, true);
-if ($d['GroheRed']['s']=='On') sw('GroheRed', 'Off', basename(__FILE__).':'.__LINE__, true);
+if ($d['GroheRed']['s']=='On'&&$d['zon']==0) sw('GroheRed', 'Off', basename(__FILE__).':'.__LINE__, true);
 if ($d['bose102']['s']=='On') {
 	bosekey("POWER", 0, 102);
 	sw('bose102', 'Off',basename(__FILE__).':'.__LINE__);
--- secure/telegram.php
+++ secure/telegram.php
@@ -1,13 +1,15 @@
 <?php
 if (isset($_REQUEST['alert'],$_REQUEST['text'],$_REQUEST['time'])) {
 	include 'functions.php';
-	alert($_REQUEST['alert'], str_replace('__', PHP_EOL, $_REQUEST['text']), $_REQUEST['time']);
+	if (isset($_REQUEST['silent'])) $silent=$_REQUEST['silent'];
+	else $silent=true;
+	alert($_REQUEST['alert'], str_replace('__', PHP_EOL, $_REQUEST['text']), $_REQUEST['time'], $silent);
 	exit;
 } else {
 	require '/var/www/config.php';
 }
 $silent=true;
-$to=null;
+$to=1;
 if (isset($_REQUEST['silent'])) {
 	$silent=$_REQUEST['silent'];
 }
@@ -24,29 +26,31 @@ if (!empty($argv[1])&&!empty($argv[2])) {
 if (isset($content)) {
 	echo $content;
 	$bot_url="https://api.telegram.org/bot".$telegrambot."/";
-	$url=$bot_url."sendMessage?chat_id=".$telegramchatid1.'&disable_web_page_preview&parse_mode=html';
-	$post_fields=array(
-		'text'=>$content,
-		'disable_notification'=>$silent
-	);
-	$ch=curl_init();
-	curl_setopt(
-		$ch,
-		CURLOPT_HTTPHEADER,
-		array("Content-Type:multipart/form-data")
-	);
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 4);
-	for ($x=1;$x<=10;$x++) {
-		$result=json_decode(curl_exec($ch), true);
-		if (is_array($result)&&$result['ok']===true) {
-			break;
+	if ($to==1||$to==3) {
+		$url=$bot_url."sendMessage?chat_id=".$telegramchatid1.'&disable_web_page_preview&parse_mode=html';
+		$post_fields=array(
+			'text'=>$content,
+			'disable_notification'=>$silent
+		);
+		$ch=curl_init();
+		curl_setopt(
+			$ch,
+			CURLOPT_HTTPHEADER,
+			array("Content-Type:multipart/form-data")
+		);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 4);
+		for ($x=1;$x<=10;$x++) {
+			$result=json_decode(curl_exec($ch), true);
+			if (is_array($result)&&$result['ok']===true) {
+				break;
+			}
+			sleep($x*2);
 		}
-		sleep($x*2);
 	}
-	if ($to=='2') {
+	if ($to==2||$to==3) {
 		$url=$bot_url."sendMessage?chat_id=".$telegramchatid2.'&disable_web_page_preview&parse_mode=html';
 		$post_fields=array(
 			'text'=>$content,
