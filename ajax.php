5aa915e123a2ac053288d914f4b29f8aac7a20b8
guy@egregius.be <guy@egregius.be>
2025-03-23 09:00:51 +0100

    Update

--- ajax.php
+++ ajax.php
@@ -91,11 +91,9 @@ elseif (isset($_REQUEST['device'])&&isset($_REQUEST['command'])&&isset($_REQUEST
 			if($dow==0||$dow==6) $t=strtotime('7:30');
 			elseif($dow==2||$dow==5) $t=strtotime('6:45');
 			else $t=strtotime('7:00');
-			if (TIME<$t+900||TIME>strtotime('12:00')||$user=='Guy') {
-				if ($d['badkamervuur1']['s']=='Off'&&$d['badkamer_temp']['s']<$_REQUEST['action']) sw('badkamervuur1', 'On', basename(__FILE__).':'.__LINE__);
-				setpoint($_REQUEST['device'].'_set', $_REQUEST['action'], basename(__FILE__).':'.__LINE__);
-				storemode($_REQUEST['device'].'_set', 1, basename(__FILE__).':'.__LINE__);
-			}
+			if ($d['badkamervuur1']['s']=='Off'&&$d['badkamer_temp']['s']<$_REQUEST['action']) sw('badkamervuur1', 'On', basename(__FILE__).':'.__LINE__);
+			setpoint($_REQUEST['device'].'_set', $_REQUEST['action'], basename(__FILE__).':'.__LINE__);
+			storemode($_REQUEST['device'].'_set', 1, basename(__FILE__).':'.__LINE__);
 		} else {
 			setpoint($_REQUEST['device'].'_set', $_REQUEST['action'], basename(__FILE__).':'.__LINE__);
 			if ($_REQUEST['device']=='living') {
