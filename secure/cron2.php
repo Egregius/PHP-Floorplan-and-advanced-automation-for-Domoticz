9f154a9a7eac41e1951b407812f5003794301c72
guy@egregius.be <guy@egregius.be>
2025-06-29 08:00:52 +0200

    Update

--- secure/cron2.php
+++ secure/cron2.php
@@ -8,13 +8,15 @@ $lastfetch=$time-20;
 while (1){
 	$start=microtime(true);
 	$time=time();
+	$d['time']=$time;
 	$d=fetchdata($lastfetch,basename(__FILE__).':'.__LINE__);
 	$lastfetch=$time-20;
 	include 'cron2B.php';
 	$time_elapsed_secs=microtime(true)-$start;
 	$sleep=10-$time_elapsed_secs;
-	if ($sleep<0) $sleep=0;
-	$sleep=round($sleep*1000000);
-	usleep($sleep);
+	if ($sleep>0) {
+		$sleep=round($sleep*1000000);
+		usleep($sleep);
+	}
 }
 
