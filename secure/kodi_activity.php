ef8a6ce4ac683977e51f255e1098db6d9a6e3129
guy@egregius.be <guy@egregius.be>
2025-10-22 17:00:53 +0200

    Update

--- secure/kodi_activity.php
+++ secure/kodi_activity.php
@@ -3,43 +3,11 @@
 $uniq = substr(microtime(), 2,6);
 exec("mosquitto_pub -h 192.168.2.26 -u mqtt -P mqtt -t 'kodi_last_action' -m '{$_GET['action']}-$uniq'");
 echo 'ok';
-exit;
-lg("kodi ping: {$_GET['action']}-$uniq");
+//exit;
+lg("'{$_GET['action']}'");
 function lg($msg,$level=0) {
-/*
-Levels:
-0:	Default / Undefined
-1:	Loop starts
-2:	
-3:	
-4:	Switch commands
-5:	Setpoints
-6:	OwnTracks
-7:	
-8:	Update kWh devices
-9:	Update temperatures
-10: Store/Storemode
-99:	SQL Fetchdata
-*/
-	static $inLg = false;
-	if ($inLg) return; // voorkomt recursie
-
-	$inLg = true;
-	global $d;
-	if (isset($d['auto']['m'])) {
-		$loglevel = $d['auto']['m'];
-	} else $loglevel = 0;
-
-	if ($level <= $loglevel) {
-		$fp = fopen('/temp/domoticz.log', "a+");
-		$time = microtime(true);
-		$dFormat = "Y-m-d H:i:s";
-		$mSecs = $time - floor($time);
-		$mSecs = substr(number_format($mSecs, 3), 1);
-		fwrite($fp, sprintf("%s%s %s\n", date($dFormat), $mSecs, $msg));
-		fclose($fp);
-	}
-
-	$inLg = false;
+	$fp = fopen('/temp/kodi_activity.log', "a+");
+	fwrite($fp, sprintf("%s\n", $msg));
+	fclose($fp);
 }
 ?>
