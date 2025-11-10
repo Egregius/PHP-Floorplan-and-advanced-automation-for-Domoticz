13aa596b93a5150c21061731d37aa24bae646799
guy@egregius.be <guy@egregius.be>
2025-11-10 21:00:53 +0100

    Update

--- secure/energy.php
+++ secure/energy.php
@@ -5,11 +5,9 @@ $force = in_array('--force', $argv);
 require '/var/www/html/secure/functions.php';
 date_default_timezone_set('Europe/Brussels');
 
-/** Databases */
 $dbverbruik = new mysqli('192.168.2.20','home','H0m€','verbruik');
 if($dbverbruik->connect_errno > 0) die('DB verbruik fail: '.$dbverbruik->connect_error);
 
-/** Kwartierpiek deze maand */
 $kwartierpiek = 2500;
 $q = "SELECT MAX(wH) AS wH FROM `kwartierpiek` WHERE date LIKE '".date('Y-m')."-%'";
 if ($r = $dbverbruik->query($q)) {
@@ -17,10 +15,9 @@ if ($r = $dbverbruik->query($q)) {
     $r->free();
 }
 
-/** Inladen huidige meterdata */
 $en = json_decode(getCache('en'));
 $teller = json_decode(getCache('teller'));
-if (!isset($teller->import)) exit; // niets binnen → stoppen
+if (!isset($teller->import)) exit;
 $newData = [
     'energy_import' => $teller->import,
     'energy_export' => $teller->export,
@@ -33,17 +30,16 @@ $elec=$teller->import;
 $injectie=$teller->export;
 $water=$teller->water;
 $alwayson = (int)getCache('alwayson');
-/** Variabelen uit meter */
 $newavg = $en->a;
-
-/** Vorige waarden */
 $prevavg   = getCache('energy_prevavg');
 
-/** Always-on detectie */
 if ($zon == 0) {
-    $power = $en->n;
-    if ($power>=50 && ($power<$alwayson || empty($alwayson))) {
+    if ($en->b<0) $power = $en->n-$en->b;
+    else $power = $en->n;
+    if ($power>=30 && $power<=300 && ($power<$alwayson || empty($alwayson))) {
         setCache('alwayson',$power);
+        $alwayson=$power;
+        $force=true;
         $time=time();
         lg('New alwayson '.$power.' W');
         $vandaag=date("Y-m-d",$time);
@@ -51,10 +47,9 @@ if ($zon == 0) {
     }
 }
 
-/** Kwartierpiek detectie */
 if ($prevavg > 2500) {
     if ($newavg > $kwartierpiek - 200) alert('Kwartierpiek', 'Kwartierpiek momenteel al '.$newavg.' Wh!'.PHP_EOL.'Piek deze maand = '.$kwartierpiek.' Wh', 120, false);
-    if ($newavg < $prevavg) { // nieuw kwartier
+    if ($newavg < $prevavg) {
         $dbverbruik->query("INSERT INTO `kwartierpiek` (`date`,`wh`) VALUES ('".date('Y-m-d H:i:s')."','".$prevavg."')");
         if ($prevavg > $kwartierpiek - 200) {
             alert('KwartierpiekB', 'Kwartierpiek = '.$prevavg.' Wh'.PHP_EOL.'Piek deze maand = '.$kwartierpiek.' Wh', 30, false);
@@ -63,7 +58,6 @@ if ($prevavg > 2500) {
     }
 }
 
-/** Verbruik updates als total gewijzigd */
 if (updateVerbruikCache($newData, $force)) {	
 	$prevwater=getCache('water_meter');
 	if ($prevwater!=$water && getCache('weg')>2) {
@@ -71,12 +65,9 @@ if (updateVerbruikCache($newData, $force)) {
 		alert('water_meter', 'Water verbruik gedetecteerd!', 300, true);
 		lg("Waterteller: prev=$prevwater, nu=$water");
 	}
-       
-
     $time=time();
     $vandaag=date("Y-m-d",$time);
 
-    /** Zon totalen */
     $dbzonphp = new mysqli('192.168.2.20','home','H0m€','egregius_zonphp');
     $zonvandaag=0; $zontotaal=0;
 
@@ -89,7 +80,6 @@ if (updateVerbruikCache($newData, $force)) {
     $dbverbruik->query("INSERT INTO `Guy` (`date`,`gas`,`elec`,`injectie`,`zon`,`water`) VALUES ('$vandaag','$gas','$elec','$injectie','$zontotaal','$water')
         ON DUPLICATE KEY UPDATE gas='$gas',elec='$elec',injectie='$injectie',zon='$zontotaal',water='$water'");
 
-    /** Dagwaarden */
     $q="SELECT `date`,`gas`,`elec`,`injectie`,`water` FROM `Guy` ORDER BY `date` DESC LIMIT 1,1";
     if ($r=$dbverbruik->query($q)) { $gisteren=$r->fetch_assoc(); $r->free(); }
 
@@ -120,12 +110,10 @@ if (updateVerbruikCache($newData, $force)) {
     lg($data);
     echo $data;
     setCache('energy_vandaag',$data);
-    setCache('energy_lastupdate', time());
+    setCache('energy_lastupdate', $time);
 }
 
-/** Opslaan nieuwe referenties */
 setCache('energy_prevavg', $newavg);
-
 
 function updateVerbruikCache($newData, $force = false, $thresholds = ['energy_import'=>0.1,'energy_export'=>0.1,'gas'=>0.1,'water'=>0.01]) {
     $cacheFile = '/dev/shm/cache/verbruik.json';
@@ -134,7 +122,6 @@ function updateVerbruikCache($newData, $force = false, $thresholds = ['energy_im
         $cache = json_decode(file_get_contents($cacheFile), true) ?: [];
     }
     if (!isset($cache['previous'])) {
-        // Eerste keer
         $cache['previous'] = $newData;
         $cache['current'] = $newData;
         file_put_contents($cacheFile, json_encode($cache));
@@ -149,7 +136,6 @@ function updateVerbruikCache($newData, $force = false, $thresholds = ['energy_im
     foreach ($newData as $key => $value) {
         $prevValue = $cache['previous'][$key] ?? 0;
         $dif=round(abs($value - $prevValue),3);
-//        if ($key=='energy_import') lg($key.'	value='.$value.'	prevValue='.$prevValue.'	dif='.$dif.'	treshold='.$thresholds[$key]);
         if ($dif >= ($thresholds[$key] ?? 0)) {
         	lg('update needed');
             $updateNeeded = true;
@@ -157,10 +143,9 @@ function updateVerbruikCache($newData, $force = false, $thresholds = ['energy_im
         }
     }
     if ($updateNeeded || $force) {
-		$cache['previous'] = $newData; // only update previous when we actually process
+		$cache['previous'] = $newData;
 		$cache['current'] = $newData;
 		file_put_contents($cacheFile, json_encode($cache));
 	}
     return $updateNeeded;
 }
-
