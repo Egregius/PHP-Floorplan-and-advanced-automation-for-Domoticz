f10223df374142c655e2d2970adda2716a4b2a47
guy@egregius.be <guy@egregius.be>
2025-12-06 05:00:54 +0100

    Update

--- d.php
+++ d.php
@@ -136,8 +136,8 @@ while ($row = $stmt->fetch()) {
 
 $data=json_encode($d, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 echo $data;
-//if ($type=='f') lg($msg.'	'.count($d)-6 .' updates	'.strlen($data).' bytes'.$msg2);
-//else lg($msg.'	'.count($d)-1 .' updates	'.strlen($data).' bytes'.$msg2);
+if ($type=='f') lg($msg.'	'.count($d)-6 .' updates	'.strlen($data).' bytes'.$msg2);
+else lg($msg.'	'.count($d)-1 .' updates	'.strlen($data).' bytes'.$msg2);
 function dbconnect() {
     global $dbname, $dbuser, $dbpass;
     static $db = null;
