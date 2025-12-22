a499c2c733b58ef8d34b861b9e0952e36fb958d1
guy@egregius.be <guy@egregius.be>
2025-12-22 15:00:54 +0100

    Update

--- secure/pass2php/inputliving1.php
+++ secure/pass2php/inputliving1.php
@@ -1,3 +1,7 @@
 <?php
-if ($d['bureelrechts']['s']==0) sl('bureelrechts', 50);
-else sl('bureelrechts', 0);
+$data=apcu_fetch('inputliving1');
+if ($data===false) {
+	if ($d['bureelrechts']['s']==0) sl('bureelrechts', 50);
+	else sl('bureelrechts', 0);
+	apcu_store('inputliving1', 1, 1);
+}
--- secure/pass2php/inputliving3.php
+++ secure/pass2php/inputliving3.php
@@ -1,8 +1,12 @@
 <?php
-if ($status=='On') {
-	if ($d['zithoek']['s']>0) sl('zithoek', 0);
-	else {
-		if ($d['lgtv']['s']=='On') sl('zithoek', 5);
-		else sl('zithoek', 75);
+$data=apcu_fetch('inputliving3');
+if ($data===false) {
+	if ($status=='On') {
+		if ($d['zithoek']['s']>0) sl('zithoek', 0);
+		else {
+			if ($d['lgtv']['s']=='On') sl('zithoek', 5);
+			else sl('zithoek', 75);
+		}
+		apcu_store('inputliving3', 1, 1);
 	}
 }
