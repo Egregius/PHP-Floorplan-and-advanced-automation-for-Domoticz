b4513e348072596fdb7ba4392a27eaac104955a7
guy@egregius.be <guy@egregius.be>
2026-01-16 16:00:54 +0100

    Update

--- secure/owntracks.php
+++ secure/owntracks.php
@@ -7,23 +7,20 @@ if ($_SERVER['REMOTE_ADDR']=='192.168.2.20'||$_SERVER['REMOTE_ADDR']=='192.168.2
 				if ($_GET['event']=='enter') {
 					$user=$_GET['user'];
 					$db = Database::getInstance();
-					$stmt=$db->query("SELECT n,s FROM devices WHERE n IN ('weg','dag');");
-					while ($row=$stmt->fetch(PDO::FETCH_NUM)) {
-						$d[$row[0]]['s']=$row[1];
-					}
-					if($d['weg']['s']==2) {
+					$d=fetchdata();
+					if($d['weg']->s==2) {
 						hassnotify('🏠 Huis thuis', 'door '.$user, 'mobile_app_iphone_guy', false);
 						setCache('remoteauto', time());
 						sw('voordeur', 'On', basename(__FILE__).':'.__LINE__);
 						huisthuis('Huis thuis door '.$user);
-						if ($d['dag']['s']>-2) {
+						if ($d['dag']->s>=-2) {
 							sleep(5);
 							sw('voordeur', 'Off', basename(__FILE__).':'.__LINE__);
 						}
-					} elseif($d['weg']['s']==0) {
+					} elseif($d['weg']->s==0) {
 						sw('voordeur', 'On', basename(__FILE__).':'.__LINE__);
 						huisthuis('🏠 Huis thuis door '.$user);
-						if ($d['dag']['s']>-2) {
+						if ($d['dag']->s>=-2) {
 							sleep(5);
 							sw('voordeur', 'Off', basename(__FILE__).':'.__LINE__);
 						}
