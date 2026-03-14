70e6fc1b1a7b808e079025f0866d2dbb8c165fa9
Egregius <guy@egregius.be>
2026-03-14 06:55:52 +0100

    Update

--- secure/functions.php
+++ secure/functions.php
@@ -1243,7 +1243,9 @@ function republishmqtt() {
 			}
 			list($domain, $object_id) = explode('.', $entity_id);
 			$state = ucfirst($data['state']);
-			if ($state!=$i->s) $to_publish[] = [
+			if($i->s==0) $knownstate='Closed';
+			elseif($i->s==1) $knownstate='Open';
+			if ($state!=$knownstate) $to_publish[] = [
 				'topic' => "$base_topic/$domain/$object_id/state",
 				'payload' => $state
 			];
