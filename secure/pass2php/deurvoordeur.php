bf624a5166f3e9cde596833a1140d673d94e0504
Egregius <guy@egregius.be>
2026-03-23 08:40:03 +0100

    Update

--- secure/pass2php/deurvoordeur.php
+++ secure/pass2php/deurvoordeur.php
@@ -1,5 +1,9 @@
 <?php
-if ($d['auto']->s=='On') {
-	if ($status=="Open"&&$d['voordeur']->s=='Off'&&$d['dag']->s<-2) sw('voordeur', 'On', basename(__FILE__).':'.__LINE__);
-	finkom();
-}
+if ($status=="Open") {
+	setCache('timestampweg',$time+300);
+	if ($d['auto']->s=='On') {
+		if(&&$d['voordeur']->s=='Off'&&$d['dag']->s<-2) sw('voordeur', 'On', basename(__FILE__).':'.__LINE__);
+		finkom();
+	}
+} else setCache('timestampweg',$time-240);
+
