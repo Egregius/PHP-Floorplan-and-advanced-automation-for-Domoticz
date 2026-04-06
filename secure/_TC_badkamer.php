833f8f33fcfe9605530998e3b46e5d74fffadbdd
Egregius <guy@egregius.be>
2026-04-06 16:30:02 +0200

    Update

--- secure/_TC_badkamer.php
+++ secure/_TC_badkamer.php
@@ -3,9 +3,9 @@ $user='TC_badkamer';
 $m='';$m2='';
 $preheatbath=false;
 if ($d['badkamer_set']->m==0) {$setBath=10;$m2.=__LINE__.' ';}
-else {$setBath=$d['badkamer_set']->s;$m2.=__LINE__.' ';}
+//else {$setBath=$d['badkamer_set']->s;$m2.=__LINE__.' ';}
 $pastdeurbadkamer=past('deurbadkamer');
-if ($d['weg']->s>=2) $setBath=10;
+if ($d['weg']->s>2) $setBath=10;
 elseif ($d['badkamer_set']->m==0&&$d['deurbadkamer']->s=='Open'&&$pastdeurbadkamer>57&&($d['raamkamer']->s=='Open'||$d['raamwaskamer']->s=='Open'||$d['raamalex']->s=='Open')) {
 	$setBath=5;$m2.=__LINE__.' ';
 } elseif ($d['badkamer_set']->m==0&&($d['deurbadkamer']->s=='Closed'||($d['deurbadkamer']->s=='Open'&&$pastdeurbadkamer<57))&&$d['badkamer_set']->m>0) {
