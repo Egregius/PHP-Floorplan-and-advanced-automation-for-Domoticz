a1b2711a3b439a132ff1999875cffdb76ae8a168
Egregius <guy@egregius.be>
2026-04-17 08:45:02 +0200

    Update

--- secure/mqtt__energy.php
+++ secure/mqtt__energy.php
@@ -75,16 +75,12 @@ $mqtt->subscribe('d/e/+', function (string $topic, string $status) use (&$time, 
 	static $n=0;
 	static $z=0;
 	static $b=0;
+	static $a=0;
+	static $prevavg=0;
 	${$topic}=$status;
 	$p=$n+$z-$b;
-	lg("n=$n	z=$z	b=$b	p=$p");
+	echo "n=$n	z=$z	b=$b	p=$p".PHP_EOL;
 	
-	if ($z > $peakpower || empty($peakpower)) {
-    	$peakpower=$en->z;
-    	setCache('peakpower', $peakpower);
-    	$msg='Solar Peakpower = '.$peakpower.'W';
-    	shell_exec('/var/www/html/secure/telegram.sh "'.$msg.'" "false" "1" > /dev/null 2>/dev/null &');
-    }
     if ($z == 0 || empty($alwayson)) {
         $power = ($b < 0) ? ($n - $b) : $n;
         if ($power >= 30 && ($power < $alwayson || empty($alwayson))) {
@@ -96,6 +92,44 @@ $mqtt->subscribe('d/e/+', function (string $topic, string $status) use (&$time, 
             $dbverbruik->query($q, [':date' => $vandaag, ':w' => $alwayson]);
         }
     }
+    if($topic==='z') {
+		if ($z > $peakpower || empty($peakpower)) {
+			$peakpower=$en->z;
+			setCache('peakpower', $peakpower);
+			$msg='Solar Peakpower = '.$peakpower.'W';
+			shell_exec('/var/www/html/secure/telegram.sh "'.$msg.'" "false" "1" > /dev/null 2>/dev/null &');
+		}
+    } elseif($topic==='a') {
+    	$newavg      = $a;
+		$prevavg     = (float)getCache('energy_prevavg');
+		if ($a>2500) {
+    		$kwartierpiek = 2500;
+			$q = "SELECT MAX(wH) AS wH FROM `kwartierpiek` WHERE date LIKE :date";
+			$stmt = $dbverbruik->query($q, [':date' => date('Y-m', $time) . '-%']);
+			if ($row = $stmt->fetch()) {
+				$kwartierpiek = $row['wH'] ?? 2500;
+			}
+			if ($prevavg > 2500) {
+			if ($newavg > $kwartierpiek - 200) {
+					alert('Kwartierpiek', 'Kwartierpiek momenteel al ' . $newavg . ' Wh!' . PHP_EOL . 'Piek deze maand = ' . $kwartierpiek . ' Wh',$time);
+				}
+				if ($newavg < $prevavg) {
+					try {
+						$q = "INSERT INTO `kwartierpiek` (`date`, `wh`) VALUES (:date, :wh)";
+						$dbverbruik->query($q, [':date' => date('Y-m-d H:i:s'), ':wh' => $prevavg]);
+						if ($prevavg > $kwartierpiek - 200) {
+							alert('KwartierpiekB', 'Kwartierpiek = ' . $prevavg . ' Wh' . PHP_EOL . 'Vorige piek deze maand = ' . $kwartierpiek . ' Wh',$time);
+							$kwartierpiek = $prevavg;
+						}
+					} catch (Exception $e) {
+						lg("Error updating kwartierpiek: " . $e->getMessage());
+					}
+				}
+			}
+		}
+		setCache('energy_prevavg', $newavg);
+    }
+    
 }, MqttClient::QOS_AT_LEAST_ONCE);
 
 while (true) {
@@ -137,39 +171,6 @@ function processEnergyData($dbverbruik, $dbzonphp, &$force, $newData, &$mqtt, $t
     $injectie    = $newData['export'];
     $waterStand  = $newData['water'];
     
-    $newavg      = $en->a;
-    $prevavg     = (float)getCache('energy_prevavg');
-
-    // 3. Alwayson logica
-/*    if ($en->z == 0 || empty($alwayson)) {
-        $power = ($en->b < 0) ? ($en->n - $en->b) : $en->n;
-        if ($power >= 30 && ($power < $alwayson || empty($alwayson))) {
-            setCache('alwayson', $power);
-            $alwayson = $power;
-            $force = true;
-            lg('💡 New alwayson ' . $power . ' W');
-            $q = "INSERT INTO `alwayson` (`date`, `w`) VALUES (:date, :w) ON DUPLICATE KEY UPDATE `w` = VALUES(`w`)";
-            $dbverbruik->query($q, [':date' => $vandaag, ':w' => $alwayson]);
-        }
-    }*/
-    
-	if ($prevavg > 2500) {
-		if ($newavg > $kwartierpiek - 200) {
-			alert('Kwartierpiek', 'Kwartierpiek momenteel al ' . $newavg . ' Wh!' . PHP_EOL . 'Piek deze maand = ' . $kwartierpiek . ' Wh',$time);
-		}
-		if ($newavg < $prevavg) {
-			try {
-				$q = "INSERT INTO `kwartierpiek` (`date`, `wh`) VALUES (:date, :wh)";
-				$dbverbruik->query($q, [':date' => date('Y-m-d H:i:s'), ':wh' => $prevavg]);
-				if ($prevavg > $kwartierpiek - 200) {
-					alert('KwartierpiekB', 'Kwartierpiek = ' . $prevavg . ' Wh' . PHP_EOL . 'Vorige piek deze maand = ' . $kwartierpiek . ' Wh',$time);
-					$kwartierpiek = $prevavg;
-				}
-			} catch (Exception $e) {
-				lg("Error updating kwartierpiek: " . $e->getMessage());
-			}
-		}
-	}
     // 4. Zon data ophalen
     $zonvandaag = 0; $zontotaal = 0;
     $q = "SELECT Geg_Maand FROM `tgeg_maand` WHERE `Datum_Maand` = :datum";
@@ -296,7 +297,6 @@ function processEnergyData($dbverbruik, $dbzonphp, &$force, $newData, &$mqtt, $t
         }
     }
     $force = false;
-    setCache('energy_prevavg', $newavg);
 }
 function lg($msg) {
 	echo $msg."\n";
