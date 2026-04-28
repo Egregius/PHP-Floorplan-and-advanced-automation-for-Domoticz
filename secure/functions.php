b18519b12e77ab1304087a379b852d5f701de2f3
Egregius <guy@egregius.be>
2026-04-28 22:10:03 +0200

    Update

--- secure/functions.php
+++ secure/functions.php
@@ -145,7 +145,7 @@ function fhall() {
 		}
 	} else finkom();
 	if ($d['weg']->s==0&&$d['alexslaapt']->s==1&&$d['rkamerl']->s>70&&$d['rkamerr']->s>70&&$d['time']>=strtotime('21:30')&&$d['time']<=strtotime('23:00')&&$d['kamer']->s==0&&past('kamer')>7200) {
-		sl('kamer', 1, basename(__FILE__).':'.__LINE__);
+		sl('kamer1', 1, basename(__FILE__).':'.__LINE__);
 		shell_exec('php /var/www/setSSID.php \'{"main24":1}\' > /dev/null 2>&1 &');
 		if($d['Egregius']->s!=1) store('Egregius',1,basename(__FILE__).':'.__LINE__);
 	}
