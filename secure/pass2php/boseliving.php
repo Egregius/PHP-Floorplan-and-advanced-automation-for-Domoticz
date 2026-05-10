043bd6dd6fde5edba86d8250189a2e37d51a2867
Egregius <guy@egregius.be>
2026-05-10 18:35:03 +0200

    Update

--- secure/pass2php/boseliving.php
+++ secure/pass2php/boseliving.php
@@ -1,3 +1,3 @@
 <?php
-if($status=='On') hassAddon('d5369777_music_assistant_beta','start');
-elseif($status=='Off') hassAddon('d5369777_music_assistant_beta','stop');
+//if($status=='On') hassAddon('d5369777_music_assistant_beta','start');
+//elseif($status=='Off') hassAddon('d5369777_music_assistant_beta','stop');
