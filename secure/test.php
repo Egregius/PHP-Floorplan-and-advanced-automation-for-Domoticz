206d9913457de63b50caf58c0cdd4f94c94b9ed2
Egregius <guy@egregius.be>
2026-05-18 11:30:01 +0200

    Update

--- secure/test.php
+++ secure/test.php
@@ -11,12 +11,9 @@ $user='test';
 //$db = Database::getInstance();
 
 
-
 define('WIIM_IP', '192.168.2.9');
-define('BOSE_IP', '192.168.2.101');
 define('BELL_URL', 'http://192.168.2.2/sounds/doorbell.mp3');
 
-// Basisfunctie voor de WiiM
 function Wiim(string $cmd) {
     $url = "https://" . WIIM_IP . "/httpapi.asp?command=$cmd";
     $ch = curl_init($url);
@@ -29,98 +26,18 @@ function Wiim(string $cmd) {
     return $response;
 }
 
-// Functie voor reguliere Bose API (poort 8090)
-function bosePost(string $endpoint, string $xml) {
-    $url = "http://" . BOSE_IP . ":8090/" . $endpoint;
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/xml']);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-    $response = curl_exec($ch);
-    curl_close($ch);
-    return $response;
-}
-
-// Functie voor DLNA/UPnP SOAP-calls naar Bose (poort 8091)
-function boseDlnaCall(string $action, string $arguments) {
-    $url = "http://" . BOSE_IP . ":8091/MediaRenderer/AVTransport/Control";
-    
-    $xml = '<?xml version="1.0" encoding="utf-8"?>
-    <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-        <s:Body>
-            <u:' . $action . ' xmlns:u="urn:schemas-upnp-org:service:AVTransport:1">
-                <InstanceID>0</InstanceID>
-                ' . $arguments . '
-            </u:' . $action . '>
-        </s:Body>
-    </s:Envelope>';
-
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        'Content-Type: text/xml; charset="utf-8"',
-        'SOAPACTION: "urn:schemas-upnp-org:service:AVTransport:1#' . $action . '"'
-    ]);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-    $response = curl_exec($ch);
-    curl_close($ch);
-    return $response;
-}
-
-// --- HOOFDSCRIPT ---
-
-// 1. Check of de WiiM speelt, zo ja: pauzeer Spotify netjes
+// 1. Vraag de huidige status op om het volume te weten
 $statusJson = Wiim("getPlayerStatus");
 $status = json_decode($statusJson, true);
-$wasPlaying = ($status && isset($status['status']) && $status['status'] === 'play');
+$currentVol = $status['vol'] ?? 50;
 
-if ($wasPlaying) {
-    Wiim("setPlayerCmd:pause");
-}
+// 2. Bereken het deurbelvolume (bijv. minimaal naar 70)
+$bellVol = ($currentVol < 70) ? 70 : $currentVol;
 
-// 2. Zet het volume op de Bose alvast goed via de basisfunctie
-$volumeXml = @file_get_contents("http://" . BOSE_IP . ":8090/volume");
-$currentVolume = 17;
-if ($volumeXml) {
-    $xmlObj = simplexml_load_string($volumeXml);
-    if ($xmlObj && isset($xmlObj->actualvolume)) {
-        $currentVolume = (int)$xmlObj->actualvolume;
-    }
-}
-$targetVolume = ($currentVolume < 60) ? 60 : $currentVolume;
-//bosePost('volume', "<volume>{$targetVolume}</volume>");
-
-// 3. DLNA: Geef de deurbel URL door aan de SoundTouch renderer (Poort 8091)
-$argsSet = '<CurrentURI>' . htmlspecialchars(BELL_URL) . '</CurrentURI><CurrentURIMetaData></CurrentURIMetaData>';
-boseDlnaCall('SetAVTransportURI', $argsSet);
-
-// 4. DLNA: Start het afspelen direct
-$argsPlay = '<Speed>1</Speed>';
-boseDlnaCall('Play', $argsPlay);
-
-// 5. Wacht exact de tijd dat de deurbel nodig heeft om af te spelen
-sleep(9);
-
-// 6. Sla de deurbel stream plat via DLNA Stop
-boseDlnaCall('Stop', '');
-
-// 7. Dwing de Bose via de reguliere API terug naar de AUX input (WiiM)
-bosePost('key', '<key state="press" sender="GAV">AUX_INPUT</key>');
-bosePost('key', '<key state="release" sender="GAV">AUX_INPUT</key>');
-
-// 8. Herstel het volume naar het oude niveau
-bosePost('volume', "<volume>{$currentVolume}</volume>");
-
-// 9. Start Spotify op de WiiM weer op
-if ($wasPlaying) {
-    Wiim("setPlayerCmd:resume");
-}
-
-
+// 3. Stuur de deurbel als een officiële 'WMS' notificatie-interruptie
+// Formaat: wms_notify:URL:VOLUME
+$encodedUrl = urlencode(BELL_URL);
+echo Wiim("wms_notify:$encodedUrl:$bellVol");
 
 
 
