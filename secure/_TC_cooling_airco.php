db3d10672c8d61707c153aa798821f39e796da38
Egregius <guy@egregius.be>
2026-06-02 14:30:01 +0200

    Update

--- secure/_TC_cooling_airco.php
+++ secure/_TC_cooling_airco.php
@@ -6,7 +6,7 @@ $daikinDefaults = ['power'=>99,'mode'=>99,'set'=>99,'fan'=>99,'spmode'=>99];
 $daikin ??= new stdClass();
 foreach (['living','kamer','alex'] as $k) {
 	$daikin->$k ??= (object)$daikinDefaults;
-	if ($d[$k.'_set']->s!='D') {
+	if ($d[$k.'_set']->s!='D'&&$d[$k.'_set']->s!='Off') {
 		${'dif'.$k}=number_format($d[$k.'_temp']->s-$d[$k.'_set']->s,1);
 		if (${'dif'.$k}>$bigdif) $bigdif=${'dif'.$k};
 	}
@@ -24,12 +24,12 @@ else $spmode=-1;
 
 foreach (['living','kamer','alex'] as $k) {
 	$fan='A';
-	if ($d[$k.'_set']->m==0&&$d['raam'.$k]->s=='Closed') {
+	if (($d[$k.'_set']->m==0||$d[$k.'_set']->m==2)&&($d['raam'.$k]->s=='Closed'||($d['raam'.$k]->s=='Open'&&past('raam'.$k)<=60))) {
 		$mode=2;
 		$power=1;
 		$set=22;
 		if ($d[$k.'_set']->s!='D') store($k.'_set','D',basename(__FILE__).':'.__LINE__);
-	} elseif ($d[$k.'_set']->m>=1&&$d[$k.'_set']->s<33&&$d['raam'.$k]->s=='Closed'&&past('raam'.$k)>60) {
+	} elseif ($d[$k.'_set']->m==1&&$d[$k.'_set']->s<33&&($d['raam'.$k]->s=='Closed'||($d['raam'.$k]->s=='Open'&&past('raam'.$k)<=60))) {
 		$mode=3;
 		$power=1;
 		if($d[$k.'_set']->s==1) {
@@ -57,6 +57,7 @@ foreach (['living','kamer','alex'] as $k) {
 		$mode=2;
 		$power=0;
 		$set=33;
+		if ($d[$k.'_set']->s!='Off') store($k.'_set','Off',basename(__FILE__).':'.__LINE__);
 	}
 	if ((($daikin->$k->set!=$set||$daikin->$k->power!=$power||$daikin->$k->mode!=$mode||$daikin->$k->fan!=$fan)&&$spmode<2)||($power!=0&&$daikin->$k->lastset <= $time-281)) {
 		if(daikinset($k, $power, $mode, $set, basename(__FILE__).':'.__LINE__, $fan, $spmode, $maxpow)) {
