4a9fbd3be8e22c29b1351a3dd0167893bb0b87ac
Egregius <guy@egregius.be>
2026-06-08 11:30:01 +0200

    Update

--- secure/_weather.php
+++ secure/_weather.php
@@ -274,7 +274,7 @@ if (!isset($weathercache)||$weathercache!==$weather) {
 		13 => 'snow',
 		50 => 'fog',
 	];
-	setNextubeWeather(round($temp,1),$hum,$icons[$icon],$rain);
+	setNextubeWeather(round($temp,1),$hum,$icons[$icon],($rain<10?round($rain,1):round($rain,0)));
 }
 //$avg=null;
 //if ($d['buiten_temp']['icon']!=$avg) storeicon('buiten_temp',$avg);
