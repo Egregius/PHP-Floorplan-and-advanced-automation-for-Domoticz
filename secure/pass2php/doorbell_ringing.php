93961b59a8311fe8c84456ab2f59cb3048058402
Egregius <guy@egregius.be>
2026-06-15 15:00:01 +0200

    Update

--- secure/pass2php/doorbell_ringing.php
+++ secure/pass2php/doorbell_ringing.php
@@ -5,7 +5,7 @@ if ($status=='On'&&$d['deurvoordeur']->s=='Closed') {
 		if ($d['deurvoordeur']->s=='Closed') {
 			setCache('ring_ding', $time);
 			if ($d['weg']->s==0) {
-				if (($d['bose101']->s=='On'||$d['bose102']->s=='On'||$d['bose103']->s=='On'||$d['bose104']->s=='On'||$d['bose105']->s=='On'||$d['bose106']->s=='On'||$d['bose107']->s=='On'||$d['bose108']->s=='On'||$d['bose109']->s=='On')&&$d['pirliving']->s=='Off'&&$d['pirkeuken']->s=='Off') {
+				if (($d['bose102']->s=='On'||$d['bose103']->s=='On'||$d['bose104']->s=='On'||$d['bose105']->s=='On'||$d['bose106']->s=='On'||$d['bose107']->s=='On'||$d['bose108']->s=='On'||$d['bose109']->s=='On')&&$d['pirliving']->s=='Off'&&$d['pirkeuken']->s=='Off') {
 					shell_exec('curl -s "http://127.0.0.1/secure/pass2php/belknopbose101.php" > /dev/null 2>/dev/null &');
 				}
 
