fdd29998228d341de59ac7acae297bb907a26141
Egregius <guy@egregius.be>
2026-06-15 09:30:01 +0200

    Update

--- secure/test.php
+++ secure/test.php
@@ -22,6 +22,7 @@ include('pass2php/doorbell_ringing.php');
 
 //echo boseplayinfo("doorbell", 60, basename(__FILE__).':'.__LINE__, 101);
 //Wiim("setPlayerCmd:playPromptUrl:" . urlencode("http://192.168.2.2/sounds/doorbell.mp3"));
+
 //sleep(5);
 //$preset=wiimplaylist();
 //Wiim("MCUKeyShortClick:$preset");
