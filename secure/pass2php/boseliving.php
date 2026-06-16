c6aae485ba50e77af64160f252ba3cb9a4df11b5
Egregius <guy@egregius.be>
2026-06-16 08:30:01 +0200

    Update

--- secure/pass2php/boseliving.php
+++ secure/pass2php/boseliving.php
@@ -2,7 +2,6 @@
 //if($status=='On') hassAddon('d5369777_music_assistant_beta','start');
 //else
 if($status=='Off') {
-	if($d['music_assistant_beta']->s!='Off') sw('music_assistant_beta','Off',basename(__FILE__).':'.__LINE__);
 	Wiim('setPlayerCmd:stop');
 } elseif($status=='On') {
 	Wiim('setPlayerCmd:clear_playlist');
