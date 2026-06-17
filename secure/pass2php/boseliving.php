65b789ee15e4f129ebc5d115a8aa55227abb1f0f
Egregius <guy@egregius.be>
2026-06-17 10:00:01 +0200

    Update

--- secure/pass2php/boseliving.php
+++ secure/pass2php/boseliving.php
@@ -1,9 +1,11 @@
 <?php
 if($status=='Off') {
 	Wiim('setPlayerCmd:stop');
-} elseif($status=='On') {
 	Wiim('setPlayerCmd:clear_playlist');
+} elseif($status=='On') {
 	$preset=wiimplaylist();
+	Wiim("setPlayerCmd:playindex:1");
+	sleep(1);
 	Wiim("MCUKeyShortClick:$preset");
 	sleep(1);
 	Wiim("setPlayerCmd:playindex:1");
