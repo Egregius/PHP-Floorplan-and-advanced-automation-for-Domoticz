7a9056e388ea8a786f0e88a17bf7ecc6882312de
Egregius <guy@egregius.be>
2026-07-03 17:00:01 +0200

    Update

--- secure/_cron10.php
+++ secure/_cron10.php
@@ -96,5 +96,5 @@ if ($d['auto']->s=='On') {
 if ($d['weg']->s<=2&&$d['n']<-1200&&$d['b']>0&&$d['grohered']->s=='Off') sw('grohered', 'On', ' n='.$d['n'].'W b='.$d['b'].'W', true);
 elseif ($d['grohered']->s=='On'&&past('8keuken_8')>1800&&$d['n']>100&&$d['b']<0&&$d['wasbak']->s==0&&$d['snijplank']->s==0) sw('grohered', 'Off', ' n='.$d['n'].'W b='.$d['b'].'W',true);
 if ($d['regenpomp']->s=='On'&&past('regenpomp')>50) sw('regenpomp', 'Off');
-
-if ($d['water']->s=='On'&&past('water')>=$d['water']->m) sw('water', 'Off');
+$pastwater=past('water');
+if ($d['water']->s=='On'&&$pastwater>50&&$pastwater>=$d['water']->m) sw('water', 'Off');
