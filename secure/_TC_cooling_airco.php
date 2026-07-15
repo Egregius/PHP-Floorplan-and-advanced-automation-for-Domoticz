a386e36e1cce02af4fd6b6556abe8ec722e2561f
Egregius <guy@egregius.be>
2026-07-15 18:30:01 +0200

    Update

--- secure/_TC_cooling_airco.php
+++ secure/_TC_cooling_airco.php
@@ -23,13 +23,26 @@ elseif ($bigdif>=0.3) $maxpow=50;
 else $maxpow=40;
 
 if ($d['weg']->s>0) $maxpow=40;
-if ($maxpow<=40) {$maxpow=40;$spmode=-1;}
-elseif ($maxpow>=100) {$maxpow=100;$spmode=0;}
-else $spmode=-1;
+if ($maxpow<=40) $maxpow=40;
+elseif ($maxpow>=100) $maxpow=100;
 
 
 foreach (['living','kamer','alex'] as $k) {
+	if ($d[$k.'_set']->m==1&&$d[$k.'_set']->s<33&&($d['raam'.$k]->s=='Closed'||($d['raam'.$k]->s=='Open'&&past('raam'.$k)<=60))) {
+		if($d[$k.'_set']->s==2) {
+			if($maxpow<50) $maxpow=50;
+		} elseif($d[$k.'_set']->s==3) {
+			if($maxpow<60) $maxpow=60;
+		} elseif($d[$k.'_set']->s==4) {
+			if($maxpow<80) $maxpow=80;
+		} elseif($d[$k.'_set']->s==5) {
+			if($maxpow<100) $maxpow=100;
+		}
+	}
+}
+foreach (['living','kamer','alex'] as $k) {
 	$fan='A';
+	$spmode=-1;
 	if (($d[$k.'_set']->m==0||$d[$k.'_set']->m==2)&&($d['raam'.$k]->s=='Closed'||($d['raam'.$k]->s=='Open'&&past('raam'.$k)<=60))) {
 		$mode=2;
 		$power=1;
@@ -44,22 +57,20 @@ foreach (['living','kamer','alex'] as $k) {
 		} elseif($d[$k.'_set']->s==2) {
 			$fan=4;
 			$set=18;
-			$maxpow=50;
 		} elseif($d[$k.'_set']->s==3) {
 			$fan=5;
 			$set=18;
-			$maxpow=60;
 		} elseif($d[$k.'_set']->s==4) {
 			$fan=6;
 			$set=18;
 			$spmode=0;
-			$maxpow=80;
 		} elseif($d[$k.'_set']->s==5) {
 			$fan=7;
 			$set=18;
 			$spmode=1;
-			$maxpow=100;
-		} else $set=$d[$k.'_set']->s;
+		} else {
+			$set=$d[$k.'_set']->s;
+		}
 	} else {
 		$mode=2;
 		$power=0;
@@ -67,7 +78,7 @@ foreach (['living','kamer','alex'] as $k) {
 		if ($d[$k.'_set']->s!='Off') store($k.'_set','Off',basename(__FILE__).':'.__LINE__);
 	}
 	if ($d['daikin']->s=='On') {
-		if ((($daikin->$k->set!=$set||$daikin->$k->power!=$power||$daikin->$k->mode!=$mode||$daikin->$k->fan!=$fan)&&$spmode<2)||(($d['daikin']->s=='On'&&$power!=0&&$daikin->$k->lastset <= $time-291)||($d['daikin']->s=='On'&&$power==0&&$daikin->$k->lastset <= $time-291))) {
+		if ((($daikin->$k->set!=$set||$daikin->$k->power!=$power||$daikin->$k->mode!=$mode||$daikin->$k->spmode!=$spmode||$daikin->$k->fan!=$fan)&&$spmode<2)||(($d['daikin']->s=='On'&&$power!=0&&$daikin->$k->lastset <= $time-291)||($d['daikin']->s=='On'&&$power==0&&$daikin->$k->lastset <= $time-291))) {
 			if(daikinset($k, $power, $mode, $set, basename(__FILE__).':'.__LINE__, $fan, $spmode, $maxpow)) {
 				$daikin->$k->power=$power;
 				$daikin->$k->mode=$mode;
