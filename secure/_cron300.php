11f71e9e7070e71e401bae97d6e0b2e6ced9046d
Egregius <guy@egregius.be>
2026-07-25 09:00:01 +0200

    Update

--- secure/_cron300.php
+++ secure/_cron300.php
@@ -59,13 +59,7 @@ if ($d['auto']->s!='On'&&past('auto')>43200) {
 	sw('auto', 'On', basename(__FILE__).':'.__LINE__);
 	alert('AUTO','AUTO ingeschakeld na 12 uur',60,false,3);
 }
-if (past('weg')>18000&& $d['weg']->s==0&& past('pirliving')>18000&& past('pirkeuken')>18000&& past('pirinkom')>18000&& past('pirhall')>18000&& past('pirgarage')>18000) {
-	store('weg', 1, basename(__FILE__).':'.__LINE__);
-	alert('WEG','Slapen ingeschakeld na 5 uur geen beweging',60,false,3);
-} elseif (past('weg')>36000&& $d['weg']->s==1&& past('pirliving')>36000&& past('pirkeuken')>36000&& past('pirinkom')>36000&& past('pirhall')>36000&& past('pirgarage')>36000) {
-	store('weg', 2, basename(__FILE__).':'.__LINE__);
-	alert('WEG','Weg ingeschakeld na 10 uur geen beweging',60,false,3);
-}
+
 if ($d['zolderg']->s=='On'&&past('zolderg')>7200&&past('pirgarage')>7200) sw('zolderg', 'Off', basename(__FILE__).':'.__LINE__);
 
 
--- secure/_cron900.php
+++ secure/_cron900.php
@@ -1,3 +1,13 @@
 <?php
 setNextubeMode();
 
+if (past('weg')>18000&& $d['weg']->s==0&& past('pirliving')>18000&& past('pirkeuken')>18000&& past('pirinkom')>18000&& past('pirhall')>18000&& past('pirgarage')>18000) {
+	store('weg', 1, basename(__FILE__).':'.__LINE__);
+	alert('WEG','Slapen ingeschakeld na 5 uur geen beweging',60,false,3);
+} elseif (past('weg')>36000&& $d['weg']->s==1&& past('pirliving')>36000&& past('pirkeuken')>36000&& past('pirinkom')>36000&& past('pirhall')>36000&& past('pirgarage')>36000) {
+	store('weg', 2, basename(__FILE__).':'.__LINE__);
+	alert('WEG','Weg ingeschakeld na 10 uur geen beweging',60,false,3);
+} elseif (past('weg')>86400&& $d['weg']->s==2&& past('pirliving')>86400&& past('pirkeuken')>86400&& past('pirinkom')>86400&& past('pirhall')>86400&& past('pirgarage')>86400) {
+	store('weg', 3, basename(__FILE__).':'.__LINE__);
+	alert('WEG','Vakantie ingeschakeld na 24 uur geen beweging',60,false,3);
+}
