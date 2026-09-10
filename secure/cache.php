445c5c95e467ae10fe0462576bf3641cc764b38b
Egregius <guy@egregius.be>
2026-09-10 08:00:01 +0200

    Update

--- secure/cache.php
+++ secure/cache.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require 'functions.php';
 if (isset($_REQUEST['zon'])) {
 	$en=json_decode(getCache('en'),true);
